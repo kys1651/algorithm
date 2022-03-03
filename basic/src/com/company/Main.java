a23bff2a23e907fe6d580565bf89f7c35abfe5bb
kimyongsu <akys159357@naver.com>
2022-03-03 23:07:51 +0900

    세 값의 최솟값

--- basic/src/com/company/Main.java
+++ basic/src/com/company/Main.java
@@ -3,18 +3,18 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    //세 값의 최댓값
+    //세 값의 최솟값
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
 
-        System.out.println("세 정수의 최대값을 구합니다.");
+        System.out.println("세 정수의 최솟값을 구합니다.");
         System.out.print("a의 값: "); int a = sc.nextInt();
         System.out.print("b의 값: "); int b = sc.nextInt();
         System.out.print("c의 값: "); int c = sc.nextInt();
 
-        int max = (a > b)? a : b;
-        max = (max>c)? max : c;
+        int min = (a > b)? b : a;
+        min = (min>c)? c : min;
 
-        System.out.println("최댓값은 "+max+"입니다.");
+        System.out.println("최댓값은 " + min + "입니다.");
     }
 }
