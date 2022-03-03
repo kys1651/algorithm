be2c28ff0e15e3fbd5233f52632fbc5052d7532a
kimyongsu <akys159357@naver.com>
2022-03-03 23:22:10 +0900

    2022.03.03

--- basic/src/com/company/Main.java
+++ basic/src/com/company/Main.java
@@ -3,18 +3,19 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    //세 값의 최솟값
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
+//1부터 n까지의 합
+//        System.out.println("1부터 n까지의 합을 구합니다.");
+//        System.out.print("n의 값 : ");
+//        int n = sc.nextInt();
+//
+//        int sum = 0;
+//        for(int i = 1; i <= n; i++){
+//            sum += i;
+//        }
+//        System.out.println("1부터 "+ n +"까지의 합은 " + sum + "입니다.");
 
-        System.out.println("세 정수의 최솟값을 구합니다.");
-        System.out.print("a의 값: "); int a = sc.nextInt();
-        System.out.print("b의 값: "); int b = sc.nextInt();
-        System.out.print("c의 값: "); int c = sc.nextInt();
 
-        int min = (a > b)? b : a;
-        min = (min>c)? c : min;
-
-        System.out.println("최댓값은 " + min + "입니다.");
     }
 }
