8b6e7ce58b14e1d67a442fe341b6bce3629968c2
kimyongsu <akys159357@naver.com>
2022-03-03 23:03:26 +0900

    세 값의 최댓값 구하기

--- basic/src/com/company/Main.java
+++ basic/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    //세 값의 최댓값
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+	    Scanner sc = new Scanner(System.in);
+
+        System.out.println("세 정수의 최대값을 구합니다.");
+        System.out.print("a의 값: "); int a = sc.nextInt();
+        System.out.print("b의 값: "); int b = sc.nextInt();
+        System.out.print("c의 값: "); int c = sc.nextInt();
+
+        int max = (a > b)? a : b;
+        max = (max>c)? max : c;
+
+        System.out.println("최댓값은 "+max+"입니다.");
     }
 }
