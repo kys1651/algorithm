b7523864929943e089155a287bd12ac6a0494840
김용수 <43926186+kys1651@users.noreply.github.com>
2023-04-08 13:42:28 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 588 ms, Memory: 294808 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -9,29 +9,18 @@ class Main {
         int k = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder("<");
 
-        boolean[] arr = new boolean[n];
-        int count = 0; // 점프 한 수
-        int idx = 0; // 인덱스
-        int check = 0; // 총 몇개를 넣었는지
-
-        while(check != n){
-            idx = idx % n;
-
-            if(arr[idx] == false){
-                count++;
-            }
-            if(count == k){
-                arr[idx] = true;
-                sb.append((idx+1) + (check==(n-1)?">":", "));
-                count = 0;
-                check++;
-            }
-            idx++;
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
         }
+
+        while(q.size() != 1){
+            for(int i = 0 ; i < k-1;i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
         System.out.println(sb);
-
-
-
-
     }
 }
