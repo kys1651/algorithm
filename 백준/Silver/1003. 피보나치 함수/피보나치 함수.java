be11af077f7797c5ce7857c9f715a722a0146c30
김용수 <43926186+kys1651@users.noreply.github.com>
2023-06-01 11:09:05 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.util.*;
 
 public class Main {
-    static int zero;
-    static int one;
-    static int zero_plus_one;
-//    static int[][] dp = new int[41][2];
+    static int[][] dp = new int[41][2];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,30 +13,34 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
 
-//        for (int i = 0; i < dp.length; i++) {
-//            for (int j = 0; j < dp[0].length; j++) {
-//                dp[i][j] = -1;
-//            }
-//        }
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
         while (T-- > 0) {
+
             int n = Integer.parseInt(br.readLine());
 
             fibonacci(n);
-            sb.append(zero).append(" ").append(one).append("\n");
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
         }
+
         System.out.println(sb);
 
     }
 
-    private static void fibonacci(int n) {
-        zero = 1;
-        one = 0;
-        zero_plus_one = 1;
-
-        for (int i = 0; i < n; i++) {
-            zero = one;
-            one = zero_plus_one;
-            zero_plus_one = zero + one;
+    private static int[] fibonacci(int n) {
+        if (dp[n][0] == -1 || dp[n][1] == -1) {
+            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
+            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
         }
+        return dp[n];
     }
 }
