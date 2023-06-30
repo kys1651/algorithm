cd4a96330490e4e4f78c1b8d6ae54b067a710ad0
김용수 <43926186+kys1651@users.noreply.github.com>
2023-07-01 00:23:46 +0900

    [level 1] Title: 3진법 뒤집기, Time: 19.10 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,32 +1,16 @@
 class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder(numtosam(n));
-        int answer = samtonum(sb.reverse().toString());
+        String str = "";
+        
+        while(n > 0){
+            str = (n%3) + str;
+            n /= 3;
+        }
+        str = new StringBuilder(str).reverse().toString();
+        int answer = Integer.parseInt(str,3);
         
         System.out.println(answer);
         
-        return answer;
-    }
-    
-    public String numtosam(int n){
-        String str = "";
-        
-        int num = 3;
-        while(n != 0){
-            str = String.valueOf(n % 3) + str;
-            n /= 3;
-        }
-        return str;
-    }
-    
-    public int samtonum(String str){
-        int answer = 0;
-        int digit = 1;
-        for(int i = str.length()-1; i >= 0; i--){
-            char ch = str.charAt(i);
-            answer += digit * (ch - '0');
-            digit *= 3;
-        }
         return answer;
     }
 }
