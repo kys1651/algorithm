9b67758f62162f98d06b7551483430b2a242a772
김용수 <43926186+kys1651@users.noreply.github.com>
2023-07-18 21:39:23 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 184 ms, Memory: 25504 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static class Point{
+        int idx;
+        int count;
+
+        public Point(int idx, int count) {
+            this.idx = idx;
+            this.count = count;
+        }
+    }
+    static int N,K;
+    static int[] map = new int[100001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        map[N] = 1;
+
+        bfs(new Point(N, 1));
+    }
+
+    private static void bfs(Point n) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(n);
+
+        // 앞 뒤 체크
+        while (!q.isEmpty()) {
+            Point tmp = q.poll();
+
+            // 앞으로 한칸
+            if (tmp.idx + 1 >= 0 && tmp.idx + 1 <= 100000) {
+                if (map[tmp.idx + 1] == 0 || map[tmp.idx + 1] > tmp.count + 1) {
+                    map[tmp.idx + 1] = tmp.count + 1;
+                    q.add(new Point(tmp.idx + 1, tmp.count + 1));
+                }
+            }
+
+            // 뒤로 한칸
+            if (tmp.idx - 1 >= 0 && tmp.idx - 1 <= 100000) {
+                if (map[tmp.idx - 1] == 0 || map[tmp.idx - 1] > tmp.count + 1) {
+                    map[tmp.idx - 1] = tmp.count + 1;
+                    q.add(new Point(tmp.idx - 1, tmp.count+1));
+                }
+            }
+
+            // 순간이동 경우
+            if (2 * tmp.idx >= 0 && 2 * tmp.idx <= 100000) {
+                if(map[2 * tmp.idx] == 0 || map[2 * tmp.idx] > tmp.count){
+                    map[2 * tmp.idx] = tmp.count;
+                    q.add(new Point(2 * tmp.idx, tmp.count));
+                }
+            }
+        }
+
+        System.out.println(map[K]-1);
+    }
+}
