44593442da25ab96c469c080269fb91d71f2bf02
김용수 <43926186+kys1651@users.noreply.github.com>
2023-07-22 18:11:27 +0900

    [Silver III] Title: N과 M (4), Time: 192 ms, Memory: 19536 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N,M;
-    static int[] map;
-//    static boolean[] visited;
+    static int N, M;
+    static boolean[] visited;
+    static int [] answer;
     static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,28 +15,28 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new int [N + 1];
-//        visited = new boolean[N + 1];
+        visited = new boolean[N + 1];
+        answer = new int[N + 1];
 
         solution(0,1);
-
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
-    private static void solution(int index, int n) {
-        if(index==M){
-            for(int i = 0 ; i < M; i++){
-                sb.append(map[i] + " ");
+    private static void solution(int index,int pos) {
+        if(index == M){
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i] + " ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = 1; i <= N; i++){
-                if(i >= n) {
-                    map[index] = i;
-                    solution(index + 1,i);
-                }
+        for (int i = 1; i <= N; i++) {
+            if(index > 0 && answer[index - 1] > i ) continue;
+            answer[index] = i;
+            solution(index + 1, i + 1);
         }
     }
 }
+
+
