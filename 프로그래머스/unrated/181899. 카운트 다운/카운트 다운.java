aed7266f2a4d175cbce4a86bc1bd899fab80d058
김용수 <43926186+kys1651@users.noreply.github.com>
2023-08-12 21:10:50 +0900

    [unrated] Title: 카운트 다운, Time: 3.00 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,7 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int start, int end) {
+        return IntStream.rangeClosed(end,start).map(i -> end + (start - i)).toArray();
+    }
+}
