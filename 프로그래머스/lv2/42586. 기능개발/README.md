1f65122c86ee8339080c8d922a9dfe9953833c75
김용수 <43926186+kys1651@users.noreply.github.com>
2023-08-23 16:54:12 +0900

    [level 2] Title: 기능개발, Time: 2.14 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/README.md
+++ 프로그래머스/lv2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 1.94 ms
+메모리: 75 MB, 시간: 2.14 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,40 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        // 정답을 저장 할 ArrayList
         ArrayList<Integer> answer = new ArrayList<>();
+        // 계산을 위한 stack
         Stack<Integer> stack = new Stack<>();
         
+        // 뒤에서부터 해주어 pop하면 첫번째 인덱스부터 계산 할 수 있도록 한다.
         for(int i = speeds.length - 1; i >= 0 ; i --){
+            // 100에서 현재 진행률을 빼주어 남은 진행률을 구해준다.
             int progress = 100 - progresses[i];
             int speed = speeds[i];
             int tmp = progress/speed;
             
+            // 나머지가 있다면 하루가 더 늘어나야하므로 계산식을 넣어줌
             if(progress%speed > 0){
                 tmp ++;
             }
-            
+            // 계산 값을 스택에 넣어줌
             stack.push(tmp);
         }
         
-        
+        // idx = 지표, count = 현재 배포 할 수 있는 기능
         int idx = stack.pop();
-        int tmp = 1;
+        int count = 1;
         while(!stack.isEmpty()){
             int check = stack.pop();
             
-            // System.out.print(idx + " " + check);
+            // 현재 기준보다 작거나 같으면 카운트를 늘려주고 continue;
             if(check <= idx){
-                // System.out.println(" skip");
-                tmp++;
+                count++;
                 continue;
             }
             
-            // System.out.println(" no skip");
+            // 더 크다면 idx에 현재 스택에서 pop한 값을 넣어주고 리스트에 count를 추가   
             idx = check;
-            answer.add(tmp);
-            tmp = 1;
+            answer.add(count);
+            count = 1;
         }
-        answer.add(tmp);
+        // while문이 끝나고 count값이 남으므로 한번 더 add하여준다.
+        answer.add(count);
+        
         return answer.stream().mapToInt(i->i).toArray();
     }
 }
