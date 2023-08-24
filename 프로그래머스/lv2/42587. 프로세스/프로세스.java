8ee25119a324c465757f0a06d5252ec912ee83a2
김용수 <43926186+kys1651@users.noreply.github.com>
2023-08-24 20:45:49 +0900

    [level 2] Title: 프로세스, Time: 0.63 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -2,20 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
+        // 내림차순으로 우선순위 큐 정렬
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
-        
-        for(int i = 0 ; i < priorities.length; i++){
-            pq.offer(priorities[i]);
+        //
+        for(int priority : priorities){
+            pq.offer(priority);
         }
         
         while(!pq.isEmpty()){
             for(int i = 0 ; i < priorities.length;i++){
+                // 전부 확인해준 뒤 우선 순위 큐에 가장 첫번째 값과 같은지 확인
                 if(priorities[i] == pq.peek()){
-                    if( i == location){
+                    // i와 location이 같다면 +1 후 리턴 해줌
+                    if(i == location){
                         answer++;
                         return answer;
                     }
+                    // 같지 않다면 제거 한뒤 answer을 1 증가
                     pq.remove();
                     answer++;
                 }
