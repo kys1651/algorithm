8f4439bbee7492913d6b198bf8adc0403b9c312c
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-05 13:41:52 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 2.49 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        int [] arr = new int [num_list.length -5];
-        
-        System.arraycopy(num_list,5,arr,0,arr.length);
-        
-        return arr;
+        return Arrays.stream(num_list).sorted().skip(5).toArray();
     }
 }
