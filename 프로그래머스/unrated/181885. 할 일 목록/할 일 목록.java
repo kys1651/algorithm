e5a7bd73d677a975ecf1a09e2daf2c1e0bb9c27a
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-06 13:30:48 +0900

    [unrated] Title: 할 일 목록, Time: 4.02 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -1,13 +1,11 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        ArrayList<String> list = new ArrayList<>();
-        for(int i = 0; i < finished.length; i++){
-            if(!finished[i])
-                list.add(todo_list[i]);
-        }
-        return list.toArray(new String[list.size()]);
+        return IntStream.range(0,finished.length)
+            .filter(i -> !finished[i])
+            .mapToObj(i -> todo_list[i])
+            .toArray(String[]::new);
     }
 
 }
