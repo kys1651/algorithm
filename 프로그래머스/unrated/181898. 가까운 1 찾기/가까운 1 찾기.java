c4ed4483ca2c09b1f1737b02b5a09e4cb3eee701
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-06 15:53:20 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr, int idx) {
+        for(int i = idx; i < arr.length; i++){
+            if(arr[i] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
