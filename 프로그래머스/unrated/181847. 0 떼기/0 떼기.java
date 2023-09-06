91a15fc8f88c2c3f6b745f7253cc7c71d905266f
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-06 13:15:23 +0900

    [unrated] Title: 0 떼기, Time: 9.26 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        int idx = 0;
-        while(true){
-            if(n_str.charAt(idx)!='0')
-                break;
-            idx++;
-        }
-        return n_str.substring(idx);
+        return "" + Integer.parseInt(n_str);
     }
 }
