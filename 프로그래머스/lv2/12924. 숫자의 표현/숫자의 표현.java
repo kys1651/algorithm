172a169b80c2de4f7beee392e4e6e9eb2015589d
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-07 10:49:53 +0900

    [level 2] Title: 숫자의 표현, Time: 0.06 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,25 +1,11 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 1; i <= n ; i++){
-            if(check(i,n)){
+        for(int i = 1; i <= n ; i+=2){
+            if(n % i == 0){
                 answer++;
             }
         }
         return answer;
-    }
-    
-    public boolean check(int start, int goal){
-        int result = 0;
-        
-        for(int i = start; i <= goal; i++){
-            result += i;
-            if(result > goal){
-                return false;
-            }else if(result == goal){
-                break;
-            }
-        }
-        return true;
     }
 }
