afe898fc175aae25c3d30deb8449e57f844d505f
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-10 19:30:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -4,23 +4,16 @@ class Solution {
     public int[] solution(int n, String[] words) {
         Set<String> check = new HashSet<>();
 
-        String prev = "";
-        for(int i = 0; i < words.length/n; i++){
-            for(int j = 0; j < n; j++){
-                int idx = i * n + j;
-                if(idx == 0){
-                    prev = words[idx];
-                    check.add(prev);
-                    continue;
-                }
-                
-                if(check.contains(words[idx]) || words[idx].charAt(0) != prev.charAt(prev.length()-1)){
-                    return new int[] {j+1, i+1};
-                }else{
-                    check.add(words[idx]);
-                    prev = words[idx];
-                }
+        char end = words[0].charAt(words[0].length()-1);
+        check.add(words[0]);
+        for(int i = 1; i < words.length; i++){
+            char start = words[i].charAt(0);
+            
+            if(check.contains(words[i]) || start != end){
+                return new int[] {(i % n)+1 , (i / n)+1};
             }
+            end = words[i].charAt(words[i].length() - 1);
+            check.add(words[i]);    
         }
 
         return new int[] {0,0};
