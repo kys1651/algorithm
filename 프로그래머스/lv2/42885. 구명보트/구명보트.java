7055fcea8ace38a4189ba42a55ef95d489898f87
김용수 <43926186+kys1651@users.noreply.github.com>
2023-09-10 20:00:57 +0900

    [level 2] Title: 구명보트, Time: 7.35 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,27 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        
         Arrays.sort(people);
+        
         int left = 0;
         int right = people.length - 1;
-        while(left <= right){
-            int leftPeople = people[left];
-            int rightPeople = people[right];
-            
-            answer++;
-            if(left == right){
-                break;
-            }
-            else if(leftPeople + rightPeople <= limit){
+        
+        for(; left < right;right--){
+            if(people[left] + people[right] <= limit){
                 left++;
-                right--;
-                continue;
-            }else{
-                right--;
             }
         }
-        return answer;
+        
+        return people.length - left;
     }
 }
