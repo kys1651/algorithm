11b81818ed1bcbdfb6864f1ec96ace0b600e42ab
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-10 13:28:29 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        for(char ch : number.toCharArray()){
+            answer += ch - '0';
+            answer %= 9;
+        }
+        return answer;
+    }
+}
