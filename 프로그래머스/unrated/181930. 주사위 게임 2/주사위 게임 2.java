9c0ef56465ec9e67285055b8983ba555d0259eb5
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-10 13:09:16 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,12 +1,22 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = a + b + c;
+        int count = 1;
+        
         if(a == b && b == c){
-            return answer * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
-        }else if(a == b || b == c || a == c){
-            return answer * (a * a + b * b + c * c);
-        }else {
-            return answer;
+            count++;
         }
+        if(a == b || b == c || a == c){
+            count++;
+        }
+        int result = 1;
+        for(int i = 1 ; i <= count; i++){
+            result *= (pow(a,i) + pow(b,i) + pow(c,i));
+        }
+        return result;
+    }
+    
+    public int pow(int base, int exponent){
+        if(exponent == 0) return 1;
+        return base * pow(base, exponent - 1);
     }
 }
