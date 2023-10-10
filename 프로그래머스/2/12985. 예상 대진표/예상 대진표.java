65dabd7837e5d8681a3363eecc82eebbc95b3070
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-10 14:19:32 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,16 +2,10 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int count = 1;
-        while(true){
-            if(a % 2 != 0) a++;
-            if(b % 2 != 0) b++;
-            
-            if(a == b)
-                break;
-            
-            a /= 2;
-            b /= 2;
+        int count = 0;
+        while(a != b){
+            a = (a / 2) + (a % 2);
+            b = (b / 2) + (b % 2);
             count++;
         }
         return count;
