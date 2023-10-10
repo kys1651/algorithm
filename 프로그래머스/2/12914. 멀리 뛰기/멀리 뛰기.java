812d8785d7d54c6510579af3da5bc0bf29c9215d
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-10 15:00:26 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,19 @@
+class Solution {
+    static final long divide = 1234567;
+        
+    public long solution(int n) {
+        if(n == 1 || n == 2){
+            return n;
+        }
+        
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i- 1] + dp[i - 2]) % divide;
+        }
+        return dp[n];
+    }
+}
