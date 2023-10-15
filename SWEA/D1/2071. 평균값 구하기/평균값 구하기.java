0f0ec5f440c2f6fa4ca6877220662045a2bcf928
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-15 14:14:00 +0900

    [D1] Title: 평균값 구하기, Time: 98 ms, Memory: 18,576 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+
+            double result = 0;
+            int size = st.countTokens();
+            while (st.hasMoreTokens()) {
+                result += Integer.parseInt(st.nextToken());
+            }
+            result /= size;
+            bw.write("#" + test_case + " " + (int) Math.round(result) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
