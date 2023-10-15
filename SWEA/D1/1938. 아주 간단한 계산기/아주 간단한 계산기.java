6519731840920cf3c78716360774cc6667bc757b
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-15 16:19:03 +0900

    [D1] Title: 아주 간단한 계산기, Time: 96 ms, Memory: 18,600 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write((a + b) + "\n");
+        bw.write((a - b) + "\n");
+        bw.write((a * b) + "\n");
+        bw.write((a / b) + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
