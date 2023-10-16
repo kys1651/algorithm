1966201432351e4d2cd3df1a75121d30991414bc
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-16 10:25:27 +0900

    [D2] Title: 초심자의 회문 검사, Time: 125 ms, Memory: 21,104 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -11,23 +11,13 @@ class Solution
 		for(int tc = 1; tc <= T; tc++)
 		{
             String word = sc.next();
+            StringBuilder sb = new StringBuilder(word);
+            String reverseWord = sb.reverse().toString();
+            
             boolean check = true;
-            // 0 1 2 3 4 len = 5; len/2 = 2
-            // 0 == 5 - 0 - 1(4) == true
-            // 1 == 5 - 1 -1(3) == true
-            
-            // 0 1 2 len = 3 len / 2 -> 1
-            // 0 == 3 - 0 - 1(2) == true
-            // 
-            for(int i = 0; i < word.length() / 2; i++){
-                char front = word.charAt(i);
-                char rear = word.charAt(word.length() - i - 1);
-				if(front != rear){
-	                check = false;
-                    break; 
-                }
+            if(!word.equals(reverseWord)){
+                check = false;
             }
-            
             System.out.printf("#%d %d\n",tc,check==true?1:0);
         }
 	}
