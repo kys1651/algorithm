4d0cd070599eaf0c22428950cbda936ad8bfdeec
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-16 21:10:29 +0900

    [D2] Title: 조교의 성적 매기기, Time: 154 ms, Memory: 25,700 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
@@ -3,43 +3,30 @@ import java.io.*;
 
 class Solution {
     public static void main(String args[]) throws Exception {
-        String[] grade = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"};
+        String[] grade = {"D0", "C-", "C0", "C+", "B-", "B0", "B+", "A-", "A0", "A+"};
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
 
+        int T = sc.nextInt();
         for (int tc = 1; tc <= T; tc++) {
             int N = sc.nextInt();
             int K = sc.nextInt();
-			double[] scores = new double[N + 1];
-            
-            for (int i = 1; i <= N; i++) {
+			double[] scores = new double[N];
+            for (int i = 0; i < N; i++) {
                 int m = sc.nextInt();
                 int f = sc.nextInt();
                 int h = sc.nextInt();
-                double score = cal(m, f, h);
-                scores[i] = score;
+                scores[i] = m * 0.35 + f * 0.45 + h * 0.2;
             }
-            double standard = scores[K];
+            
+            double standard = scores[K-1];
             Arrays.sort(scores);
-            int idx = 1;
-            int unit = 0;
-            for(int i = N; i >= 1; i--){
-                if(scores[i] > standard){
-                    idx++;
-                    if(idx > N/10){
-                    	idx = 1;
-                        unit++;
-                    }
-                }
-                else if(scores[i] == standard){
-                    System.out.printf("#%d %s\n",tc,grade[unit]);
+            for(int i = 0; i < N; i++){
+                if(scores[i] == standard){
+                    System.out.printf("#%d %s\n",tc,grade[i / (N / 10)]);
                     break;
                 }
             }
 
         }
-    }
-    static double cal(int m, int f, int h){
-        return m * 0.35 + f * 0.45 + h * 0.2;
     }
 }
