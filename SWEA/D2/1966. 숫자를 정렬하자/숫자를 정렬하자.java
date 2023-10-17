0c7928f400ee04667a83e677ba149d606f73afff
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-17 21:10:37 +0900

    [D2] Title: 숫자를 정렬하자, Time: 148 ms, Memory: 20,876 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -17,12 +17,28 @@ class Solution
                 input[i] = sc.nextInt();
             }
             
-            Arrays.sort(input);
+            // Arrays.sort(input); -> 가장 기본
+            bubbleSort(input);
             
             System.out.print("#" + tc + " ");
             printArray(input);
 		}
 	}
+    private static void bubbleSort(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+        	for(int j = 0; j < arr.length - 1 - i; j++){
+            	if(arr[j] > arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+    
+    private static void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+    	arr[j] = tmp;
+    }
     
     private static void printArray(int[] arr){
         for(int num : arr){
