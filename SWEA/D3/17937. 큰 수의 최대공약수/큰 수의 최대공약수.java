6ccbf51b2b8ffd155dcb3d2adabbff2ad5b3beda
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-20 10:20:16 +0900

    [D3] Title: 큰 수의 최대공약수, Time: 147 ms, Memory: 21,060 KB -BaekjoonHub

--- SWEA/D3/17937. 큰 수의 최대공약수/큰 수의 최대공약수.java
+++ SWEA/D3/17937. 큰 수의 최대공약수/큰 수의 최대공약수.java
@@ -14,28 +14,7 @@ class Solution
             BigInteger A = sc.nextBigInteger();
             BigInteger B = sc.nextBigInteger();;
             
-            BigInteger gcd = A;
-            if(!A.equals(B)){
-                gcd = GCD(A,B);
-            }
-            
-            System.out.println("#" + tc + " " + gcd);
+            System.out.println("#" + tc + " " + (A.equals(B)? A : "1"));
 		}
     }
-    
-    private static BigInteger GCD(BigInteger A, BigInteger B){
-        BigInteger result = A;
-        BigInteger One = new BigInteger("1");
-        
-        while(A.compareTo(B)  <= 0){
-
-            result = result.gcd(A);
-            if(result.equals(One)){
-                return One;
-            }
-            A = A.add(One);
-        }
-        return result;
-    }
-    
 }
