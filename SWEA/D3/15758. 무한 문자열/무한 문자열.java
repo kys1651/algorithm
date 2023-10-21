a33642097cdf7411d76defae88ed3f540b7566ce
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-21 09:29:08 +0900

    [D3] Title: 무한 문자열, Time: 133 ms, Memory: 20,408 KB -BaekjoonHub

--- SWEA/D3/15758. 무한 문자열/무한 문자열.java
+++ SWEA/D3/15758. 무한 문자열/무한 문자열.java
@@ -12,35 +12,13 @@ class Solution
 		{
             String a = sc.next();
             String b = sc.next();
-            int lcm = lcm(a.length(),b.length());
-            StringBuilder result1 = new StringBuilder();
-            StringBuilder result2 = new StringBuilder();
-			while(result1.length() != lcm){
-                result1.append(a);
-            }
-            while(result2.length() != lcm){
-                result2.append(b);
-            }
             System.out.print("#" + tc +" ");
-            if(result1.toString().equals(result2.toString())){
+            if((a+b).equals(b+a)){
                 System.out.println("yes");
             }else{
                 System.out.println("no");
             }
 		}
 	}
-    
-    private static int lcm(int a,int b){
-        return a * b / gcd(a,b);
-    }
-    
-    private static int gcd(int a, int b){
-        while(b!=0){
-            int tmp = b;
-            b = a % b;
-            a = tmp;
-        }
-        return a;
-    }
 }
     
