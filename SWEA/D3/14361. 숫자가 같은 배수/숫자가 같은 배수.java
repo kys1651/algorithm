6908a614f82c78388a924cfcddbddd8b9170661b
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-22 21:04:24 +0900

    [D3] Title: 숫자가 같은 배수, Time: 136 ms, Memory: 21,168 KB -BaekjoonHub

--- SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.java
+++ SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.java
@@ -7,46 +7,33 @@ class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-
 		for(int tc = 1; tc <= T; tc++)
 		{
-            int n = sc.nextInt();
-            int len = numberLen(n);
-            char[] nChar = String.valueOf(n).toCharArray();
-
-            int k = 2 * n;
-            String result = "impossible";
-            while(len == numberLen(k)){
-                char[] kChar = String.valueOf(k).toCharArray();
+            int num = sc.nextInt();
+			String origin = String.valueOf(num);
+            int i = 2;
+            String mul = String.valueOf(num * i);
+            boolean check = false;
+            
+            while(mul.length() == origin.length()){
+                check = true;
                 
-                boolean search = true;
-                for(int i = 0; i < len; i++){
-                    search = false;
-                    for(int j = 0; j < len; j++){
-                        if(nChar[i] == kChar[j]){
-                            kChar[j] = 'y';
-                            search = true;
-                            break;
-                        }
+                for(int idx = 0; idx < mul.length(); idx++){
+                    String ch = String.valueOf(mul.charAt(idx));
+                    if(!origin.contains(ch)){
+                        check = false;
+                        break;
                     }
-                    if(!search) break;
                 }
-                if(search) {
-                    result = "possible";
+                
+                if(check){
                     break;
+                }else{
+                    i++;
+                    mul = String.valueOf(num * i);
                 }
-                    
-                k += n;
             }
-            System.out.printf("#%d %s\n",tc,result);
+            System.out.println("#" + tc + " " + (check ? "possible" : "impossible"));
 		}
 	}
-    private static int numberLen(int n){
-        int count = 0;
-        while(n != 0){
-            n /= 10;
-            count++;
-        }
-        return count;
-    }
 }
