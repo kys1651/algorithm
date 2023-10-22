bb11014b75d09262b66c41d948368b60ec23cb6e
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-23 00:31:15 +0900

    [Silver II] Title: 최소 힙, Time: 364 ms, Memory: 27112 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                bw.write((pq.isEmpty() ? 0 : pq.poll()) + "\n");
+            } else {
+                pq.offer(x);
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
