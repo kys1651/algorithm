6010b7775ff65df04fae17c334175b866c3596c1
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-23 13:54:39 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 161 ms, Memory: 35,428 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -16,25 +16,13 @@ class Solution
             for(int i = 0; i < SIZE;i++){
                 boxes[i] = sc.nextInt();
             }
-            while(dumpCount-->0){
-                dump(boxes);
+            while( dumpCount-- > 0){
+                Arrays.sort(boxes);
+                boxes[0]++;
+                boxes[99]--;
             }
-            System.out.println("#" + tc + " "+(dump(boxes)+2));
+                Arrays.sort(boxes);
+            System.out.println("#" + tc + " " + (boxes[99] - boxes[0]));
 		}
 	}
-    private static int dump(int [] boxes){
-        int maxIdx = 0;
-        int minIdx = 0;
-		for(int i = 0; i < boxes.length; i++){
-            if(boxes[i] > boxes[maxIdx]){
-                maxIdx = i;
-            }
-            if(boxes[i] < boxes[minIdx]){
-                minIdx = i;
-            }
-        }
-        boxes[maxIdx]--;
-        boxes[minIdx]++;
-        return boxes[maxIdx] - boxes[minIdx];
-    }
 }
