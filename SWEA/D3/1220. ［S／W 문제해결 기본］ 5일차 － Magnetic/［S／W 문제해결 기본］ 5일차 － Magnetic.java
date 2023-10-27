462f4a296f8f9807b355f89d4e715539c291d93d
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-27 16:23:16 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 305 ms, Memory: 105,064 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 class Solution
 {
-    static int SIZE = 100;
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		int T = 10;
-		for(int tc = 1; tc <= T; tc++)
+		for(int tc = 1; tc <= 10; tc++)
 		{
-            sc.next();
-            int[][] map = new int[SIZE][SIZE];
-            for(int i = 0; i < SIZE; i ++){
-                for(int j = 0; j < SIZE; j++){
+            int N = sc.nextInt();
+            int[][] map = new int[N][N];
+            int result = 0;
+            for(int i = 0; i < N; i ++){
+                for(int j = 0; j < N; j++){
                     map[i][j] = sc.nextInt();
                 }
             }
-            int result = 0;
-            for(int i = 0; i < SIZE; i++){
+            for(int i = 0; i < N; i++){
                 int count = 0;
-                for(int j = 0; j < SIZE; j++){
+                for(int j = 0; j < N; j++){
                     if(map[j][i] == 1) count++;
-                    if(map[j][i] == 2 && count != 0){
-                        result++;
-                        count = 0;
-                    }
+                    if(map[j][i] == 2 && count != 0){result++;count = 0;}
                 }
             }
             System.out.println("#" + tc + " " + result);
