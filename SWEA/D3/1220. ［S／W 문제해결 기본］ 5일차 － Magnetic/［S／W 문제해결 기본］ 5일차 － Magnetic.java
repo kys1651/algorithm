2b1ed3fc5196922705a79da7ebf3fc5d5cc7f860
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-27 16:21:22 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 351 ms, Memory: 109,348 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -17,29 +17,18 @@ class Solution
                     map[i][j] = sc.nextInt();
                 }
             }
-            int result = check(map);
+            int result = 0;
+            for(int i = 0; i < SIZE; i++){
+                int count = 0;
+                for(int j = 0; j < SIZE; j++){
+                    if(map[j][i] == 1) count++;
+                    if(map[j][i] == 2 && count != 0){
+                        result++;
+                        count = 0;
+                    }
+                }
+            }
             System.out.println("#" + tc + " " + result);
         }
-    }
-    private static int check(int[][] map){
-        int result = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < SIZE; i++){
-            stack.clear();
-            for(int j = 0; j < SIZE; j ++){
-                if(map[j][i] == 0){
-                    continue;
-                }
-                if((stack.isEmpty() && map[j][i] == 2) || (!stack.isEmpty() && stack.peek() == map[j][i])){
-                    continue;
-                }
-                stack.push(map[j][i]);
-            }
-            while(!stack.isEmpty() && stack.peek() == 1){
-                stack.pop();
-            }
-            result += (stack.size()/ 2);
-        }
-        return result;
     }
 }
