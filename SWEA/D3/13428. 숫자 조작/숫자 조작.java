08ee3275c69b4053800131cfcea061961185edc9
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-28 18:45:40 +0900

    [D3] Title: 숫자 조작, Time: 163 ms, Memory: 22,180 KB -BaekjoonHub

--- SWEA/D3/13428. 숫자 조작/숫자 조작.java
+++ SWEA/D3/13428. 숫자 조작/숫자 조작.java
@@ -1,39 +1,29 @@
 import java.util.Scanner;
 class Solution
 {
-    static int max;
-    static int min;
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for(int tc = 1; tc <= T; tc++)
 		{
-            int n = sc.nextInt();
+            String n = sc.next();
             char[] strN = String.valueOf(n).toCharArray();
-            max = n;
-            min = n;
-            solution(0,0,strN);
+            int max = Integer.parseInt(n);
+            int min = Integer.parseInt(n);
+            for(int i = 0; i < n.length(); i++){
+                for(int j = i + 1; j < n.length(); j++){
+                    char[] numChar = n.toCharArray();
+                    swap(i,j,numChar);
+                    if(numChar[0] == '0') continue;
+                    int num = Integer.parseInt(new String(numChar));
+                    max = Math.max(num,max);
+                    min = Math.min(num,min);
+                }
+            }
             System.out.println("#" + tc + " " + min + " " + max);
 		}
 	}
-    private static void solution(int at,int count,char[] arr){
-        if(count == 1 || at == arr.length){
-            String tmp = new String(arr);
-            if(!tmp.startsWith("0")){
-                max = Math.max(max, Integer.valueOf(tmp));
-                min = Math.min(min, Integer.valueOf(tmp));
-            }
-            return;
-        }
-        for(int i = at; i < arr.length; i++){
-            swap(at,i,arr);
-            solution(i+1,count + 1,arr);
-            swap(at,i,arr);
-            solution(i+1,count,arr);
-        }
-    }
-    
     private static void swap(int i ,int j, char[] arr){
         char tmp = arr[i];
         arr[i] = arr[j];
