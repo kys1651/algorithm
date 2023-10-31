4a3feb089f9005f34a5e87fa4d838de5e97736c2
김용수 <43926186+kys1651@users.noreply.github.com>
2023-10-31 09:41:15 +0900

    [D3] Title: 안경이 없어!, Time: 472 ms, Memory: 118,164 KB -BaekjoonHub

--- SWEA/D3/7272. 안경이 없어！/안경이 없어！.java
+++ SWEA/D3/7272. 안경이 없어！/안경이 없어！.java
@@ -4,29 +4,11 @@ class Solution
 	public static void main(String args[]) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-        String noHole = "CEFGHIJKLMNSTUVWXYZ";
-        String oneHole = "ADOPQR";
 		for(int tc= 1; tc <= T; tc++)
 		{
-	        String a = sc.next();
-            String b = sc.next();
-            String result = "SAME";
-            if(a.length() != b.length()){
-                result = "DIFF";
-            }else{
-                for(int i = 0; i < a.length(); i++){
-                    String posA = String.valueOf(a.charAt(i));
-                    String posB = String.valueOf(b.charAt(i));
-                    if(posA.equals("B") && posB.equals("B")){
-                   }else if(noHole.contains(posA) && noHole.contains(posB)){
-                   }else if(oneHole.contains(posA) && oneHole.contains(posB)){
-                   }else{
-                       	result = "DIFF";
-                       	break;
-                    }
-               }
-           }
-            System.out.println("#" + tc + " " + result);
+	        String a = sc.next().replaceAll("[CEFGHIJKLMNSTUVWXYZ]","C").replaceAll("[ADOPQR]","A");
+            String b = sc.next().replaceAll("[CEFGHIJKLMNSTUVWXYZ]","C").replaceAll("[ADOPQR]","A");
+            System.out.println("#" + tc + " " + (a.equals(b)?"SAME":"DIFF"));
         }
 	}
 }
