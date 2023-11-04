ca7883ba90e7498a3975afbc99f948449950e760
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-04 22:12:45 +0900

    [D3] Title: 세상의 모든 팰린드롬, Time: 128 ms, Memory: 20,364 KB -BaekjoonHub

---
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/README.md
@@ -0,0 +1,15 @@
+# [D3] 세상의 모든 팰린드롬 - 4522 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWO6Oao6N4QDFAWw) 
+
+### 성능 요약
+
+메모리: 20,364 KB, 시간: 128 ms, 코드길이: 717 Bytes
+
+### 제출 일자
+
+2023-11-04 22:12
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4522. 세상의 모든 팰린드롬/세상의 모든 팰린드롬.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+            String text = sc.next();
+            String result = "Exist";
+            for(int i = 0; i < text.length()/2; i++){
+                char front = text.charAt(i);
+                char back = text.charAt(text.length() - i - 1);
+                if(front != back) {
+                    if(front != '?' && back != '?') {
+                    	result = "Not exist";
+	                    break;
+                    }
+                }
+            }
+            System.out.println("#" + tc + " " + result);
+		}
+	}
+}
