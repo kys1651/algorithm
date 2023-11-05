a38acaa6d4284142d24cba1749561c173edf6d3b
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-05 19:41:27 +0900

    [D3] Title: 코딩 토너먼트1, Time: 282 ms, Memory: 88,788 KB -BaekjoonHub

--- SWEA/D3/8673. 코딩 토너먼트1/코딩 토너먼트1.java
+++ SWEA/D3/8673. 코딩 토너먼트1/코딩 토너먼트1.java
@@ -5,21 +5,19 @@ class Solution {
  		int T = sc.nextInt();
 		for(int tc = 1; tc <= T; tc++) {
             int k = (int)Math.pow(2,sc.nextInt());
-            int sum = 0;
-            Queue<Integer> q = new LinkedList<>();
-            for(int i =0; i < k; i++){
-                q.offer(sc.nextInt());
+            int [] a= new int[k];
+            for(int i = 0; i < k; i++){
+                a[i] = sc.nextInt();
             }
-            while( k != 1){
-                for(int i = 0; i < k/2; i++){
-                    int a = q.poll();
-                    int b = q.poll();
-                    int max = Math.max(a,b);
-                    int min = Math.min(a,b);
-                    q.offer(max);
-                    sum += max - min;
+            int sum = 0;
+            while( k > 0 ){
+                for(int i = 0; i < k / 2; i++){
+                    int num1 = a[i*2];
+                    int num2 = a[i*2+1];
+                    sum += Math.abs(num1 - num2);
+                    a[i] = Math.max(num1, num2);
                 }
-                k/= 2;
+                k /= 2;
             }
             System.out.println("#" + tc + " " + sum);
 		}
