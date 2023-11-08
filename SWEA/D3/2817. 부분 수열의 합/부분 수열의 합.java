01aefb82a53f785349ce00e203eacfb82932b7f9
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-08 10:45:50 +0900

    [D3] Title: 부분 수열의 합, Time: 168 ms, Memory: 22,872 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 class Solution
 {
     static int n,k,result;
+    static boolean[] visit;
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-
 		for(int tc = 1; tc <= T; tc++)
 		{
 			n = sc.nextInt();
             k = sc.nextInt();
             result = 0;
             int[] num = new int[n];
+            visit = new boolean[n];
             for(int i = 0; i < n; i++){
                 num[i] = sc.nextInt();
             }
@@ -22,17 +23,16 @@ class Solution
 		}
 	}
     private static void solution(int depth, int sum, int[] num){
-        if(sum > k){
+        if(sum >= k){
+            if(sum == k) result++;
             return;
         }
-        if(depth == n){
-            if(sum == k){
-                result++;
-            }
-            return;
+        for(int i = depth; i < n; i++){
+            if(visit[i]) continue;
+            visit[i] = true;
+            solution(i,sum + num[i], num);
+            visit[i] = false;
         }
-        solution(depth+1,sum + num[depth],num);
-        solution(depth+1,sum,num);
     }
     
 }
