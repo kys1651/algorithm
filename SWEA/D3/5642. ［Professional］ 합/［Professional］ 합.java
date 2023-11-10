16235c8c49b275f1193b058a7dfa9c3d129c7175
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-10 15:05:21 +0900

    [D3] Title: [Professional] 합, Time: 1,167 ms, Memory: 128,748 KB -BaekjoonHub

--- SWEA/D3/5642. ［Professional］ 합/［Professional］ 합.java
+++ SWEA/D3/5642. ［Professional］ 합/［Professional］ 합.java
@@ -5,22 +5,20 @@ class Solution{
 		int T = sc.nextInt();
 		for(int tc = 1; tc <= T; tc++) {
             int n = sc.nextInt();
-            int[] nums = new int[n+1];
+            int[] nums = new int[n];
+            int[] dp = new int[n];
             for(int i = 0; i < n; i++){
                 nums[i] = sc.nextInt();
             }
-            int[][] dp = new int[n][2];
-            dp[0][0] = dp[0][1] = nums[0];
+
+            dp[0] = nums[0];
+            int max = dp[0];
             for(int i = 1; i < n; i++){
-                int current = dp[i-1][0] + nums[i];
-                dp[i][1] = Math.max(dp[i-1][1],current);
-                if(current <= 0){
-                    dp[i][0] = 0;
-                }else{
-                    dp[i][0] = current;
-                }
+                dp[i] = Math.max(dp[i-1] + nums[i] , nums[i]);
+                max = Math.max(dp[i], max);
             }
-            System.out.println("#" + tc + " " + dp[n-1][1]);
+            
+            System.out.println("#" + tc + " " + max);
 		}
 	}
 }
