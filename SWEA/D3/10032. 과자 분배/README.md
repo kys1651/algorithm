a1b9fe14f39e99b992788dc243f764f36c388bca
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-12 20:18:58 +0900

    [D3] Title: 과자 분배, Time: 305 ms, Memory: 52,244 KB -BaekjoonHub

---
+++ SWEA/D3/10032. 과자 분배/README.md
@@ -0,0 +1,15 @@
+# [D3] 과자 분배 - 10032 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXJZ6_6KCLcDFAU3) 
+
+### 성능 요약
+
+메모리: 52,244 KB, 시간: 305 ms, 코드길이: 347 Bytes
+
+### 제출 일자
+
+2023-11-12 20:18
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/10032. 과자 분배/과자 분배.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            System.out.println("#" + tc + " "  + (n % k == 0? 0 : 1));
+		}
+	}
+}
