b60126923014e6d38d3370dd108cceb48e6f723f
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-21 10:59:14 +0900

    [Gold IV] Title: 돌 그룹, Time: 196 ms, Memory: 50696 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -13,6 +13,7 @@ public class Main {
     }
 
     private static int bfs(int[] arr) {
+        // 합이 3으로 나누어지지 않는다면 결과는 0
         int sum = arr[0] + arr[1] + arr[2];
         if(sum % 3 != 0){
             return 0;
@@ -20,6 +21,7 @@ public class Main {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(arr);
 
+        // 방문하는 배열은 2차원 배열이여도 됨 why? 어차피 돌의 개수는 고정이니까 두개의 합 + 나머지의 합 -> 총합
         boolean[][] visit = new boolean[sum+1][sum+1];
         visit[arr[0]][arr[1]] = true;
 
@@ -34,6 +36,10 @@ public class Main {
                 return 1;
             }
 
+            /*
+                개수가 다르면 연산을 처리 후 방문을 했다고 남김(만약 방문한 곳을 또 방문(연산한 값을 또 연산)하는 것은 의미가 없기 때문에
+                방문 배열을 통해서 계산 안해본 경우만 계산 후 큐에 삽입
+             */
             // 갯수가 다른 연산
             if(x != y){
                 int nx = x > y ? x - y : x + x;
