15cf6242f991be1e6c0775d6186d1caab410ed10
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-24 10:01:19 +0900

    [Silver IV] Title: 카드, Time: 316 ms, Memory: 38388 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -8,21 +8,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashMap<Long,Integer> map = new HashMap<>();
         int n =Integer.parseInt(br.readLine());
+        int max = 0;
+        long result = 0;
         for(int i = 0; i < n; i++){
             long val = Long.parseLong(br.readLine());
             map.put(val,map.getOrDefault(val, 0)+1);
-        }
-        int max = 0;
-        long result = Long.MAX_VALUE;
-        for(long key :map.keySet()){
-            int count = map.get(key);
-            if(max <= count){
-                if(max == count){
-                    result = Math.min(key,result);
-                }else{
-                    result = key;
-                }
-                max = count;
+            if(map.get(val) > max){
+                max = map.get(val);
+                result = val;
+            }else if(map.get(val) == max){
+                result = Math.min(result,val);
             }
         }
         System.out.println(result);
