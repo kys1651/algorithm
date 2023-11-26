cea598f838b6518caaf6aa62049b4cd49a8c2b83
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-26 11:46:16 +0900

    [Silver I] Title: 곱셈, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,23 +1,34 @@
-import java.util.Scanner;
-class Main {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-        long a = sc.nextInt();
-        long b = sc.nextInt();
-        long c = sc.nextInt();
-        System.out.println(pow(a, b, c));
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    private static long pow(long base, long exponent, long mod){
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(POW(a,b,c));
+    }
+
+    private static long POW(long base, long exponent, long mod){
         if(exponent == 1){
             return base % mod;
         }
 
-        long tmp = pow(base, exponent / 2, mod);
-        
+        long tmp = POW(base,exponent / 2, mod);
+
+        // 홀수일 때 ex) 2^5 -> 2 * 2^2 * 2^2
         if(exponent % 2 == 1){
             return (tmp * tmp % mod) * base % mod;
         }
-        return tmp * tmp % mod;
+        // 짝수일 때 ex) 2^8 -> 2^4 * 2^4
+        else{
+            return tmp * tmp % mod;
+        }
+
     }
 }
