4c46fea8ba46bc999bbc3fe9f19b58d4b01611e4
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-27 09:49:45 +0900

    [Silver V] Title: 배열 합치기, Time: 1352 ms, Memory: 372060 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -13,32 +13,15 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int [] arr = new int[n+m];
-        int [] a = new int[n];
-        int [] b = new int[m];
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
-            a[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++){
-            b[i] = Integer.parseInt(st.nextToken());
+            arr[n+i] = Integer.parseInt(st.nextToken());
         }
-        int adx = 0;
-        int bdx = 0;
-        int idx = 0;
-        while(adx < n && bdx < m){
-            if(a[adx] < b[bdx]){
-                arr[idx++] = a[adx++];
-            }else{
-                arr[idx++] = b[bdx++];
-            }
-        }
-        for(;adx<n;adx++){
-            arr[idx++] = a[adx];
-        }
-        for(;bdx<m;bdx++){
-            arr[idx++] = b[bdx];
-        }
+        Arrays.sort(arr);
         for(int i = 0; i < arr.length; i++){
             sb.append(arr[i] + " ");
         }
