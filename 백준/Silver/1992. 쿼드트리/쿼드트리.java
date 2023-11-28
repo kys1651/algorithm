4fef96b93f2ba7b007cd55dc112a434ea109f6ba
김용수 <43926186+kys1651@users.noreply.github.com>
2023-11-28 09:39:05 +0900

    [Silver I] Title: 쿼드트리, Time: 84 ms, Memory: 12328 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -16,21 +16,27 @@ public class Main {
     }
 
     private static String recursion(int n, int x, int y) {
+        // (x,y)위치 값을 변수에 저장하여 확인
         char mark  = map[x][y];
+        // 전부가 같은지 아닌지 확인하는 flag
         boolean check = true;
         loop : for(int i = x; i < x + n; i++){
             for(int j = y;j < y + n; j++){
                 if(mark != map[i][j]){
+                    // 다르다면 for문 종료
                     check = false;
                     break loop;
                 }
             }
         }
-        if(check || n == 1){
+        // 같다면 ( n == 1처럼 쪼개질 수 없을 때도 포함) return 
+        if(check){
             return String.valueOf(mark);
         }
+        // 아니라면 분할한 값들을 괄호안에 씌어야함으로 ( 초기화
         StringBuilder sb = new StringBuilder("(");
 
+        // size로 나눠서 초기화 해줌
         int size = n / 2;
         for (int i = x; i < x + n; i += size) {
             for (int j = y; j < y + n; j += size) {
@@ -38,6 +44,7 @@ public class Main {
             }
         }
 
+        // ) 를 붙여서 리턴
         return sb.append(")").toString();
 
     }
