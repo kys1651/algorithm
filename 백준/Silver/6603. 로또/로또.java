e8adfc5712fe608015fd37f9d627d16a45529e28
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-16 14:26:25 +0900

    [Silver II] Title: 로또, Time: 92 ms, Memory: 13016 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,59 +1,50 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static  int[] result = new int[6];
-    static boolean[] visited;
-    static int K;
-    static StringBuilder sb = new StringBuilder();
+    static int[] nums;
+    static int[] result = new int[6];
+    static boolean[] visit;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        while(true){
+        sb = new StringBuilder();
+        result = new int[6];
+        while (true) {
             st = new StringTokenizer(br.readLine());
-
-            if((K = Integer.parseInt(st.nextToken()))==0)
+            int k = Integer.parseInt(st.nextToken());
+            if (k == 0) {
                 break;
-
-            arr = new int[K];
-            visited = new boolean[K];
-
-            for(int i = 0; i < K; i++){
-                int n = Integer.parseInt(st.nextToken());
-                arr[i] = n;
             }
 
-            solution(0,0);
+            visit = new boolean[k];
+            nums = new int[k];
+            for (int i = 0; i < k; i++) {
+                nums[i] = Integer.parseInt(st.nextToken());
+            }
+            dfs(0,0);
+
             sb.append("\n");
         }
-
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
-    private static void solution(int depth,int pos) {
-        if(depth == 6){
-            for(int i = 0 ; i < 6; i++){
+    private static void dfs(int depth,int pos) {
+        if (depth == 6) {
+            for (int i = 0; i < 6; i++) {
                 sb.append(result[i] + " ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = pos; i < K; i++) {
-            if(!visited[i]){
-                result[depth] = arr[i];
-                visited[i] = true;
-                solution(depth+1,i+1);
-                visited[i] = false;
-            }
+        for (int i = pos; i < nums.length; i++) {
+            result[depth] = nums[i];
+            dfs(depth + 1, i + 1);
         }
     }
-
-
 }
