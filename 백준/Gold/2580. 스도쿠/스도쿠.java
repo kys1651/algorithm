30ba7303e541107754a723015e2f6fb70e3fc8ae
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-17 16:20:35 +0900

    [Gold IV] Title: 스도쿠, Time: 368 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,80 +1,73 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] map = new int[9][9];
+    static class EmptyPoint{
+        int x;
+        int y;
 
+        public EmptyPoint(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    static int[][] map = new int[9][9];
+    static ArrayList<EmptyPoint> empty = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        for (int i = 0; i < 9; i++) {
+        for(int i = 0; i < 9; i++){
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 9; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 0){
+                    empty.add(new EmptyPoint(i, j));
+                }
             }
         }
-        sudoku(0,0);
+        dfs(0);
     }
 
-    private static void sudoku(int row, int col) {
-        if(col == 9){
-            sudoku(row + 1, 0);
-            return ;
-        }
-
-        if (row == 9) {
+    private static void dfs(int depth) {
+        if (depth == empty.size()) {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sb.append(map[i][j]).append(' ');
+            for(int i = 0; i < 9; i++){
+                for(int j = 0; j < 9; j++){
+                    sb.append(map[i][j] + " ");
                 }
-                sb.append('\n');
+                sb.append("\n");
             }
             System.out.println(sb);
             System.exit(0);
         }
 
-        if (map[row][col] == 0) {
-            for (int i = 1; i <= 9; i++) {
-                // 값 들어갈 수 있는지 검사
-                if (check(row, col, i)) {
-                    map[row][col] = i;
-                    sudoku(row, col + 1);
-                }
+        EmptyPoint pos = empty.get(depth);
+        for (int i = 1; i <= 9; i++) {
+            if (validationNumber(i, pos)) {
+                map[pos.x][pos.y] = i;
+                dfs(depth + 1);
+                map[pos.x][pos.y] = 0;
             }
-            map[row][col] = 0;
-            return;
         }
-
-        sudoku(row, col + 1);
-
     }
 
-    private static boolean check(int row, int col, int value) {
-
-        // 행 체크
-        for (int i = 0; i < 9; i++) {
-            if(map[row][i] == value) {
+    private static boolean validationNumber(int value, EmptyPoint pos) {
+        // 가로 줄, 세로 줄에 value값이 존재하는지 확인
+        for(int i = 0; i < 9; i++){
+            if (map[pos.x][i] == value || map[i][pos.y] == value) {
                 return false;
             }
         }
 
-        // 열 체크
-        for (int i = 0; i < 9; i++) {
-            if (map[i][col] == value) {
-                return false;
-            }
-        }
-
-        int rowCheck = (row / 3) * 3;
-        int colCheck = (col / 3) * 3;
-
-        for (int i = rowCheck; i < rowCheck + 3; i++) {
-            for (int j = colCheck; j < colCheck + 3; j++) {
-                if (map[i][j] == value) {
+        // 해당 위치 3 x 3 위치에 value값이 있는지 확인해줌
+        int x = (pos.x / 3) * 3;
+        int y = (pos.y / 3) * 3;
+        for(int i = x; i < x + 3; i++){
+            for(int j = y; j < y + 3; j++){
+                if(map[i][j] == value){
                     return false;
                 }
             }
