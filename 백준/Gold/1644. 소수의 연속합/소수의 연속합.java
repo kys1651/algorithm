3f730d59708ac7561fccbfbfab7744a7179e0326
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-17 16:43:25 +0900

    [Gold III] Title: 소수의 연속합, Time: 184 ms, Memory: 24836 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,62 +1,46 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer> list = new ArrayList<>();
-    static int n;
+    static boolean[] prime;
+    static ArrayList<Integer> primeNumber = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        n = Integer.parseInt(br.readLine());
-        getPrime();
-
-        int left = 0, right = 0;
-        int ans = 0, sum = 0;
-        int size = list.size();
-
-        while(true){
-            if(sum >= n){
-                sum -= list.get(left++);
-            }else if(right == size){
-                break;
-            }else {
-                sum += list.get(right++);
-            }
-            if(sum == n) ans ++;
-        }
-        System.out.println(ans);
-
-    }
-
-    private static void getPrime() {
-        int[] tmp = new int[n + 1];
-        int root = (int) Math.sqrt(n);
-
-        for (int i = 2; i <= n; i++) {
-            tmp[i] = i;
-        }
-
-        for (int i = 2; i <= root; i++) {
-            if(tmp[i] == 0) continue;
-
-            for (int j = i * 2; j <= n; j += i) {
-                tmp[j] = 0;
+        int n = Integer.parseInt(br.readLine());
+        getPrime(n);
+        int count = 0;
+        for(int i = 0; i < primeNumber.size(); i++){
+            // 시작하는 숫자가 절반보다 크다면 종료
+            int sum = primeNumber.get(i);
+            if(sum > n / 2) break;
+            for(int j = i + 1; j < primeNumber.size(); j++){
+                sum += primeNumber.get(j);
+                if(sum == n) count ++;
+                else if(sum > n){
+                    break;
+                }
             }
         }
-
-        for (int i = 2; i <= n; i++) {
-            if(tmp[i] == 0) continue;
-
-            list.add(tmp[i]);
-        }
-
+        // n이 소수인지 확인 -> 하나만으로 구성이 가능하기 때문에
+        if(!prime[n]) count++;
+        System.out.println(count);
     }
 
-
+    private static void getPrime(int n) {
+        prime = new boolean[n + 1];
+        prime[0] = prime[1] = true;
+        for(int i = 2 ; i <= Math.sqrt(n); i++){
+            if(prime[i]) continue;
+            for(int j = 2 * i; j <= n; j+= i){
+                prime[j] = true;
+            }
+        }
+        for(int i = 2; i <= n; i++){
+            if(!prime[i]){
+                primeNumber.add(i);
+            }
+        }
+    }
 }
