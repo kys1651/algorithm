02aab63c68e70bd62c4bab6262ea6da7c0375aae
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-18 16:00:55 +0900

    [Gold III] Title: 환상의 짝꿍, Time: 928 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Gold/15711. 환상의 짝꿍/환상의 짝꿍.java
+++ 백준/Gold/15711. 환상의 짝꿍/환상의 짝꿍.java
@@ -1,74 +1,57 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean prime[] = new boolean[2000001];
-    static List<Integer> list = new ArrayList<>();
-
+    static boolean[] prime = new boolean[2000001];
+    static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-
+        int n = Integer.parseInt(br.readLine());
         getPrime();
-
-        while (T -- > 0) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            long a = Long.parseLong(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
-            long sum = a + b;
-
+            long sum = Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken());
             if(sum < 4){
-                sb.append("NO").append('\n');
-            } else if (sum % 2 == 0) {
-                sb.append("YES").append('\n');
-            } else {
-                // 홀수가 되기위해서는 짝수 + 홀수가 되어야만 함
-                // 짝수인 소수는 2밖에 없으므로 sum에서 2를 뺀 뒤 소수인지 확인
-                if (check(sum - 2)) {
-                    // 소수일 때
-                    sb.append("YES").append('\n');
-                } else {
-                    sb.append("NO").append('\n');
+                sb.append("NO");
+            }else if(sum % 2 == 0){
+                sb.append("YES");
+            }else{
+                if(checkPrime(sum-2)){
+                    sb.append("YES");
+                }else{
+                    sb.append("NO");
                 }
             }
+            sb.append("\n");
         }
-
         System.out.println(sb);
-    }
-
-    private static boolean check(long num) {
-        if (num <= 2000000) { // 4 * 10^ 12의 루트인 2000000보다 작은 경우
-            return !prime[(int) num];
-        }
-
-        // 큰 경우
-        for (int i = 0; i < list.size(); i++) {
-            // 소수로 나누어진다면 그것은 소수가 아님
-            if (num % list.get(i) == 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     private static void getPrime() {
         prime[0] = prime[1] = true;
-
         for (int i = 2; i < prime.length; i++) {
             if(prime[i]) continue;
 
             list.add(i);
-            for (int j = i * 2; j < prime.length; j+= i) {
+            for (int j = 2 * i; j < prime.length; j += i) {
                 prime[j] = true;
             }
         }
     }
 
+    private static boolean checkPrime(long sum) {
+        if(sum <= prime.length) return !prime[(int)sum];
 
+        for(int i = 0; i < list.size(); i++){
+            if(sum % list.get(i) == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
