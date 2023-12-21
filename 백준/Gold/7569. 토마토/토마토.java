67fe9fe06ac5303b1882c37bb779e20fc7b0b3b4
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-21 14:29:58 +0900

    [Gold V] Title: 토마토, Time: 784 ms, Memory: 122128 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,92 +1,95 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-
-class tomato{
-    int x;
-    int y;
-    int h;
-
-    public tomato(int h, int x, int y) {
-        this.h= h;
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static class Tomato {
+        int x;
+        int y;
+        int h;
+
+        public Tomato(int h, int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.h = h;
+        }
+    }
+
+    static int n, m, h;
     static int[][][] map;
-    static int M,N,H;//가로 세로 높이
-    static int[] dirx = {1,-1,0,0,0,0}; // 상,하
-    static int[] diry = {0,0,-1,1,0,0}; // 좌,우
-    static int[] dirh = {0,0,0,0,1,-1}; // 위,아래
-    static Queue<tomato> q = new LinkedList<>();
+    static Queue<Tomato> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        map = new int[H][N][M];
-
-        for(int h = 0; h < H; h++){
-            for (int n = 0; n < N; n++) {
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        map = new int[h][n][m];
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
                 st = new StringTokenizer(br.readLine());
-                for (int m = 0; m < M; m++) {
-                    map[h][n][m] = Integer.parseInt(st.nextToken());
-                    if(map[h][n][m] == 1) q.add(new tomato(h, n, m));
+                for (int k = 0; k < map[i][j].length; k++) {
+                    map[i][j][k] = Integer.parseInt(st.nextToken());
+                    if (map[i][j][k] == 1) {
+                        queue.offer(new Tomato(i, j, k));
+                    }
                 }
             }
         }
-        int result = bfs();
-        System.out.println(result);
+        bfs();
+        int result = 0;
+        loop:
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                for (int k = 0; k < map[i][j].length; k++) {
+                    if (map[i][j][k] == 0) {
+                        result = 0;
+                        break loop;
+                    } else {
+                        result = Math.max(result, map[i][j][k]);
+                    }
+                }
+            }
+        }
+        System.out.println(result - 1);
     }
 
-    private static int bfs() {
+    private static void bfs() {
+        int[] dirH = {0, 0, 0, 0, -1, 1};
+        int[] dirX = {1, -1, 0, 0, 0, 0};
+        int[] dirY = {0, 0, -1, 1, 0, 0};
+        while (!queue.isEmpty()) {
+            int size = queue.size();
 
-        while (!q.isEmpty()) {
-            tomato t = q.poll();
+            for (int s = 0; s < size; s++) {
+                Tomato curTomato = queue.poll();
 
-            int h = t.h;
-            int y = t.y;
-            int x = t.x;
+                for (int d = 0; d < 6; d++) {
+                    int nextH = curTomato.h + dirH[d];
+                    int nextX = curTomato.x + dirX[d];
+                    int nextY = curTomato.y + dirY[d];
 
-            for (int i = 0; i < 6; i++) {
-                int nx = x + dirx[i]; // 세로
-                int ny = y + diry[i]; // 가로
-                int nh = h + dirh[i]; // 높이
+                    // 상자의 크기를 벗어나는 경우
+                    if (check(nextH, nextX, nextY)) {
+                        continue;
+                    }
 
-                // 유효한 범위 내
-                if (nx >= 0 && ny >= 0 && nh >= 0 && nx < N && ny < M && nh < H) {
-                    if(map[nh][nx][ny]==0){
-                        q.offer(new tomato(nh, nx, ny));
-                        map[nh][nx][ny] = map[h][x][y] + 1;
+                    // 다음 좌표가 익지않는 토마토인 경우
+                    if (map[nextH][nextX][nextY] == 0) {
+                        map[nextH][nextX][nextY] = map[curTomato.h][curTomato.x][curTomato.y] + 1;
+                        queue.offer(new Tomato(nextH, nextX, nextY));
                     }
                 }
 
             }
         }
+    }
 
-        int result = -1;
-
-        for (int h = 0; h < H; h++) {
-            for (int x = 0; x < N; x++) {
-                for (int y = 0; y < M; y++) {
-                    if(map[h][x][y] == 0) {
-                        return -1;
-                    }
-
-                    result = Math.max(result, map[h][x][y]);
-                }
-            }
-        }
-        if(result == 1) return 0;
-        return result-1;
+    private static boolean check(int nZ, int nX, int nY) {
+        return nZ < 0 || nZ >= map.length || nX < 0 || nX >= map[nZ].length || nY < 0 || nY >= map[nZ][nX].length;
     }
 }
