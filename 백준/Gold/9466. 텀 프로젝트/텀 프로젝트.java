0b88a893b0e0cc78751f1f2fd2437f87ffdd1e1c
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-23 18:48:56 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1088 ms, Memory: 300236 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -16,31 +16,21 @@ public class Main {
         int TC = Integer.parseInt(br.readLine());
         for (int t = 0; t < TC; t++) {
             n = Integer.parseInt(br.readLine());
-            answer = 0;
 
-            // 싸이클이 생성된 결과를 저장하는 result 배열
+            // 방문여부를 저장하는 visit 배열
             visit = new boolean[n + 1];
-            // 방문한 경로를 저장하는 visit 배열
+            // 싸이클 여부를 저장하는 finish 배열
             finish = new boolean[n + 1];
             // 학생들이 선택한 학생을 저장하는 students 배열
             students = new int[n + 1];
+            answer = 0;
+
             st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= n; i++) {
                 students[i] = Integer.parseInt(st.nextToken());
             }
-            // 자기 자신을 가르키는 학생을 먼저 처리해준다.
             for (int i = 1; i <= n; i++) {
-                if (i == students[i]) {
-                    // 처리 완료
-                    finish[i] = visit[i] = true;
-                    // 구성원에 있으므로 더해준다.
-                    answer++;
-                }
-            }
-
-            for (int i = 1; i <= n; i++) {
-                // 방문한 적 있는 학생이라면 굳이 dfs 할 필요 없다.
-                if (!visit[i]) {
+                if(!visit[i] || !finish[i]){
                     dfs(i);
                 }
             }
