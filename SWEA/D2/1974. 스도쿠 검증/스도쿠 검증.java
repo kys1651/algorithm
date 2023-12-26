41be5ee62c7ae776ce86a6493ae089f8b0049c37
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-26 20:00:16 +0900

    [D2] Title: 스도쿠 검증, Time: 132 ms, Memory: 21,280 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -3,12 +3,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 class Solution {
     static int[][] map;
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++) {
+        for(int tc = 1; tc <= T; tc++) {
             map = new int[9][9];
             for(int i = 0; i < 9; i++){
                 st = new StringTokenizer(br.readLine());
@@ -17,41 +17,41 @@ class Solution {
                 }
             }
             sb.append("#" + tc + " " + sudoku()).append("\n");
-		}
+        }
         System.out.println(sb);
-	}
+    }
     private static int sudoku(){
+        boolean[] visit1,visit2;
+        // 한번에 가로 세로 확인하기
         for(int i = 0; i < 9; i++){
+            visit1 = new boolean[10];
+            visit2 = new boolean[10];
             for(int j = 0; j < 9; j++){
-                if(!check(i,j)){
+                int value1 = map[i][j];
+                int value2 = map[j][i];
+                if(visit1[value1] || visit2[value2]) return 0;
+                visit1[value1] = visit2[value2] = true;            
+            }
+        }
+        // 분면 확인하기
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(!check(i * 3, j * 3))
                     return 0;
-                }
             }
         }
         return 1;
     }
+
     private static boolean check(int x, int y){
-        // 가로 세로 확인
-        for(int i = 0; i < 9; i++){
-            if(map[x][i] == map[x][y] && i != y) {
-                return false;
-            }
-            if(map[i][y] == map[x][y] && i != x) {
-                return false;
-            }
-        }
-        
-        // 분면 확인
-        int checkX = (x / 3) * 3;
-        int checkY = (y / 3) * 3;
-        for(int i = checkX; i < checkX + 3; i++){
-            for(int j = checkY; j < checkY + 3; j++){
-                if(map[i][j] == map[x][y] && (i != x || j != y)) {
-                    return false;
-                }
+        boolean[] visit = new boolean[10];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                int value = map[i][j];
+                if(visit[value]) return false;
+                else visit[value] = true;
             }
         }
-        
         return true;
     }
 }
