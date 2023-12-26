cd5f9cb2134474da0120ec8fe1a83eeedc0b414a
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-26 18:27:06 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 206 ms, Memory: 41,636 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,33 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-        int[] nums;
-        int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int why = sc.nextInt();
-            nums = new int[101];
+class Solution {
+	public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;        
+        int[] num;
+        
+        int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+            br.readLine();
+            num = new int[101];
 
-            int maxIdx = 0;
-            for(int i = 0; i < 1000; i++){
-                int N = sc.nextInt();
-                nums[N]++;
-                
-				int currentValue = nums[N];
-                int maxValue = nums[maxIdx];
-				                		
-                if(currentValue > maxValue){
-                    maxIdx = N;
-                }else if(currentValue == maxValue){
-                    maxIdx = Math.max(N, maxIdx);
+            st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < 1000; i++){
+                int idx = Integer.parseInt(st.nextToken());
+                num[idx]++;
+            }
+            int max = 0;
+            for(int i = 1; i < num.length; i++){
+                if(num[max] <= num[i]){
+                    max = i;
                 }
             }
-            System.out.printf("#%d %d\n",tc,maxIdx);
+            sb.append("#" + tc + " " + max).append("\n");
 		}
+        System.out.println(sb);
 	}
 }
