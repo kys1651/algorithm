030d38779c4b769ffd5db0ad11edb04b8efe904a
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-26 19:49:53 +0900

    [D2] Title: 스도쿠 검증, Time: 132 ms, Memory: 21,280 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,60 +1,57 @@
-import java.util.*;
-import java.io.*;
-
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[][] map;
-        int SIZE = 9;
-		for(int tc = 1; tc <= T; tc++)
-		{
-            map = new int[SIZE][SIZE];
-            for(int i = 0; i < SIZE; i++){
-            	for(int j = 0; j < SIZE; j++){
-                    map[i][j] = sc.nextInt();
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class Solution {
+    static int[][] map;
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+            map = new int[9][9];
+            for(int i = 0; i < 9; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < 9; j++){
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            System.out.print("#"+tc +" ");
-			if(checkRowCol(map)&&checkSquare(map)){
-                System.out.println("1");
-            }else{
-                System.out.println("0");
+            sb.append("#" + tc + " " + sudoku()).append("\n");
+		}
+        System.out.println(sb);
+	}
+    private static int sudoku(){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(!check(i,j)){
+                    return 0;
+                }
             }
         }
+        return 1;
     }
-    private static boolean checkRowCol(int[][] map){
+    private static boolean check(int x, int y){
+        // 가로 세로 확인
         for(int i = 0; i < 9; i++){
-            int sumRow = 0;
-            int sumCol =0;
-            for(int j = 0; j < 9; j++){
-                sumRow += map[i][j];
-                sumCol += map[j][i];
+            if(map[x][i] == map[x][y] && i != y) {
+                return false;
             }
-            if(sumRow != 45 || sumCol != 45){
+            if(map[i][y] == map[x][y] && i != x) {
                 return false;
             }
         }
-        return true;
-    }
-    
-    private static boolean checkSquare(int[][] map){
-        for(int i = 0; i < 9; i+= 3){
-            for(int j = 0; j < 9 ; j += 3){
-                int sum = 0;
-                for(int x = 0; x < 3; x++){
-                    for(int y = 0 ; y < 3; y++){
-                        sum += map[i+x][j+y];
-                    }
-                }
-                if(sum != 45){
+        
+        // 분면 확인
+        int checkX = (x / 3) * 3;
+        int checkY = (y / 3) * 3;
+        for(int i = checkX; i < checkX + 3; i++){
+            for(int j = checkY; j < checkY + 3; j++){
+                if(map[i][j] == map[x][y] && (i != x || j != y)) {
                     return false;
                 }
             }
         }
+        
         return true;
     }
 }
