f53dc48716b48f12302b44a87b658d479151ae23
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-30 02:43:03 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder2, Time: 144 ms, Memory: 31,400 KB -BaekjoonHub

--- SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.java
+++ SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.java
@@ -13,17 +13,18 @@ class Solution {
         int T = 10;
         for (int tc = 1; tc <= T; tc++) {
             br.readLine();
-            // 사다리
+            // 사다리 배열
             map = new int[100][100];
-
             for (int i = 0; i < map.length; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < map.length; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+            // distance : 최소값을 구하기 위해 최대값으로 초기화
             distance = Integer.MAX_VALUE;
             answer = 0;
+            // 출발 지점 마주치면 시작
             for (int i = 0; i < map.length; i++) {
                 if (map[0][i] == 1) {
                     dfs(i);
@@ -35,25 +36,33 @@ class Solution {
     }
 
     private static void dfs(int start) {
+        // (0,start)에서 시작
         int x = 0;
         int y = start;
+        // 총 이동거리를 저장 할 count
         int count = 0;
+        // map 거리만큼 이동
         while (x < map.length) {
+            // 왼쪽에 사다리 존재시 쭉 이동한다.
             if( y - 1 >= 0 && map[x][y-1] == 1){
                 while(y - 1 >= 0 && map[x][y-1] == 1){
                     y--;
                     count++;
                 }
-            } else if (y + 1 < map.length && map[x][y + 1] == 1) {
+            }
+            // 오른쪽에 사다리 존재시 쭉 이동해준다.
+            else if (y + 1 < map.length && map[x][y + 1] == 1) {
                 while (y + 1 < map.length && map[x][y + 1] == 1) {
                     count++;
                     y++;
                 }
             }
+            // 이동 후 한칸 내려감과 동시에 count++
             x++;
             count++;
         }
 
+        // distance 갱신 가능하다면 해줌.
         if (distance >= count) {
             distance = count;
             answer = start;
