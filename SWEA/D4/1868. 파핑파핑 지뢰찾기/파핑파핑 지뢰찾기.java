129983b6939c921c0d861d1533a285dcfe0afd08
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-29 15:54:05 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 314 ms, Memory: 95,104 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,64 +1,84 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Solution {
-	static char[][] map;
-	static int n;
-	static int result;
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int tc = 1; tc <= T; tc++) {
-			n = sc.nextInt();
-			result = 0;
-			map = new char[n][n];
-			for (int i = 0; i < n; i++) {
-				map[i] = sc.next().toCharArray();
-			}
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (map[i][j] == '.') {
-						check(i, j, false);
-					}
-				}
-			}
-			for(int i = 0; i < n; i++) {
-				for(int j = 0; j < n; j++) {
-					if(map[i][j] == '.') result++;
-				}
-			}
-			System.out.println("#" + tc + " " + result);
-		}
-	}
+    static char[][] map;
+    // 상하좌우,왼쪽(위,아래),오른쪽(위,아래)
+    static int[] dirX = {-1, 1, 0, 0, -1, 1, -1, 1};
+    static int[] dirY = {0, 0, -1, 1, -1, -1, 1, 1};
 
-	private static void check(int x, int y, boolean call) {
-		int[] dirX = { -1, 1, 0, 0, -1, -1, 1, 1 };
-		int[] dirY = { 0, 0, -1, 1, -1, 1, -1, 1 };
-		int count = 0;
-		for (int i = 0; i < dirX.length; i++) {
-			int nx = x + dirX[i];
-			int ny = y + dirY[i];
-			// 범위 밖이면 신경 쓸 일 없
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
-				continue;
-			}
-			if (map[nx][ny] == '*') {
-				count++;
-			}
-		}
-		if (count == 0) {
-			map[x][y] = 0;
-			for (int i = 0; i < dirX.length; i++) {
-				int nx = x + dirX[i];
-				int ny = y + dirY[i];
-				if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
-					continue;
-				}
-				if(map[nx][ny] == '.') check(nx,ny,true);
-			}
-			if(!call) result++;
-		}else if(call) {
-			map[x][y] = (char)(count + '0');
-		}
-	}
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+
+            map = new char[n][n];
+            for (int i = 0; i < n; i++) {
+                String tmp = br.readLine();
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = tmp.charAt(j);
+                }
+            }
+
+            int result = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (map[i][j] == '.' && countMine(i, j, false)) {
+                        result++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (map[i][j] == '.')
+                        result++;
+                }
+            }
+
+            sb.append("#" + tc + " " + result).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean countMine(int x, int y, boolean mem) {
+        Queue<int[]> queue = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < dirX.length; i++) {
+            int nX = x + dirX[i];
+            int nY = y + dirY[i];
+
+            if (check(nX, nY)) {
+                continue;
+            }
+
+            if(map[nX][nY] == '*'){
+                count++;
+            } else if (map[nX][nY] == '.') {
+                queue.offer(new int[]{nX, nY});
+            }
+        }
+
+        if (count == 0) {
+            map[x][y] = '0';
+            while (!queue.isEmpty()) {
+                int[] cur = queue.poll();
+                countMine(cur[0], cur[1], true);
+            }
+            return true;
+        }
+        if (mem) {
+            map[x][y] = (char) ('0' + count);
+        }
+        return false;
+    }
+
+    private static boolean check(int x, int y) {
+        return x < 0 || x >= map.length || y < 0 || y >= map[x].length;
+    }
 }
