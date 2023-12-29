f4e3e6d2ad338faabed255a73421fd5ca51f1413
김용수 <43926186+kys1651@users.noreply.github.com>
2023-12-30 00:59:24 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 197 ms, Memory: 41,160 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,50 +1,66 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 class Solution {
-	static int[][] map;
-	static int[][] value;
-	static int n;
+    // 지도를 저장 할 map
+    static int[][] map;
+    // 방문하는데 필요한 시간이 저장 될 visit
+    static int[][] visit;
+    // 상하좌우
+    static int[] dirX = {-1,1,0,0};
+    static int[] dirY ={0,0,-1,1};
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            map = new int[n][n];
+            visit = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String line = br.readLine();
+                for(int j = 0; j < n; j++){
+                    map[i][j] = line.charAt(j) - '0';
+                    // visit 배열은 0값을 가질 수 있으므로 -1로 초기화해준다.
+                    visit[i][j] = -1;
+                }
+            }
+            bfs();
+            sb.append("#" + tc + " " + visit[n-1][n-1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    private static void bfs(){
+        Queue<int[]> queue = new LinkedList<>();
+        // 시작 지점 저장 후 0 넣어줌
+        queue.offer(new int[]{0, 0});
+        visit[0][0] = 0;
 
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int tc = 1; tc <= T; tc++) {
-			n = sc.nextInt();
-			map = new int[n][n];
-			value = new int[n][n];
-			for (int i = 0; i < n; i++) {
-				String line = sc.next();
-				for (int j = 0; j < n; j++) {
-					map[i][j] = line.charAt(j) - '0';
-					value[i][j] = -1;
-				}
-			}
-			bfs();
-			System.out.println("#" + tc + " " + value[n-1][n-1]);
-		}
-	}
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int curTime = visit[cur[0]][cur[1]];
 
-	private static void bfs() {
-		int[] dirX = {-1,1,0,0};
-		int[] dirY = {0,0,-1,1};
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {0,0});
-		value[0][0] = 0;
-		
-		while(!queue.isEmpty()) {
-			int[] pos = queue.poll();
-			int curX = pos[0];
-			int curY = pos[1];
-			for(int i = 0; i < 4; i++) {
-				int nX= curX+ dirX[i];
-				int nY = curY + dirY[i];
-				if(nX < 0 || nX >= n || nY < 0 || nY >= n) {
-					continue;
-				}
-				if(value[nX][nY] == -1 || (value[nX][nY] > value[curX][curY] + map[nX][nY])) {
-					value[nX][nY] = value[curX][curY] + map[nX][nY];
-					queue.offer(new int[] {nX,nY});
-				}
-			}
-		}
-	}
+            for(int i = 0; i < dirX.length; i++){
+                int nX = cur[0] + dirX[i];
+                int nY = cur[1] + dirY[i];
+
+                // 범위밖인 경우
+                if(check(nX, nY)){
+                    continue;
+                }
+
+                // 처음 방문했거나 기존 방문시간보다 작은 경로 복구 시간을 가질 경우
+                if(visit[nX][nY] == -1 || (visit[nX][nY] > curTime + map[nX][nY])){
+                    visit[nX][nY] = curTime + map[nX][nY];
+                    queue.offer(new int[]{nX, nY});
+                }
+            }
+        }
+    }
+
+    private static boolean check(int nX, int nY) {
+        return nX < 0 || nX >= map.length || nY < 0 || nY >= map[nX].length;
+    }
 }
