58c194ede64776c041f6aea2708334218d2cef36
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-02 00:00:23 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 102 ms, Memory: 18,380 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,13 +1,32 @@
-import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		for(int tc = 1; tc <= 10; tc++)
-		{
-            System.out.println("#" + sc.nextInt() + " " +(int)Math.pow(sc.nextInt(),sc.nextInt()));
+class Solution {
+	public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+		for(int tc = 1; tc <= 10; tc++) {
+            br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append("#" + tc + " " + pow(n,m)).append("\n");
 		}
+        System.out.println(sb);
 	}
+    private static int pow(int base, int exponent){
+        int result = 1;
+
+        while(exponent != 0){
+            if(exponent % 2 != 0){
+                result *= base;
+                exponent -= 1;
+            }
+            base *= base;
+            exponent /= 2;
+            }
+        
+        return result;
+    }
 }
