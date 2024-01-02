cffd421698b15490697b8f47acca73673dcf0b53
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-02 21:54:14 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 108 ms, Memory: 18,376 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.java
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.java
@@ -1,34 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Stack;
+
 class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		for(int tc = 1; tc <= 10; tc++) {
-            sc.nextInt();
-            Stack<Integer> nums = new Stack<>();
-            Stack<Character> op = new Stack<>();
-            String line = sc.next();
-            int result = 0;
-            for(char ch : line.toCharArray()){
-                if(ch == '+' || ch == '*'){
-                    if(ch == '+' && !op.isEmpty() && op.peek() == '+'){
-                        nums.push(nums.pop() + nums.pop());
-                    }else{
-                        op.push(ch);
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= 10; tc++) {
+            br.readLine();
+            String line = br.readLine();
+            Stack<Character> operator = new Stack<>();
+            Stack<Integer> num = new Stack<>();
+            for (int i = 0; i < line.length(); i++) {
+                char ch = line.charAt(i);
+                // 숫자인 경우 곱셈이라면 바로 곱해줌 아니면 스택에 넣는다.
+                if (Character.isDigit(ch)) {
+                    if (!operator.isEmpty() && operator.peek() == '*') {
+                        num.push(num.pop() * (ch - '0'));
+                        operator.pop();
+                    } else {
+                        num.push(ch - '0');
                     }
-                }else{
-                    int num = ch - '0';
-                    if(!op.isEmpty() && op.peek() == '*'){
-                        nums.push(nums.pop() * num);
-                        op.pop();
-                    }else{
-                        nums.push(num);
+                }
+                // 연산인 경우
+                else {
+                    // +가 두개면 숫자 두개를 pop해서 더해준뒤 다시 Push
+                    if (ch == '+' && !operator.isEmpty() && operator.peek() == '+') {
+                        num.push(num.pop() + num.pop());
+                    } else {
+                        operator.push(ch);
                     }
                 }
             }
-            result = nums.pop();
-            if(!op.isEmpty()){result += nums.pop();}
-            System.out.println("#" + tc + " " + result);
+            int result = num.pop();
+            if(!operator.isEmpty()){
+                result += num.pop();
+            }
+            sb.append("#" + tc + " " + result).append("\n");
         }
-	}
+        System.out.println(sb);
+    }
 }
