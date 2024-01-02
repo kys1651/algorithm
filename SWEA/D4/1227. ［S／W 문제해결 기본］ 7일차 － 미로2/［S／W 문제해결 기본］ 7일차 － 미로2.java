dd00acd68f969dbe842784d172f0570571171def
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-02 23:22:11 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 148 ms, Memory: 24,708 KB -BaekjoonHub

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -4,25 +4,34 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 class Solution {
+    // 미로를 저장 할 Map
     static int[][] map;
+    // 방문 여부를 저장 할 visit
     static boolean[][] visit;
+    // 방향을 저장하는 배열
     static int[] dirX = {-1,1,0,0};
     static int[] dirY = {0,0,-1,1};
+    // 시작지점과 목표 지점
     static int[] start,end;
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         for(int tc = 1; tc <= 10; tc++) {
             br.readLine();
+
+            // 100으로 초기화해준다.
             map = new int[100][100];
             visit = new boolean[100][100];
             for(int i = 0; i < 100; i++){
                 String line = br.readLine();
                 for(int j = 0; j < 100; j++){
                     map[i][j] = line.charAt(j) - '0';
+                    // 저장을 하면서 시작 지점이면 start에 저장
                     if(map[i][j] == 2){
                         start = new int[]{i,j};
-                    }else if(map[i][j] == 3){
+                    }
+                    // 목표 지점이라면 end에 저장
+                    if(map[i][j] == 3){
                         end = new int[]{i,j};
                     }
                 }
@@ -33,10 +42,11 @@ class Solution {
     }
 
     private static boolean bfs(){
+        // bfs를 위해 큐 선언
         Queue<int []> queue = new LinkedList<>();
+        // 시작 지점 큐에 삽입 및 방문 처리
         queue.offer(start);
         visit[start[0]][start[1]] = true;
-
         while(!queue.isEmpty()){
             int[] ns = queue.poll();
             int x = ns[0];
@@ -46,20 +56,19 @@ class Solution {
                 int nX = x + dirX[i];
                 int nY = y + dirY[i];
 
-                if(check(nX, nY) || map[nX][nY] == 1 || visit[nX][nY]){
+                // 벽이거나 Or 배열 밖이라면 continue (테두리는 벽이므로 처리 안해도 됨)
+                if(map[nX][nY] == 1 || visit[nX][nY]){
                     continue;
                 }
 
                 if(nX == end[0] && nY == end[1]){
                     return true;
                 }
+                // 방문 처리 후 큐에 삽입
                 visit[nX][nY] = true;
                 queue.offer(new int[] { nX, nY});
             }
         }
         return false;
-    }
-    private static boolean check(int x, int y){
-        return x < 0 || x > 100 || y < 0 || y > 100;
     }
 }
