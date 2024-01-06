f4b8cb8cde73b99e20994ee3b6776e156501dd61
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-06 17:03:43 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 80 ms, Memory: 11636 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                sb.append(" ");
+            }
+            for (int j = 0; j < (n - i - 1) * 2 + 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
