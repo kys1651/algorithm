461180bbe72063580e2664f90f8cbc2151087af4
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-12 17:07:35 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 272 ms, Memory: 68684 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,55 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static char[][] map;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        map = new char[n][n];
-        print(n,0,0,false);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                sb.append(map[i][j]);
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
+class Main{
+	static char[][] map;
+    public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+    	map = new char[n][n];
+    	
+    	recursion(0,0,0,n);
+    	for(int i = 0; i < map.length; i++) {
+    		for(int j = 0; j < map[i].length; j++) {
+    			sb.append(map[i][j]);
+    		}
+    		sb.append("\n");
+    	}
+    	System.out.println(sb);
     }
-
-    private static void print(int n,int x,int y,boolean space) {
-        if(space){
-            for(int i = x; i < x+n; i ++){
-                for (int j = y; j < y + n; j++) {
-                    map[i][j] = ' ';
-                }
-            }
-            return;
-        }
-
-        if(n == 1){
-            map[x][y] = '*';
-            return;
-        }
-
-
-
-
-        int size = n/3;
-        int count = 0;
-        for(int i = x; i < x + n; i += size){
-            for (int j = y; j < y + n; j += size) {
-                count++;
-                if(count == 5){
-                    print(size, i, j, true);
-                }else{
-                    print(size,i,j,false);
-                }
-            }
-        }
+    private static void recursion(int x,int y, int idx, int len) {
+    	if(idx == 4) {
+    		for(int i = 0; i < len; i++) {
+    			for(int j = 0; j < len; j++) {
+    				map[i+x][j+y] = ' ';
+    			}
+    		}
+    		return;
+    	}
+    	if(len == 1) {
+    		map[x][y] = '*';
+    		return;
+    	}
+    	int num = 0;
+    	int nextLen = len / 3;
+    	for(int i = 0; i < 3; i++) {
+    		for(int j = 0; j < 3; j++) {
+    			recursion(x + (i * nextLen), y + (j * nextLen), num++,nextLen);
+    		}
+    	}
     }
-
 }
