fa9fbd3f2c93e6f191c10c7646703779d7c3194f
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-14 01:24:44 +0900

    [Gold IV] Title: 알파벳, Time: 796 ms, Memory: 12432 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    // 알파벳을 저장하기 위한 Set
-    static HashSet<Character> set = new HashSet<>();
+    // 알파벳을 저장하기 위한 배열
+    static boolean[] alphabet;
     // 알파벳을 입력받기 위한 map
     static char[][] map;
 
@@ -17,13 +17,13 @@ class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new char[n][m];
-
+        alphabet = new boolean[26];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             map[i] = line.toCharArray();
         }
-        // 스타트 지점은 미리 넣어준다.
-        set.add(map[0][0]);
+        // 스타트 지점은 미리 방문 처리를 해준다.
+        alphabet[map[0][0] - 'A'] = true;
         // 시작 지점을 포함한 1부터 시작
         searchRoute(0,0,1);
         System.out.println(result);
@@ -37,15 +37,15 @@ class Main {
             int nY = y + dirY[i];
 
             // nX,nY가 배열 범위 밖에 있거나 || 같은 알파벳을 지나온 적 있다면 넘어간다.
-            if(isValid(nX,nY) || set.contains(map[nX][nY])){
+            if(isValid(nX,nY) || alphabet[map[nX][nY]-'A']){
                 continue;
             }
 
-            // 경로에 다음 위치를 포함해준 뒤 재귀 호출
-            set.add(map[nX][nY]);
+            // 이미 지나온 알파벳으로 처리를 해준 뒤 재귀 호출
+            alphabet[map[nX][nY]-'A'] = true;
             searchRoute(nX, nY, count + 1);
-            // 다시 set에서 지워주어 배열의 상태를 원래로 만든다.
-            set.remove(map[nX][nY]);
+            // 안지나온 알파벳으로 갱신한다.
+            alphabet[map[nX][nY]-'A'] = false;
         }
         // result값을 최대값으로 갱신한다.
         result = Math.max(count,result);
