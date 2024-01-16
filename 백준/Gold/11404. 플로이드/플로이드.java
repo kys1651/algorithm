fddd7f1b5880cafd9cb7ba0d4e2152c72b4b9b76
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-17 00:19:50 +0900

    [Gold IV] Title: 플로이드, Time: 344 ms, Memory: 42752 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -13,6 +13,10 @@ public class Main {
         // 도시 개수 입력 받기
         n = Integer.parseInt(br.readLine());
         graph = new int[n][n];
+        // 최소값을 찾기 위해 비용의 최대값 100,000 * 최대 도시 100값으로 초기화
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(graph[i], 10000000);
+        }
         int m = Integer.parseInt(br.readLine());
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -20,12 +24,9 @@ public class Main {
             int to = Integer.parseInt(st.nextToken()) - 1;
             int value = Integer.parseInt(st.nextToken());
             // 노선의 개수는 한개가 아닐수도 있기 때문에 처음 받는 값이 아니라면 최소값 삽입
-            if (graph[from][to] != 0) {
-                graph[from][to] = Math.min(graph[from][to], value);
-            }else{
+            if (graph[from][to] > value) {
                 graph[from][to] = value;
             }
-
         }
 
         // 플로이드 워셜
@@ -34,7 +35,7 @@ public class Main {
         // sb에 추가
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i == j) graph[i][j] = 0;
+                if (i == j || graph[i][j] == 10000000) graph[i][j] = 0;
                 sb.append(graph[i][j] + " ");
             }
             sb.append("\n");
@@ -50,16 +51,9 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 // 도착 도시
                 for (int j = 0; j < n; j++) {
-                    // i -> k 노선 연결 && k -> j 노션 연결인 경우
-                    if (graph[i][k] != 0 && graph[k][j] != 0) {
-                        // 기존 노선이 연결되지 않은 경우
-                        if(graph[i][j] == 0){
-                            graph[i][j] = graph[i][k] + graph[k][j];
-                        }
-                        // 기존에 이미 노선이 있는 경우 최소값 연결
-                        else if(graph[i][j] > graph[i][k] + graph[k][j]){
-                            graph[i][j] = graph[i][k] + graph[k][j];
-                        }
+                    // 더 빠른 노선이 있다면 그것으로 초기화 시켜줌
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
                     }
                 }
             }
