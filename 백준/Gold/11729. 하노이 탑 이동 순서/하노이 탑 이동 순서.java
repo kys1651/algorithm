fb394354acf589ba7af63769b06c5d40349b4b1d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-18 20:37:19 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 348 ms, Memory: 96160 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,27 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static int result = 0;
+    static int count;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        hanoi(n,1,2,3);
-        System.out.println(result);
-        System.out.println(sb.toString());
+        hanoi(n,1, 3, 2);
+        System.out.println((int) (Math.pow(2, n) - 1));
+        System.out.println(sb);
     }
 
-    private static void hanoi(int n, int start, int mid, int to){
-        result++;
-        if(n == 1){
-            sb.append(start + " " + to).append("\n");
+    // num을 cur에서 goal로 옮기기
+    private static void hanoi(int num,int cur, int goal, int middle) {
+        count++;
+        // num이 1이면 자유롭게 이동가능한 경우
+        if(num == 1){
+            sb.append(cur + " " + goal + "\n");
             return;
         }
-        hanoi(n-1,start,to,mid);
-        sb.append(start + " " + to).append("\n");
-        hanoi(n-1,mid,start,to);
+
+        // num이 1이 아닌 경우 위에 숫자 N-1들을 중간 지점에 옮겨줘야한다.
+        hanoi(num - 1, cur, middle, goal);
+        // 그럼 현재 num을 옮길 수 있음
+        sb.append(cur + " " + goal + "\n");
+        // middle에 있는 탑들을 지금 목적지로 옮겨줘야한다.
+        hanoi(num - 1, middle, goal, cur);
     }
 }
