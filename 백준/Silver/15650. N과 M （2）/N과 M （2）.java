f826b4eb4800b176b79edf43181a82a75e1e50e6
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-25 00:20:23 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,41 +1,52 @@
-import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
 import java.util.*;
+import java.io.*;
 
+/*
+    작성자 : 김용수
+    문제 : [백준] 15650 : N과 M(2) Silver3(실버3)
+    제출 :
+    결과 :
+    성능 요약 :
+
+    접근 방법
+    1. 문제는 숫자 N개중 M을 뽑는 것이다.
+    2. 오름차순으로 출력해야하기때문에 이전에 방문한 숫자는 방문하지 않는다.
+    3. 이전 문제와 다르게 방문처리를 할 필요 없이 파라미터를 하나 추가하여 상태를 가지고 dfs하는 형식으로 진행
+    3. 메모리와 시간을 아끼기 위해서 Stringbuilder 사용
+*/
 public class Main {
-    static int N,M;
-    static int[] map;
-    static boolean[] visited;
+    // 총 개수 n과 뽑아야 하는 수 M
+    static int m, n;
+    // 정답을 담는 answer 배열
+    static int[] answer;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        answer = new int[m];
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        map = new int [N + 1];
-        visited = new boolean[N + 1];
-
-        solution(0,1);
-
-        System.out.println(sb.toString());
+        solution(0, 1);
+        System.out.println(sb);
     }
 
-    private static void solution(int index,int pos) {
-        if(index==M){
-            for(int i = 0 ; i < M; i++){
-                sb.append(map[i] + " ");
+    private static void solution(int depth, int at) {
+        // n개의 숫자 중 m개를 뽑았다면 StringBuilder에 추가
+        if (depth == m) {
+            for (int i = 0; i < answer.length; i++) {
+                sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = pos; i <= N; i++){
-                map[index] = i;
-                solution(index + 1,i+1);
+        // at ~ n까지 순회
+        for (int i = at; i <= n; i++) {
+            // 현재 i값을 넣어준 뒤 depth + 1, 현재 값 + 1을 해준다.
+            answer[depth] = i;
+            solution(depth + 1, i + 1);
         }
     }
 }
