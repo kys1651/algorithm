c023b3977ebaa02cbfb9ba71af5f146088eee4df
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-26 00:20:17 +0900

    [Silver I] Title: 사각 사각, Time: 84 ms, Memory: 12028 KB -BaekjoonHub

--- 백준/Silver/3359. 사각 사각/사각 사각.java
+++ 백준/Silver/3359. 사각 사각/사각 사각.java
@@ -2,12 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         int[][] arr = new int[n + 1][2];
         int[][] dp = new int[n + 1][2];
         for (int i = 1; i <= n; i++) {
@@ -15,23 +13,20 @@ public class Main {
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        // 사각형 변을 a,b로 둔다.
-        // [0] => a가 윗변 b가 높이인 경우
-        // [1] => b가 윗변 a가 높이인 경우
+
+        // 정방향[0], 회전[1]
         dp[1][0] = arr[1][0];
         dp[1][1] = arr[1][1];
+        // 현재 모양을 그대로 두고 이전에서 최대값을 구해서 그대로 가지고 간다.
         for (int i = 2; i <= n; i++) {
-            // 현재 a가 윗변이고 이전 사각형은 a가 윗변인 경우
-            int a = Math.abs(arr[i - 1][0] - arr[i][1]) + arr[i][0] + dp[i - 1][1];
-            // 현재 a가 윗변이고 이전 사각형은 b가 윗변인 경우
-            int b = Math.abs(arr[i - 1][1] - arr[i][1]) + arr[i][0] + dp[i - 1][0];
-            dp[i][0] = Math.max(a, b);
-
-            // 현재 b가 윗변이고 이전 사각형은 a가 윗변인 경우
-            a = Math.abs(arr[i - 1][1] - arr[i][0]) + arr[i][1] + dp[i - 1][0];
-            // 현재 a가 윗변이고 이전 사각형은 b가 윗변인 경우
-            b = Math.abs(arr[i - 1][0] - arr[i][0]) + arr[i][1] + dp[i - 1][1];
-            dp[i][1] = Math.max(a, b);
+            // 현재 x * y 모양, a : 이전이 x * y 모양인 경우, b : 이전이 y * x인 경우
+            int a = Math.abs(arr[i - 1][1] - arr[i][1]);
+            int b = Math.abs(arr[i - 1][0] - arr[i][1]);
+            dp[i][0] = Math.max(a + dp[i - 1][0], b + dp[i - 1][1]) + arr[i][0];
+            // 현재 y * x 모양, a : 이전이 x * y 모양인 경우, b : 이전이 y * x인 경우
+            a = Math.abs(arr[i - 1][1] - arr[i][0]);
+            b = Math.abs(arr[i - 1][0] - arr[i][0]);
+            dp[i][1] = Math.max(a  + dp[i - 1][0] , b  + dp[i - 1][1]) + arr[i][1];
         }
         System.out.println(Math.max(dp[n][0], dp[n][1]));
     }
