282426fccb7e6c1640c0b607e492433a22b1791b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-27 19:03:42 +0900

    [Silver III] Title: N과 M (5), Time: 236 ms, Memory: 30892 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,55 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M;
-    static boolean[] visited;
-    static int [] answer;
-    static int [] arr;
+    // 메모리를 아끼기 위해서 StringBuilder
     static StringBuilder sb = new StringBuilder();
+    // M개를 뽑아서 저장하는 answer, N개의 숫자를 저장하는 num
+    static int[] answer, num;
+    // 중복 방문을 처리하는 boolean 배열
+    static boolean[] visit;
+    /**
+     * 입력값 N과 M
+     */
+    static int n, m;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        // m개의 숫자를 뽑는 것이니까 m개로 선언
+        answer = new int[m];
+        num = new int[n];
+        visit = new boolean[n];
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[N + 1];
-        answer = new int[N + 1];
-        arr = new int[N + 1];
-
+        // 입력 받은 뒤 정렬
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(num);
 
-        Arrays.sort(arr);
+        combination(0);
 
-        solution(0);
-        
         System.out.println(sb);
     }
 
-    private static void solution(int index) {
-        if(index == M){
-            for (int i = 0; i < M; i++) {
-                sb.append(answer[i] + " ");
+    // 조합을 뽑는 메서드 dfs를 기반으로 로직을 처리
+    private static void combination(int depth) {
+        // 현재 뽑아온 숫자가 m개면 값을 저장한다.
+        if (depth == m) {
+            // 배열에 있는 숫자를 전부 저장
+            for (int i = 0; i < m; i++) {
+                sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 1; i <= N; i++) {
-            if(visited[i]) continue;
+        // 1부터 n까지 중복이 없고 오름차순. 방문한 곳은 넘어간다.
+        for (int i = 0; i < n; i++) {
+            if (visit[i]) continue;
 
-            answer[index] = arr[i];
-            visited[i] = true;
-            solution(index + 1);
-            visited[i] = false;
+            visit[i] = true;
+            answer[depth] = num[i];
+            combination(depth + 1);
+            visit[i] = false;
         }
     }
 }
-
-
