28da266b61cbe582d1952712206475aae142de25
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-27 19:37:41 +0900

    [Silver III] Title: N과 M (7), Time: 616 ms, Memory: 219964 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,52 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Stream;
+import java.io.*;
 
+
+/*
+	작성자 : 김용수
+	문제 : [백준] 15655번 : N과 M(6) Silve3(실버3)
+	제출 : 2024년 1월 27일 19:14:23
+	결과 : 맞았습니다!!
+	성능 요약 : 메모리:11528KB 시간:80ms
+
+	접근 방법
+	1. 숫자 N개 중 M개를 중복 허용하여 뽑는 것이다.
+    2. 오름차순으로 출력하기 위해서 정렬 후
+*/
 public class Main {
-    static int N, M;
-    static boolean[] visited;
-    static int [] answer;
-    static int [] arr;
+    // 메모리를 아끼기 위해서 StringBuilder
     static StringBuilder sb = new StringBuilder();
+    // M개를 뽑아서 저장하는 answer, N개의 숫자를 저장하는 num
+    static int[] answer, num;
+    /**
+     * 입력값 N과 M
+     */
+    static int n, m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        // m개의 숫자를 뽑는 것이니까 m개로 선언
+        answer = new int[m];
+        num = new int[n];
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[N];
-        answer = new int[M];
-        arr = new int[N];
-
+        // 입력 받은 뒤 정렬
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(num);
 
-        solution(0,0);
-        
+        combination(0);
+
         System.out.println(sb);
     }
 
-    static void solution(int depth,int pos) {
-        if (depth == M) {
-            for(int num : answer){
-                sb.append(num).append(" ");
+    // 조합을 뽑는 메서드 dfs를 기반으로 로직을 처리
+    private static void combination(int depth) {
+        // 현재 뽑아온 숫자가 m개면 값을 저장한다.
+        if (depth == m) {
+            // 배열에 있는 숫자를 전부 저장
+            for (int i = 0; i < m; i++) {
+                sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            answer[depth] = arr[i];
-            solution(depth + 1, pos + 1);
+        // 1부터 n까지 중복 가능한 오름차순. 오름차순된 배열이기 때문에 0부터 삽입
+        for (int i = 0; i < n; i++) {
+            answer[depth] = num[i];
+            combination(depth + 1);
         }
     }
 }
-
-
