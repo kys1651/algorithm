778aeb1175c7b7649e2e9f20b389e248c394b860
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-27 18:25:02 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 111820 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,41 +1,42 @@
-import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N,M;
-    static int[] map;
-//    static boolean[] visited;
+    /** 메모리를 아끼기 위해서 StringBuilder */
     static StringBuilder sb = new StringBuilder();
-
+    /** N개 숫자중 M개를 뽑아서 저장하는 배열 */
+    static int[] answer;
+    /** 입력값 N과 M */
+    static int n,m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        // m개의 숫자를 뽑는 것이니까 m개로 선언
+        answer = new int[m];
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        combination(0);
 
-        map = new int [N + 1];
-//        visited = new boolean[N + 1];
-
-        solution(0);
-
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
-    private static void solution(int index) {
-        if(index==M){
-            for(int i = 0 ; i < M; i++){
-                sb.append(map[i] + " ");
+    // 조합을 뽑는 메서드 dfs를 기반으로 로직을 처리
+    private static void combination(int depth) {
+        // 현재 뽑아온 숫자가 m개면 값을 저장한다.
+        if (depth == m) {
+            // 배열에 있는 숫자를 전부 저장
+            for (int i = 0; i < m; i++) {
+                sb.append(answer[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = 1; i <= N; i++){
-                map[index] = i;
-                solution(index + 1);
+        // 1부터 n까지 중복이 없는 것이기 때문에 값을 저장 후 재귀호출을 한다.
+        for (int i = 1; i <= n; i++) {
+            answer[depth] = i;
+            combination(depth + 1);
         }
     }
 }
