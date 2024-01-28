d20ca10caafc940bc911e8509845c4eb0c3e1bc3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-28 17:04:39 +0900

    [Gold IV] Title: 오큰수, Time: 968 ms, Memory: 197896 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,44 +1,32 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());;
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
         }
-
-        /**
-         * 스택이 비어있지 않으면서
-         * 현재 원소가 스택의 Top 원소보다 큰 경우
-         * 해당 조건을 만족할 때 까지 stack에서 pop
-         * 해당 인덱스의 값을 현재원소로 교체함
-         * 
-         * 쉽게 표현하면 스택에 들어있는 인덱스의 value를 가장 크면 바로 교체해주는 것임
-         */
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                arr[stack.pop()] = arr[i];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            // 현재 위치보다 작은 인덱스에 전부 삽입해줌
+            while (!stack.isEmpty() && num[stack.peek()] < num[i]) {
+                num[stack.pop()] = num[i];
             }
-
             stack.push(i);
         }
-        while (!stack.isEmpty()) {
-            arr[stack.pop()] = -1;
+        while(!stack.isEmpty()){
+            num[stack.pop()] = -1;
         }
 
-        for (int n : arr) {
-            bw.write(n + " ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(num[i]).append(" ");
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
