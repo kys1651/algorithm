eeee17244de1a385d898c6ea26fe5960276d638b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-28 16:10:25 +0900

    [Silver I] Title: 동물원, Time: 88 ms, Memory: 15048 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -6,13 +6,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        // 경우의 수 3가지 - 현재 위치에 사자를 안놓는 경우[0], 왼쪽에 놓는 경우[1], 오른쪽에 놓는 경우[2]
         int[][] dp = new int[n + 1][3];
-        dp[1][0] = dp[1][1] = dp[1][2] = 1;
+        // N = 1일 때는 경우의 수가 모두 1이다.
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        // 2부터 N까지 진행
         for (int i = 2; i <= n; i++) {
-            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
-            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
-            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+            // 아무것도 놓지 않는 경우는 위에 모든 경우의 수(아무것도 없을 때, 왼쪽일 때, 오른쪽 일 때)를 더한 것.
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] * 2) % MOD;
+            // 왼쪽인 경우 - 이전 경우가 아무것도 없을 때, 오른쪽일 때
+            // 오른쪽인 경우 - 이전 경우가 아무것도 없을 때, 왼쪽일 때
+            // 즉 대칭이므로 아무것도 없을 때 + 왼쪽인 경우만 담음
+            dp[i][1] = (dp[i-1][0] + dp[i-1][1]) % MOD;
         }
-        System.out.println(((dp[n][0] + dp[n][1]) % MOD + dp[n][2])% MOD);
+        System.out.println((dp[n][0] + dp[n][1] * 2) % MOD);
     }
 }
