08ddf0e3906725043af262da09152daf7131d92d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-28 23:54:44 +0900

    [Gold IV] Title: N-Queen, Time: 4140 ms, Memory: 12288 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,45 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[] visit;
-    static int n;
-    static int result = 0;
+    static int result,n;
+    static int[] map;
+    static boolean[] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        visit = new int[n];
-        dfs(0);
+        map = new int[n];
+        visit = new boolean[n];
+
+        searchCanQueen(0);
+
         System.out.println(result);
     }
 
-    private static void dfs(int depth) {
+    private static void searchCanQueen(int depth) {
         if (depth == n) {
             result++;
             return;
         }
-        for(int i = 0; i < n; i++){
-            visit[depth] = i;
-            if (check(depth)) {
-                dfs(depth + 1);
-            }
+
+        for (int i = 0; i < n; i++) {
+            // 방문한 곳이라면 -> 해당 숫자는 삽입이 불가능함.(이미 해당 열에는 퀸이 존재하기 때문에)
+            if(visit[i]) continue;
+
+            // 대각선이 유효하지 않다면 넘어감.
+            if(!possible(depth,i)) continue;
+
+            visit[i] = true;
+            map[depth] = i;
+            searchCanQueen(depth + 1);
+            visit[i] = false;
         }
     }
 
-    private static boolean check(int depth) {
+    private static boolean possible(int depth, int idx) {
         for (int i = 0; i < depth; i++) {
-            // 같은 행에 존재 
-            if (visit[i] == visit[depth]) {
-                return false;
-            }
-            // 대각선 존재
-            else if (Math.abs(i - depth) == Math.abs(visit[i] - visit[depth])) {
+            // 대각선에 존재하는지 확인
+            if (Math.abs(i - depth) == Math.abs(map[i] - idx)) {
                 return false;
             }
         }
-        
-        // 유효한 위치
+       // 가능한 경우
         return true;
     }
 }
