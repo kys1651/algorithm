d2770fe33c5f0592720da4276c958436da836bfc
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-29 17:14:46 +0900

    [Gold V] Title: ABCDE, Time: 236 ms, Memory: 20684 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n,m;
+	static ArrayList<Integer>[] graph;
+	static boolean[] visit;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		visit = new boolean[n];
+		graph = new ArrayList[n+1];
+		for(int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			graph[b].add(a);
+		}
+		
+		if(n < 5) {
+			System.out.println(0);
+			return;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			searchABCDE(i,1);
+		}
+		System.out.println(0);
+	}
+	private static void searchABCDE(int pos, int count) {
+		visit[pos] = true;
+		if(count == 5) {
+			System.out.println("1");
+			System.exit(0);
+		}
+		
+		for(int next : graph[pos]) {
+			if(visit[next]) continue;
+			visit[next] = true;
+			searchABCDE(next,count+1);
+			visit[next] = false;
+		}
+		visit[pos] = false;
+		
+	}
+
+}
