8be755e655186455406214845e1253a50b4fe1b2
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 15:20:34 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 158 ms, Memory: 30,968 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,42 +1,50 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		for (int tc = 1; tc <= 10; tc++) {
-			sc.nextInt();
-			int[][] map = new int[100][100];
-			int endX = 0;
-			int endY = 0;
-			for (int i = 0; i < 100; i++) {
-				for (int j = 0; j < 100; j++) {
-					map[i][j] = sc.nextInt();
-					if(map[i][j] == 2){
-						endX = i;
-						endY = j;
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = 10;
+		final int SIZE = 100;
+		for(int tc = 1; tc<= T; tc++) {
+			br.readLine();
+			int [][] map = new int[SIZE][SIZE];
+			
+			for(int i = 0; i < SIZE; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < SIZE; j++) {
+					map[i][j] =Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			int start = 0;
+			for(int i =0; i < SIZE; i++) {
+				if(map[SIZE-1][i] == 2) {
+					start = i;
+				}
+			}
+			
+			int i = SIZE-1, j = start;
+			while(i != 0) {
+				// 왼쪽으로 길이 열렸을 경우
+				if(j-1>=0 && map[i][j-1] == 1) {
+					while(j - 1 >= 0 && map[i][j-1] == 1) {
+						j--;
+					}
+				}else if(j + 1 < SIZE && map[i][j+1] == 1) {
+					while(j+1 < SIZE && map[i][j+1] == 1) {
+						j++;
 					}
 				}
+				i--;
 			}
-			System.out.println("#" + tc + " " + check(endX, endY,map));
+			sb.append("#").append(tc).append(" ").append(j).append("\n");
 		}
+		System.out.println(sb);
 	}
 
-	private static int check(int startX, int startY, int[][] map) {
-		int curX = startX;
-		int curY = startY;
-		while(curX > 0) {
-			curX -= 1;
-			if(curY - 1 >= 0 && map[curX][curY - 1] == 1) {
-				while(curY - 1>= 0 && map[curX][curY-1] == 1) {
-					curY--;
-				}
-			}
-			else if(curY + 1 < map.length && map[curX][curY+1] == 1) {
-				while(curY + 1 < map.length && map[curX][curY+1] == 1) {
-					curY++;
-				}
-			}
-		}
-		return curY;
-	}
 }
