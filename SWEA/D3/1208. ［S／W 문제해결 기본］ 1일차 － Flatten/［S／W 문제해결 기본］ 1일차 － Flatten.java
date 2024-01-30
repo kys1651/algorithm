4b2e01b4f78735f8d93862af8645b51b3b92fb8d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 14:28:43 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 127 ms, Memory: 21,108 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,28 +1,31 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-        int[] boxes;
-		int SIZE = 100;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int T = 10;
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int dumpCount = sc.nextInt();
-            boxes = new int[100];
-            for(int i = 0; i < SIZE;i++){
-                boxes[i] = sc.nextInt();
-            }
-            while( dumpCount-- > 0){
-                Arrays.sort(boxes);
-                boxes[0]++;
-                boxes[99]--;
-            }
-                Arrays.sort(boxes);
-            System.out.println("#" + tc + " " + (boxes[99] - boxes[0]));
+		final int SIZE = 100;
+		for(int tc = 1; tc <= T; tc++) {
+			int k = Integer.parseInt(br.readLine());
+			int[] num = new int[SIZE];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < SIZE; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(num);
+			while(k --> 0) {
+				num[0]++; num[99]--;
+				Arrays.sort(num);
+			}
+			
+			sb.append("#").append(tc).append(" ").append(num[99] - num[0]).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
