5b04a9bb57b5f7025b7c58c683c50bb5b989151d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 22:28:08 +0900

    [Gold V] Title: 최소비용 구하기, Time: 608 ms, Memory: 55932 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,62 +1,88 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
+// 한 도시에서 다른 도시 최소비용 -> 다익스트라(dijkstra)
 public class Main {
-	static ArrayList<int[]>[] list;
-	static int[] values;
-	static int n;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		int k =Integer.parseInt(br.readLine());
-		list = new ArrayList[n + 1];
-		values = new int[n+1];
-		Arrays.fill(values, Integer.MAX_VALUE);
-		for(int i = 1; i <= n; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i = 0; i < k; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			list[from].add(new int[] {to,w});
-		}
-		
-		
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-		
-		dijkstra(start);
-		System.out.println(values[end]);
-	}
-	
-	private static void dijkstra(int start) {
-		PriorityQueue<int[]> queue = new PriorityQueue<>((o1,o2)->o1[1] - o2[1]);
-		queue.offer(new int[] {start,0});
-		
-		boolean[] visit = new boolean[n+1];
-		// 시작점이니까 0 넣어줌
-		values[start] = 0;
-		
-		while(!queue.isEmpty()) {
-			int curs[] = queue.poll();
-			int cur = curs[0];
-			if(visit[cur]) continue;
-			visit[cur] = true;
-			
-			for(int[] next : list[cur]) {
-				int end = next[0];
-				int endW = next[1];
-				if(values[end] > values[cur] + endW) {
-					values[end] = values[cur] + endW;
-					queue.offer(new int[] {end,values[end]});
-				}
-			}
-		}
-		
-	}
+    // 노드 클래스. 가중치와 인덱스를 가지고 있음
+    static class Node {
+        int idx;
+        int cost;
+
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+    }
+
+    // 각 노드가 연결된 노드 리스트
+    static ArrayList<Node>[] graph;
+    // 노드의 개수
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        graph = new ArrayList[n + 1];
+        // for문을 통해서 직접 초기화해주어야한다.
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // m : 간선의 개수
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken()); // 출발지
+            int to = Integer.parseInt(st.nextToken()); // 도착지
+            int cost = Integer.parseInt(st.nextToken()); // 가중치
+            graph[from].add(new Node(to, cost));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijkstra(start, end);
+    }
+
+    // start부터 end까지 걸리는 최소 비용을 구하는 다익스트라 알고리즘
+    private static void dijkstra(int start, int end) {
+        // 우선순위큐 -> 코스트가 가장 낮은 노드가 우선순위
+        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        // 시작지점을 코스트를 0으로하여 넣어준다.
+        queue.offer(new Node(start, 0));
+
+        // 방문한 곳은 다시 방문하지 않기에 visit 배열을 생성해준다.
+        boolean[] visit = new boolean[n + 1];
+
+        // 비용을 저장하기 위한 cost 배열
+        int[] cost = new int[n + 1];
+        Arrays.fill(cost, Integer.MAX_VALUE); // 최소비용을 찾기 위해서 MAX_VALUE를 넣어준다.
+        // 시작지점 비용은 0이다.
+        cost[start] = 0;
+
+        while (!queue.isEmpty()) {
+            // 현재 노드: 현재 갈 수 있는 방문안한 노드 중 비용이 가장 싼 곳
+            Node current = queue.poll();
+
+            if (visit[current.idx]) {
+                continue;
+            }
+            visit[current.idx] = true;
+            // 현재 노드와 연결된 노드들을 차례대로 빼준다.
+            for (Node nextCity : graph[current.idx]) {
+                // 다음 도시로 가는 기존 비용이 현재 노드에서 가는 비용보다 싸다면
+                if (cost[nextCity.idx] > cost[current.idx] + nextCity.cost) {
+                    // 값을 넣어준다.
+                    cost[nextCity.idx] = cost[current.idx] + nextCity.cost;
+                    // 갱신 후 큐에 넣어준다.
+                    queue.offer(new Node(nextCity.idx, cost[nextCity.idx]));
+                }
+            }
+        }
+
+        // 출력 후 종료 -> 출발점에서 도착점을 갈 수 있는 경우만 입력으로 주어진다.
+        System.out.println(cost[end]);
+    }
 }
