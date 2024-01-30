efdabf0ba35190915c5bf2aa2fbd0d380c1ec7e2
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 14:51:43 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 84 ms, Memory: 11756 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,14 +1,25 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 제목: 17478번 재귀함수가 뭔가요? Silver5
+ * @author 김용수
+ * 제출 시간: 2024년 1월 29일 13:50:22
+ * 메모리 : 11840KB
+ * 시간 : 88ms
+ * 접근 방법 : 
+ * 1. 재귀조건과 종료조건을 파악하여 출력한다.
+ */
 class Main{
+	// 메모리 절약을 위해서 text를 미리 선언함
 	static String[] text = {
 			"\"재귀함수가 뭔가요?\"",
 			"\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.",
 			"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.",
 			"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"",
 			"\"재귀함수는 자기 자신을 호출하는 함수라네\"",
-			"라고 답변하였지."
+			"라고 답변하였지.",
+			"____"
 	};
 	static StringBuilder sb = new StringBuilder();
 	static int n;
@@ -21,25 +32,25 @@ class Main{
 	}
 
 	private static void recursion(int depth) {
-		addWord(depth * 4,text[0]);
+		addWord(depth,text[0]);
 		
 		if(depth == n) {
-			addWord(depth * 4,text[4]);
-			addWord(depth * 4,text[5]);
+			addWord(depth,text[4]);
+			addWord(depth,text[5]);
 			return;
 		}
-		addWord(depth * 4,text[1]);
-		addWord(depth * 4,text[2]);
-		addWord(depth * 4,text[3]);
+		addWord(depth,text[1]);
+		addWord(depth,text[2]);
+		addWord(depth,text[3]);
 		
 		if(depth != n) {
 			recursion(depth+1);		
 		}
-		addWord(depth * 4,text[5]);
+		addWord(depth,text[5]);
 	}
 	private static void addWord(int depth,String word) {
 		for(int i = 0; i < depth; i++) {
-			sb.append("_");
+			sb.append(text[6]);
 		}
 		sb.append(word).append("\n");
 	}
