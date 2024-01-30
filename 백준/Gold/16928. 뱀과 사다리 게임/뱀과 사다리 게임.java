28639ba43e341037fb5b627728c5cce748405b60
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 13:48:02 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 76 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,59 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-class Main {
-    static int[] ladders;
-    static int[] snake;
-    static int[] counts;
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        ladders = new int[101];
-        snake = new int[101];
-        counts = new int[101];
-        for(int i = 0; i < n ; i++){
-            ladders[sc.nextInt()] = sc.nextInt();
-        }
-        for(int i = 0; i < m ; i++){
-            snake[sc.nextInt()] = sc.nextInt();
-        }
-        bfs(1, snake, ladders, counts);
-        System.out.println(counts[100]);
+import java.util.StringTokenizer;
+
+
+public class Main {
+	static int[] visit = new int[101];
+	static int[] move = new int[101];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < n + m; i++) {
+			st = new StringTokenizer(br.readLine());
+			move[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+		}
+		bfs();
 	}
-
-    private static void bfs(int start, int[] snake,int[] ladders, int [] counts) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-        
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-
-            for(int i = 1; i <=6; i++){
-                int next = cur + i;
-                if(next > 100){
-                    continue;
-                }
-                if(counts[next] != 0 &&counts[next] <= counts[cur] + 1){
-                    continue;
-                }
-                // 일단 거기 가는 경우의 수가 제일 
-                counts[next] = counts[cur] + 1;
-                if(ladders[next] != 0){
-                    if(counts[ladders[next]] == 0 || counts[ladders[next]] > counts[next]){
-                        counts[ladders[next]] = counts[next];
-                        queue.offer(ladders[next]);
-                    }
-                    // 연산
-                }else if(snake[next] != 0){
-                    if(counts[snake[next]] == 0 || counts[snake[next]] > counts[next]){
-                        counts[snake[next]] = counts[next];
-                        queue.offer(snake[next]);
-                    }
-                }else{
-                    queue.offer(next);
-                }
-            }
-        }
-    }
+	private static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(1);
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+			
+			for(int i = 1; i <= 6; i++) {
+				// 다음 위치
+				int nX = cur + i;
+				
+				// 100을 넘거나 (방문했을때) 더 가성비 있다면 넘어감
+				if(nX > 100 || visit[nX] != 0 && visit[nX] <= visit[cur] + 1) {
+					continue;
+				}
+				
+				visit[nX] = visit[cur] + 1;
+				if(move[nX] != 0) {
+					int next = move[nX];
+					if(visit[next] == 0 || visit[next] > visit[nX] + 1) {
+						visit[move[nX]] = visit[nX];
+						queue.add(move[nX]);
+					}
+				}else {
+					queue.add(nX);
+				}
+			}
+		}
+		
+		System.out.println(visit[100]);
+		
+	}
 }
