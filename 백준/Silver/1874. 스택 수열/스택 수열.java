2f1d87815b0ebd84de873bcca72993389ccf5619
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 16:50:49 +0900

    [Silver II] Title: 스택 수열, Time: 312 ms, Memory: 27236 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        int idx = 0;
-        int num = 1;
-        stack.push(num++);
-        sb.append("+").append("\n");
-
-        while(num <= n){
-            if( !stack.isEmpty() && arr[idx] == stack.peek()){
-                sb.append("-").append("\n");
-                stack.pop();
-                idx++;
-            }else{
-                stack.push(num++);
-                sb.append("+").append("\n");
-            }
-        }
-        while(!stack.isEmpty()){
-            if(arr[idx++] == stack.pop())
-                sb.append("-").append("\n");
-            else {
-                break;
-            }
-        }
-
-        System.out.println(stack.isEmpty()?sb:"NO");
-
-    }
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int[] answer= new int[n];
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < n; i++) {
+			answer[i] = Integer.parseInt(br.readLine());
+		}
+		
+		final String plus = "+\n";
+		final String minus = "-\n";
+		int idx = 0;
+		for(int i = 1; i<= n; i++) {
+			stack.push(i);
+			sb.append(plus);
+			while(!stack.isEmpty() && answer[idx] == stack.peek()){
+				idx++;
+				stack.pop();
+				sb.append(minus);
+			}
+		}
+		if(stack.isEmpty()) {
+			System.out.println(sb);
+		}else {
+			System.out.println("NO");
+		}
+		
+	}
 }
