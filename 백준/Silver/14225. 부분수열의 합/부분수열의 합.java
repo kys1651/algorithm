1cea4eeb79fd649c7f6a7252cf04d27708854d0f
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 21:45:49 +0900

    [Silver I] Title: 부분수열의 합, Time: 96 ms, Memory: 13800 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -4,35 +4,31 @@ import java.io.*;
 public class Main {
     static int n;
     static int[] num;
-    static boolean[] visit;
-    static HashSet<Integer> possibleNum = new HashSet<>();
-
+    static boolean[] check = new boolean[100000 * 20];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         num = new int[n];
-        visit = new boolean[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
 
-        combination(0, 0, 0);
+        combination(0, 0);
 
         int idx = 1;
-        while(possibleNum.contains(idx)){
+        while(check[idx]){
             idx++;
         }
         System.out.println(idx);
     }
 
-    private static void combination(int depth, int at, int sum) {
-        for (int i = at; i < n; i++) {
-            if(visit[i]) continue;
-            visit[i] = true;
-            combination(depth + 1, i + 1, sum + num[i]);
-            visit[i] = false;
+    private static void combination(int depth,int sum) {
+        if (depth == n) {
+            check[sum] = true;
+            return;
         }
-        possibleNum.add(sum);
+        combination(depth + 1, sum + num[depth]);
+        combination(depth + 1, sum);
     }
 }
