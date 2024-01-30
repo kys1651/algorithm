4bedf6f8613b1fe91100f1cca7c41894afba7201
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 15:48:20 +0900

    [D3] Title: 농작물 수확하기, Time: 120 ms, Memory: 20,428 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,40 +1,35 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int[][] map;
-        
-        int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-			int n = sc.nextInt();
-            map = new int[n][n];
-            for(int i = 0; i < n; i++){
-                String tmp = sc.next();
-                for(int j = 0; j < n; j++){
-                    map[i][j] = tmp.charAt(j) - '0';
-                }
-            }
-            
-            int start = n / 2;
-            int end = n / 2;
-            int sum = 0;
-            for(int i = 0; i < n; i++){
-                for(int j = start;  j <= end; j++){
-                    sum += map[i][j];
-                }
-                if(i < n / 2){
-                    start--;
-                    end++;
-                }else{
-                    start++;
-                    end--;
-                }
-            }
-            System.out.println("#" +tc + " " + sum);
+public class Solution{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[][] map = new int[n][n];
+			int sum = 0;
+			for(int i = 0 ; i  < n; i++) {
+				String input = br.readLine();
+				for(int j = 0; j < n; j++) {
+					map[i][j] = input.charAt(j) - '0';
+					sum += map[i][j];
+				}
+			}
+			
+			int len = n/2;
+			int last = n-1;
+			for(int i = 0; i < len;i++) {
+				for(int j = 0; j < len - i; j++) {
+					sum -= (map[i][j] + map[last-i][j] + map[i][last-j] + map[last-i][last-j]);
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(sum).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
