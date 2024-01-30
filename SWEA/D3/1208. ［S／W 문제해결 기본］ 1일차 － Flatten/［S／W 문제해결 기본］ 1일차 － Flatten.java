3ea71a9caae2a9bdcb5742f9fc3203b0ad485a6a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-30 14:37:25 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 190 ms, Memory: 27,608 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 
@@ -13,18 +14,20 @@ public class Solution {
 		final int SIZE = 100;
 		for(int tc = 1; tc <= T; tc++) {
 			int k = Integer.parseInt(br.readLine());
-			int[] num = new int[SIZE];
+			PriorityQueue<Integer> minQueue = new PriorityQueue<>((o1,o2)->o1-o2);
+			PriorityQueue<Integer> maxQueue = new PriorityQueue<>((o1,o2)->o2-o1);
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < SIZE; i++) {
-				num[i] = Integer.parseInt(st.nextToken());
+				int tmp = Integer.parseInt(st.nextToken());
+				minQueue.add(tmp);
+				maxQueue.add(tmp);
 			}
-			Arrays.sort(num);
 			while(k --> 0) {
-				num[0]++; num[99]--;
-				Arrays.sort(num);
+				minQueue.add(minQueue.poll() + 1);
+				maxQueue.add(maxQueue.poll() - 1);
 			}
 			
-			sb.append("#").append(tc).append(" ").append(num[99] - num[0]).append("\n");
+			sb.append("#").append(tc).append(" ").append(maxQueue.poll() - minQueue.poll()).append("\n");
 		}
 		System.out.println(sb);
 	}
