2ffa52d385b4281fb1ff74a75f60da5be408e791
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-31 17:09:53 +0900

    [D2] Title: 달팽이 숫자, Time: 113 ms, Memory: 16,080 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,44 +1,45 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int[][] map;
-        // 오른쪽 -> 아래 -> 왼쪽 -> 위
-        int[] dirX = {0,1,0,-1};
-        int[] dirY = {1,0,-1,0};
-        
-        int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int N = sc.nextInt();
-            map = new int[N][N];
-            int num = 1;
-            int x = 0;
-            int y = 0;
-            int d = 0;
-            while(num <= N * N){
-               map[x][y] = num++;
-               int nextX = x + dirX[d];
-                int nextY = y + dirY[d];
-                if(nextX >= N || nextX < 0 || nextY >= N || nextY < 0 || map[nextX][nextY] != 0){
-                    d = (d + 1) % 4;
-                }
-                x += dirX[d];
-                y += dirY[d];
-            }
-            
-            System.out.println("#"+tc);
-            for(int [] arr : map){
-                for(int n : arr){
-                    System.out.print(n + " ");
-                }
-                System.out.println();
-            }
-            
+public class Solution {
+	// 우,하,좌,상으로 이동
+	static int[] dirX = { 0, 1, 0, -1 };
+	static int[] dirY = { 1, 0, -1, 0 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[][] board = new int[N][N];
+			int idx = 1;
+			int x = 0, y = 0,d = 0;
+			while(idx <= N * N) {
+				board[x][y] = idx++;
+				int nX = x + dirX[d];
+				int nY = y + dirY[d];
+				if(nX < 0 || nY < 0 || nX >= N || nY >= N || board[nX][nY] != 0) {
+					d = (d + 1) % 4;
+					x += dirX[d];
+					y += dirY[d];
+					continue;
+				}
+				x = nX;
+				y = nY;
+			}
+			
+			sb.append("#").append(tc).append("\n");
+			for(int[] nums : board) {
+				for(int num : nums) {
+					sb.append(num).append(" ");
+				}
+				sb.append("\n");
+			}
 		}
+		
+		System.out.println(sb);
 	}
 }
