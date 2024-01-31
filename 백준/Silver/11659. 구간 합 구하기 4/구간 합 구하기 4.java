d6b07ab22fb28fc57966fdf086656be6d335fd71
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-31 11:38:11 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 572 ms, Memory: 56204 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,36 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+/*
+ * 접근 방법
+ * 1. 만약 N과 M이 100,000인 경우 전부 조회하는 시간 복잡도는 10,000,000,000 = 10^9 -> 시간 초과
+ * 2. 누적합을 이용해서 i,와j를 입력받으면 j인덱스값에서 i -1 값을 빼준다.
+ */
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        long[] sum = new long[n+1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
-        }
-
-        while (m-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-
-            sb.append(sum[j] - sum[i-1]).append("\n");
-        }
-
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] num = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			// 현재 값은 이전 누적값 + 현재 값
+			num[i] = num[i-1] + value;
+		}
+		
+		for(int i = 0 ; i < m ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(num[end] - num[start-1]).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
