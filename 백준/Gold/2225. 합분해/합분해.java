6fa5ef867c687b8af9230843149bdd7b0601816b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-31 20:33:26 +0900

    [Gold V] Title: 합분해, Time: 76 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,34 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static final int MOD = 1_000_000_000;
+    static final int MOD = 1000_000_000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[k + 1][n + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        // N을 0개로 만들 수 있는 값은 0개 - 존재하지 않음
-        // N을 1개로 만들 수 있는 값은 1개 - 해당 숫자 한개
-        for (int i = 0; i <= n; i++) {
-            dp[0][i] = 0;
-            dp[1][i] = 1;
-        }
+        int[][] dp = new int[K + 1][N + 1];
+        // K가 1 -> 1로 만들 수 있는 수는 무조건 1개
+        Arrays.fill(dp[1], 1);
 
-        // 0을 만들 수 있는 경우의 수는 0 + 0 + 0 + ,,,으로 1개
-        for (int i = 1; i <= k; i++) {
+        // 0은 K개라면 무조건 1개씩은 만든다.
+        for (int i = 0; i <= K; i++) {
             dp[i][0] = 1;
         }
 
-        // 1을 만드는 경우 : 0을 두개로 만드는 경우 + 1을 1개로 만드는 경우
-        // 2를 만드는 경우 : 1을 두개로 만드는 경우 + 2를 1개로 만드는 경우
-        for (int i = 2; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+        for (int i = 2; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1])  % MOD;
             }
         }
-        System.out.println(dp[k][n]);
+        System.out.println(dp[K][N]);
     }
 }
