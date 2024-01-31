78d612d8b1cc46f30807e39737a26f40af971a77
김용수 <43926186+kys1651@users.noreply.github.com>
2024-01-31 20:00:45 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 460 ms, Memory: 54668 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -2,49 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.StringTokenizer;
 
-class Main {
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        Map<String,Integer> nameMap = new HashMap<>();
-        String[] nameArr = new String[N+1];
         StringBuilder sb = new StringBuilder();
-
-        for(int i = 1; i <= N; i++){
-            String tmp = br.readLine();
-            nameMap.put(tmp, i);
-            nameArr[i] = tmp;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        String[] idxName = new String[n + 1];
+        HashMap<String, Integer> nameIdx = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            String input = br.readLine();
+            nameIdx.put(input,i);
+            idxName[i] = input;
         }
 
-        while(M --> 0){
-            String find = br.readLine();
+        for (int i = 1; i <= m; i++) {
+            String input = br.readLine();
 
-            if(isStringNumber(find)){
-                int index = Integer.parseInt(find);
-                sb.append(nameArr[index]);
+            if (nameIdx.containsKey(input)) {
+                sb.append(nameIdx.get(input));
             }else{
-                sb.append(nameMap.get(find));
+                sb.append(idxName[Integer.parseInt(input)]);
             }
             sb.append("\n");
         }
 
-        System.out.println(sb.toString());
-
-    }
-
-    public static boolean isStringNumber(String find) {
-        try{
-            Integer.parseInt(find);
-            return true;
-        }catch(Exception e){
-            return false;
-        }
+        System.out.println(sb);
     }
 }
