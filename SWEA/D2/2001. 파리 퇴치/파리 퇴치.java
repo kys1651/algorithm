4dda01fa7daca7ffe8b9ba2344e17f045866bd8b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-01 23:21:51 +0900

    [D2] Title: 파리 퇴치, Time: 104 ms, Memory: 18,676 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -3,36 +3,40 @@ import java.io.*;
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
-	{
-        Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
             int[][] board = new int[n][n];
             for(int i =0; i < n; i++){
-            	for(int j = 0; j < n ;j++){
-                	board[i][j] = sc.nextInt();
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < n ;j++){
+                    board[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            
+
             int max = 0;
             for(int i = 0 ; i < n - m + 1; i++){
                 for(int j = 0; j < n - m + 1; j++){
                     int tmp = 0;
-                	for(int x = 0; x < m; x++){
-                    	for(int y = 0 ; y < m; y++){
+                    for(int x = 0; x < m; x++){
+                        for(int y = 0 ; y < m; y++){
                             tmp += board[i + x][j + y];
                         }
                     }
                     max = Math.max(max,tmp);
                 }
             }
-            System.out.printf("#%d %d\n",tc,max);
+            sb.append(String.format("#%d %d\n",tc,max));
         }
-	}
+        System.out.println(sb);
+    }
 }
