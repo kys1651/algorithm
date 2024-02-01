6e3466a6b0e29e8edc5504bdac9a6e1ca3ad3538
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-01 19:07:50 +0900

    [Gold V] Title: 신기한 소수, Time: 72 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -4,59 +4,43 @@ import java.io.InputStreamReader;
 
 public class Main {
     static int N;
-    static boolean[] prime;
-    static int[] firstUnitPrime = {2,3,5,7};
-    static int[] secondUnitPrime = {1,3,7,9};
+    static int[] firstUnitPrime = {2, 3, 5, 7};
+    static int[] secondUnitPrime = {1, 3, 7, 9};
     static int[] answer;
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         answer = new int[N];
-        getPrime(N+1);
 
-        for(int i = 0 ; i < firstUnitPrime.length; i++) {
-            combination(1,firstUnitPrime[i]);
+        for (int i = 0; i < firstUnitPrime.length; i++) {
+            combination(1, firstUnitPrime[i]);
         }
-
 
         System.out.println(sb);
     }
 
-    private static void combination(int depth,int value) {
-        if(depth == N) {
+    private static void combination(int depth, int value) {
+        if (depth == N) {
             sb.append(value).append("\n");
             return;
         }
 
-        for(int i = 0; i < secondUnitPrime.length; i++) {
+        for (int i = 0; i < secondUnitPrime.length; i++) {
             int nextValue = value * 10 + secondUnitPrime[i];
-            if(!isValid(nextValue)) continue;
-            combination(depth+1, nextValue);
+            if (isValid(nextValue)) {
+                combination(depth + 1, nextValue);
+            }
         }
     }
 
     private static boolean isValid(int value) {
         for (int i = 2; i <= Math.sqrt(value); i++) {
-            if(!prime[i] && value % i == 0){
+            if (value % i == 0) {
                 return false;
             }
         }
         return true;
-    }
-
-
-    private static void getPrime(int size) {
-        // true : 소수아님, false: 소수
-        size = (size / 2) + (size % 2);
-        prime = new boolean[(int)Math.pow(10,size)];
-        prime[0] = prime[1] = true;
-        for(int i = 2; i < Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
-
-            for(int j = i * 2; j < prime.length; j+= i) {
-                prime[j] = true;
-            }
-        }
     }
 }
