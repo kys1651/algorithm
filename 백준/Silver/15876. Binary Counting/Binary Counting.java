a214e038429c5b79813062d6a7ae3cc4723b35a1
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-02 16:49:07 +0900

    [Silver V] Title: Binary Counting, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/15876. Binary Counting/Binary Counting.java
+++ 백준/Silver/15876. Binary Counting/Binary Counting.java
@@ -6,16 +6,18 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-	static char[] bit = new char[10001];
-
+public class Main{
+	static char[] bit;
+	static int size;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-
+		size = n * 5;
+		
 		getBits();
 
 		for (int i = 0; i < 5; i++) {
@@ -26,12 +28,13 @@ public class Main {
 	}
 
 	private static void getBits() {
+		bit = new char[size];
 		int i = 0, idx = 0;
 		while (true) {
 			String binStr = Integer.toBinaryString(idx++);
 			for (char ch : binStr.toCharArray()) {
 				bit[i++] = ch;
-				if (i >= 10000) {
+				if (i >= size) {
 					return;
 				}
 			}
@@ -39,4 +42,3 @@ public class Main {
 	}
 
 }
-
