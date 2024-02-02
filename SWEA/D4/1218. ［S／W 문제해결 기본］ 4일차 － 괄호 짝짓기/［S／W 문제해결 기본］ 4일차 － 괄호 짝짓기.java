f9d85e65c235db684de084a655fe0c9f369c389a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-02 13:31:10 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 105 ms, Memory: 18,168 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,50 +1,41 @@
-import java.util.Stack;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Stack;
 
-class Solution {
-    static char[] front = {'(','[','{','<'};
-    static char[] rear = {')',']','}','>'};
-	public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-		for(int tc = 1; tc <= 10; tc++) {
-            int len = Integer.parseInt(br.readLine());
-            String line = br.readLine();
-            sb.append("#" + tc + " " + (validText(len,line) ? "1" : "0")).append("\n");
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = 10;
+		
+		HashMap<Character, Character> map = new HashMap<>();
+		map.put(')','(');
+		map.put(']','[');
+		map.put('}','{');
+		map.put('>', '<');
+		for(int tc = 1; tc <= T; tc++) {
+			br.readLine();
+			String input = br.readLine();
+			Stack<Character> stack = new Stack<>();
+			int result = 1;
+			for(char ch : input.toCharArray()) {
+				if(map.containsKey(ch)) {
+					if(stack.peek() == map.get(ch)) {
+						stack.pop();
+					}else {
+						result = 0;
+						break;
+					}
+				}
+				else {
+					stack.push(ch);
+				}
+			}
+			sb.append(String.format("#%d %d\n",tc,result));
 		}
-        System.out.println(sb);
+		System.out.println(sb);
 	}
-    private static boolean validText(int len, String text){
-        Stack<Character> stack = new Stack<>();
-        boolean isFront;
-        for(int i = 0 ; i < len; i++){
-            char ch = text.charAt(i);
-            isFront = true;
-            
-            // 뒤 괄호인 경우 기존 스택의 앞에 있는지 확인한다.
-            for(int j = 0; j < 4; j++){
-                // 뒤에 있는 괄호를 발견한 경우
-                if(ch == rear[j]){
-                    // 스택이 비어있거나 stack의 탑이 현재 앞 괄호와 다른 경우 숏컷으로 종료
-                    if(stack.isEmpty() || stack.peek() != front[j]){
-                        return false;
-                    }
-                    // 동일한 경우 빼준 뒤 Front가 아니라고 체크
-                    else{
-                        stack.pop();
-                        isFront = false;
-                    }
-                    break;
-                }
-            }
-            
-            // ch가 앞괄호라면 push해줌
-            if(isFront){
-                stack.push(ch);
-            }
-        }
-        // 모든 연산이 끝난 후 Stack가 비어있어야 올바른 경우
-        return stack.isEmpty();
-    }
+
 }
