f21ffa0e1f65f59c29f103d5e0c10a4cf3e1c66f
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-02 14:34:03 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 127 ms, Memory: 22,208 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,35 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		for(int tc = 1; tc <= 10; tc++)
-		{
-            int n = sc.nextInt();
-            int[] num = new int[8];
-            Queue<Integer> q = new LinkedList<>();
-            for(int i = 0; i < 8; i++){
-                q.offer(sc.nextInt());
-            }
-			while(!cycle(q)){}
-            System.out.print("#" + tc + " ");
-            while(!q.isEmpty()){
-                System.out.print(q.poll() + " ");
-            }
-            System.out.println("0");
+import java.util.StringTokenizer;
+
+public class Solution {
+	static final int SIZE = 8;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = 10;
+		
+		for(int tc = 1; tc <= T; tc++) {
+			br.readLine();
+			Queue<Integer> queue = new LinkedList<>();
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < SIZE; i++) {
+				queue.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			int minus = 1;
+			while(true) {
+				int top = queue.poll();
+				top -= minus++;
+				if(top <= 0) {
+					queue.add(0);
+					break;
+				}
+				if(minus == 6) {
+					minus = 1;
+				}
+				queue.add(top);
+			}
+			sb.append("#").append(tc).append(" ");
+			for(int i = 0; i <SIZE; i++) {
+				sb.append(queue.poll()).append(" ");
+			}
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
-    private static boolean cycle(Queue<Integer> q){
-        for(int i = 1; i <= 5; i++){
-            int n = q.poll();
-            if(n - i <= 0){
-                return true;
-            }
-            q.offer(n - i);
-        }
-        return false;
-    }
+
 }
