b811a4fafcd8ad760b00150f006c0b0a325c828e
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-02 13:07:09 +0900

    [D8] Title: 마이쭈시뮬레이션_실습, Time: 98 ms, Memory: 18,184 KB -BaekjoonHub

--- SWEA/D8/16744. 마이쭈시뮬레이션＿실습/마이쭈시뮬레이션＿실습.java
+++ SWEA/D8/16744. 마이쭈시뮬레이션＿실습/마이쭈시뮬레이션＿실습.java
@@ -5,16 +5,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-	static class Person {
-		int num;
-		int cnt;
-
-		public Person(int num, int cnt) {
-			this.num = num;
-			this.cnt = cnt;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -24,19 +14,18 @@ public class Solution {
 			int n = Integer.parseInt(br.readLine());
 			int idx = 1;
 
-			Queue<Person> queue = new LinkedList<>();
-			queue.add(new Person(idx++, 1));
+			Queue<int[]> queue = new LinkedList<>();
+			queue.add(new int[] {idx++,1});
 
 			while (true) {
-				Person p = queue.poll();
-				n -= p.cnt;
+				int[] p = queue.poll();
+				n -= p[1];
 				if (n <= 0) {
-					sb.append("#").append(tc).append(" ").append(p.num).append("\n");
+					sb.append("#").append(tc).append(" ").append(p[0]).append("\n");
 					break;
 				}
-				p.cnt++;
-				queue.offer(p);
-				queue.add(new Person(idx++, 1));
+				queue.offer(new int[] {p[0],p[1]+1});
+				queue.offer(new int[] {idx++, 1});
 			}
 		}
 		System.out.println(sb);
