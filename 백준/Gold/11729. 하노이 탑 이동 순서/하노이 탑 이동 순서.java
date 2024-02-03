ed4d9b0b09ad4e9311ab12ba44375ac5eef64258
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-03 15:35:05 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 196 ms, Memory: 39412 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -4,24 +4,28 @@ import java.io.InputStreamReader;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static final char[] top = {'1','2','3'};
+    static final String[][] answer = {
+            {"", "1 2\n", "1 3\n"},
+            {"2 1\n", "", "2 3\n"},
+            {"3 1\n", "3 2\n"}
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int count = (1 << N) - 1;
-        sb.append(count).append('\n');
+        sb.append((1 << N) - 1).append('\n');
+        
         hanoi(N, 1, 3, 2);
 
         System.out.println(sb);
     }
 
     private static void hanoi(int n, int from, int to, int mid) {
-        if(n == 0) return;
+        if (n == 0) return;
 
         hanoi(n - 1, from, mid, to);
-        sb.append(top[from - 1]).append(' ').append(top[to - 1]).append('\n');
+        sb.append(answer[from - 1][to - 1]);
         hanoi(n - 1, mid, to, from);
     }
 }
