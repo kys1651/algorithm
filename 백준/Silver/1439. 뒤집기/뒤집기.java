9c699a959a264b5d60810ea4704453c193249985
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-04 00:26:08 +0900

    [Silver V] Title: 뒤집기, Time: 92 ms, Memory: 11764 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int zero = new StringTokenizer(input, "1").countTokens();
+        int one = new StringTokenizer(input, "0").countTokens();
+        System.out.println(Math.min(zero, one));
+    }
+}
