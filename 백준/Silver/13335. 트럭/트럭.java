febcc98eefd17b192906897ff659d853c120f4fc
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-03 15:14:07 +0900

    [Silver I] Title: 트럭, Time: 88 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -14,27 +14,27 @@ public class Main {
         int W = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> trucks = new LinkedList<>();
+        int[] trucks = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            trucks.add(Integer.parseInt(st.nextToken()));
+            trucks[i] = Integer.parseInt(st.nextToken());
         }
 
         Queue<int[]> bridge = new LinkedList<>();
-        int time = 0, weight = 0;
-        while (!bridge.isEmpty() || !trucks.isEmpty()) {
+        int time = 0, weight = 0,idx = 0;
+        while (!bridge.isEmpty() || idx != N) {
             // 현재 다리에 있는 트럭이 나와야 할 시간이라면 빼준다.
             if (!bridge.isEmpty() && bridge.peek()[1] == time) {
                 weight -= bridge.remove()[0];
             }
             // 다리의 트럭의 개수가 L보다 작고 트럭을 추가해도 된다면 올려준다.
-            if (bridge.size() < W && !trucks.isEmpty() && trucks.peek() + weight <= L) {
+            if (bridge.size() < W && idx != N && trucks[idx] + weight <= L) {
                 // 트럭을 빼준다.
-                int truck = trucks.remove();
                 // 현재 가중치에 더해준다.
-                weight += truck;
+                weight += trucks[idx];
                 // 다리에 현재 시간을 기록해서 올려준다.
-                bridge.add(new int[]{truck, time + W});
+                bridge.add(new int[]{trucks[idx], time + W});
+                idx++;
             }
             time++;
         }
