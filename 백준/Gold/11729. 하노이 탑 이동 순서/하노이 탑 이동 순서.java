ac35376f52306f16ad5c01b95d26aedb9f9d811c
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-03 15:28:58 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 208 ms, Memory: 40452 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,40 +1,27 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
+    static final char[] top = {'1','2','3'};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        hanoi(n,1, 3, 2);
-        /*
-            N = 1일 때 목표지점으로 바로 옮김 - 1번
-            N = 2일 때 - 1번 + (N - 1) * 2 - 3번
-                1) N - 1(1번)을 중간지점으로 옮긴다.
-                2) N을 목표지점으로 옮김.(1번)
-                3) N - 1(1번)을 다시 목표지점으로 옮긴다.
-            N = 3일 때 - 1번 + (N - 1) * 2 - 7번
-                1) N - 2(3번)를 중간 지점으로 옮긴다.
-                2) N을 목표지점으로 옮긴다.(1번)
-                3) N - 2번(3번)을 다시 목표으로 옮긴다.
-            즉, 등비가 2고 초항이 r인 등비수열의 합 공식 : 2^n - 1
-         */
-        System.out.println((int) (Math.pow(2, n) - 1));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = (1 << N) - 1;
+        sb.append(count).append('\n');
+        hanoi(N, 1, 3, 2);
+
         System.out.println(sb);
     }
 
-    // N개의 원판을 cur(현재 지점)에서 goal(목표 지점)로 옮기기
-    private static void hanoi(int n,int cur, int goal, int middle) {
-        // n이 1이면 가장 작은 원판으로 자유롭게 이동이 가능함
-        if(n == 1){
-            sb.append(cur + " " + goal + "\n");
-            return;
-        }
+    private static void hanoi(int n, int from, int to, int mid) {
+        if(n == 0) return;
 
-        // n을 목표지점으로 옮기기 위해서는 n-1(즉, n위에 있는 원판들)개의 원판을 middle로 옮겨야 한다.
-        hanoi(n - 1, cur, middle, goal);
-        // n-1개를 middle(현재 지점과 목표지점이 아닌 다른 곳)으로 옮겼다면 현재 원판을 goal로 옮길 수 있음
-        sb.append(cur + " " + goal + "\n");
-        // n개 원판들을 중간지점(목표지점과 현재지점이 아닌 곳)에서 목표지점으로 다시 옮겨줘야한다.
-        hanoi(n - 1, middle, goal, cur);
+        hanoi(n - 1, from, mid, to);
+        sb.append(top[from - 1]).append(' ').append(top[to - 1]).append('\n');
+        hanoi(n - 1, mid, to, from);
     }
 }
