5afe99ebc3d0afeb1c5bd8735fd6346348d98cbd
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-04 00:09:58 +0900

    [Silver I] Title: 상어 키우기, Time: 548 ms, Memory: 55856 KB -BaekjoonHub

--- 백준/Silver/30892. 상어 키우기/상어 키우기.java
+++ 백준/Silver/30892. 상어 키우기/상어 키우기.java
@@ -14,27 +14,35 @@ public class Main {
         long T = Integer.parseInt(st.nextToken()); // 최초 크기
 
         // 상어의 배열
-        int[] sharks = new int[N];
+        long[] sharks = new long[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            sharks[i] = Integer.parseInt(st.nextToken());
+            sharks[i] = Long.parseLong(st.nextToken());
         }
+
         // 정렬을 해준다.
         Arrays.sort(sharks);
 
-        Stack<Integer> stack = new Stack<>();
+        Stack<Long> stack = new Stack<>();
         int count = 0; // 현재 샼이 먹은 상어의 개수
-        for (int shark : sharks) {
+        for (long shark : sharks) {
             // 스택이 비지않고 && 샼이 현재 바라보는 상어보다 작거나 같고 && K만큼 먹지 않았을 때
             while (!stack.isEmpty() && shark >= T && count < K) {
                 // 먹을 수 있는 가장 큰 상어를 먹어준 뒤 먹은 개수를 늘려준다.
                 T += stack.pop();
                 count++;
             }
-            // 만약 K만큼 먹었거나 || 값을 처리했는데도 바라보는 상어보다 작거나 같다면 종료
-            if (count == K || shark >= T) {
+            // 만약 K만큼 먹었다면 출력
+            if (count == K) {
+                System.out.println(T);
+                return;
+            }
+
+            // 더 이상 못넣는다면 나간다.
+            if (shark >= T) {
                 break;
             }
+
             // 위 연산 수행 후 shark는 샼보다 작은 상어일 것이다. 스택에 넣어준다.
             stack.add(shark);
         }
@@ -43,6 +51,7 @@ public class Main {
         for (int i = 0; i < K - count && !stack.isEmpty(); i++) {
             T += stack.pop();
         }
+
         // 출력
         System.out.println(T);
     }
