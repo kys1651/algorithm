f03eacb029b7b2ec38e884801f9b3ff415ff94ef
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-04 00:31:33 +0900

    [Silver V] Title: 뒤집기, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -7,8 +7,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        int zero = new StringTokenizer(input, "1").countTokens();
-        int one = new StringTokenizer(input, "0").countTokens();
-        System.out.println(Math.min(zero, one));
+        int zero = 0;
+        int one = 0;
+
+        if (input.charAt(0) == '0') zero++;
+        else one++;
+
+        for (int i = 1; i < input.length(); i++) {
+            // 값이 변경되는 순간
+            if (input.charAt(i - 1) != input.charAt(i)) {
+                if (input.charAt(i) == '0') zero++;
+                else one++;
+            }
+        }
+
+        if(zero > one){
+            System.out.println(one);
+        }else{
+            System.out.println(zero);
+        }
     }
 }
