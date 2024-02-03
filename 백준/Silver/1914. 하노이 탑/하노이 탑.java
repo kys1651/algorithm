d63e4c36a887870f691f25214d235410413fb7aa
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-03 13:50:30 +0900

    [Silver I] Title: 하노이 탑, Time: 252 ms, Memory: 41256 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.java
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.java
@@ -5,30 +5,26 @@ import java.math.BigInteger;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        if (n <= 30) {
-            sb.append((1 << n) - 1).append("\n");
-        }else{
-            sb.append(new BigInteger("1").shiftLeft(n).subtract(new BigInteger("1"))).append("\n");
-        }
+        BigInteger count = new BigInteger("1");
+        sb.append(count.shiftLeft(n).subtract(new BigInteger("1"))).append('\n');
 
         if (n <= 20) {
             hanoi(n, 1, 3);
         }
-        
+
         System.out.println(sb);
     }
 
     private static void hanoi(int n, int from, int to) {
-        if(n == 1){
-            sb.append(from).append(" ").append(to).append("\n");
-            return;
-        }
+        if (n == 0) return;
+
         hanoi(n - 1, from, 6 - from - to);
-        sb.append(from).append(" ").append(to).append("\n");
+        sb.append(from).append(' ').append(to).append("\n");
         hanoi(n - 1, 6 - from - to, to);
     }
 }
