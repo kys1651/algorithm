50debdbefab1cab4be6a36bb29fba2b5bea83e87
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-04 17:39:51 +0900

    [Gold V] Title: 탑, Time: 696 ms, Memory: 105996 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -4,64 +4,29 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Top {
-    // 탑의 번호
-    int idx;
-    // 탑의 높이
-    int height;
-
-    Top(int idx, int height) {
-        this.idx = idx;
-        this.height = height;
-    }
-}
-
-
 public class Main {
-
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
+        int[] top = new int[n];
 
-        Stack<Top> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = 1; i <= n; i++) {
-            int height = Integer.parseInt(st.nextToken());
-
-            // stack이 비어있다면 앞에 탑은 현재 탑보다 큰 것이 없으므로 0을 추가한다.
-            // 그 후 스택에 탑을 넣어줌
-            if (stack.isEmpty()) {
-                answer.append("0 ");
-                stack.push(new Top(i, height));
-            } else {
-                // 스택이 비어있지 않다면 while문을 실행
-                while (true) {
-                    // 스택이 비어있을 때 까지 while문을 돌린다면 현재 탑보다 더 큰 탑은 없던 것임
-                    // 위와 같은 operation을 취함
-                    if (stack.isEmpty()) {
-                        answer.append("0 ");
-                        stack.push(new Top(i, height));
-                        break;
-                    }
-                    // 현재 위치에서 가장 가까운 탑을 꺼내줌
-                    Top top = stack.peek();
-
-                    // 가장 가까운 Top의 높이가 현재 위치보다 크다면 answer에 추가해준다.
-                    if (top.height > height) {
-                        answer.append(top.idx + " ");
-                        stack.push(new Top(i, height));
-                        break;
-                    } else {
-                        // 가장 가까운 Top의 높이가 현재 위치보다 크다면 스택에 있는 탑은 제거해도 된다.
-                        stack.pop();
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            top[i] = Integer.parseInt(st.nextToken());
+            while (!stack.isEmpty() && top[stack.peek()] < top[i]) {
+                stack.pop();
             }
+            if (stack.isEmpty()) {
+                sb.append('0');
+            }else{
+                sb.append(stack.peek() + 1);
+            }
+            sb.append(' ');
+            stack.push(i);
         }
 
-        System.out.println(answer.toString());
+        System.out.println(sb);
     }
 }
