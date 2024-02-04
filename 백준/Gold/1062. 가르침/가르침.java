03b6bab46b4908eb5c6793096e3ae3ebaac93c85
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-04 17:03:22 +0900

    [Gold IV] Title: 가르침, Time: 132 ms, Memory: 12988 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,51 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K, limit, result;
-    static String[] word;
-    static HashSet<Character> set = new HashSet<>();
-    static char[] alpha;
+    static int N, K, bit, result;
+    static int[] words;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        word = new String[N];
 
-
-        for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            word[i] = input;
-            for (int j = 0; j < input.length(); j++) {
-                set.add(input.charAt(j));
-            }
+        // 필수로 배워야하는 단어들의 위치를 켜준다.
+        String alphabet = "antic";
+        for (char ch : alphabet.toCharArray()) {
+            bit = bit | (1 << ch - 'a');
         }
 
+
+        words = new int[N];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            // input값이 필요한 알파벳을 켜준다. bit는 기본으로 가지고 있어야 함.
+            int inputBit = bit;
+            for (int j = 4; j < input.length() - 4; j++) {
+                inputBit = inputBit | (1 << input.charAt(j) - 'a');
+            }
+            // words에 넣어준다.
+            words[i] = inputBit;
+        }
+
+        // K가 5개 이하면 배울 수 없음
         if (K < 5) {
             System.out.println(0);
             return;
         }
-
-        alpha = new char[set.size() - 5];
-
-        for (char ch : set) {
-            if (ch == 'a' || ch == 't' || ch == 'n' || ch == 'i' || ch == 'c') {
-                continue;
-            }
-            alpha[limit++] = ch;
+        // K가 26이라면 모든 알파벳을 배울 수 있음
+        else if (K == 26) {
+            System.out.println(N);
+            return;
         }
 
-        set.clear();
-        set.add('a');
-        set.add('n');
-        set.add('t');
-        set.add('i');
-        set.add('c');
+        // 5개를 제외한 나머지만 계산 하면 된다.
+        K -= 5;
 
         combination(0, 0);
 
@@ -53,32 +52,40 @@ public class Main {
     }
 
     private static void combination(int depth, int at) {
-        if (depth > K - 5) return;
-        int count = getWordCount();
-        if (result < count) result = count;
-
-        if (result == N) {
-            System.out.println(N);
-            System.exit(0);
-        }
-
-        for (int i = at; i < limit; i++) {
-            set.add(alpha[i]);
-            combination(depth + 1, i + 1);
-            set.remove(alpha[i]);
-        }
-    }
-
-    private static int getWordCount() {
-        int count = N;
-        for (int i = 0; i < N; i++) {
-            for (int j = 4; j < word[i].length() - 4; j++) {
-                if (!set.contains(word[i].charAt(j))) {
+        if (depth == K) {
+            int count = N;
+            for (int word : words) {
+                if ((bit | word) != bit) {
+                    // 만약 모르는 단어가 있다면 카운트를 감소.
                     count--;
-                    break;
+                    // 최대 단어 개수보다 적어진다면 더 이상 체크 필요 X
+                    if (result > count) {
+                        return;
+                    }
                 }
             }
+            result = count;
+            if (result == N) {
+                System.out.println(N);
+                System.exit(0);
+            }
+            return;
         }
-        return count;
+
+        // a ~ z 는 0 ~ 25이다.
+        for (int i = at; i < 26; i++) {
+            // 알고 있다면 넘어간다.
+            if ((bit & (1 << i)) != 0) {
+                continue;
+            }
+
+            // 모르고 있었다면 비트를 켜줌
+            bit = bit | (1 << i);
+            combination(depth + 1, i + 1);
+            // 다시 꺼준다.
+            bit = bit & ~(1 << i);
+
+        }
     }
+
 }
