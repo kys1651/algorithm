5bf01bd2b091f0104743ee83a009e4175093ea50
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-05 16:28:07 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 109 ms, Memory: 18,680 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -1,35 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-class Solution {
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        for(int tc = 1; tc <= 10; tc++) {
-            int n = Integer.parseInt(br.readLine());
-            LinkedList<Integer> list = new LinkedList<>();
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < n; i++){
-                list.add(Integer.parseInt(st.nextToken()));
-            }
-            int command =  Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < command; i++){
-                st.nextToken();
-                int pos = Integer.parseInt(st.nextToken());
-                int count = Integer.parseInt(st.nextToken());
-                for(int j =0; j < count; j++){
-                    list.add(pos+j,Integer.parseInt(st.nextToken()));
-                }
-            }
 
-            sb.append("#" + tc+ " ");
-            for(int i = 0; i < 10; i++){
-                sb.append(list.get(i)+ " " );
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
-    }
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = 10;
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			LinkedList<Integer> list = new LinkedList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; i++) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			int k = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < k; i++) {
+				st.nextToken();
+				int idx = Integer.parseInt(st.nextToken());
+				int count = Integer.parseInt(st.nextToken());
+				for(int j = 0; j < count; j++) {
+					list.add(idx+j,Integer.parseInt(st.nextToken()));
+				}
+			}
+			
+			sb.append('#').append(tc).append(' ');
+			for(int i = 0; i < 10; i++) {
+				sb.append(list.get(i)).append(' ');
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
 }
