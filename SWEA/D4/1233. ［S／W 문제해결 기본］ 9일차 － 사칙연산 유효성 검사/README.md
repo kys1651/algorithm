b0f579e62a1c759963096ced1d562a35e4782f50
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-06 11:28:28 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 123 ms, Memory: 20,980 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/README.md
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/README.md
@@ -1,14 +1,14 @@
 # [D4] [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사 - 1233 
 
 [문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV141176AIwCFAYD) 
- 
+
 ### 성능 요약
 
-메모리: 18,612 KB, 시간: 105 ms, 코드길이: 1,313 Bytes
+메모리: 20,980 KB, 시간: 123 ms, 코드길이: 1,034 Bytes
 
 ### 제출 일자
 
-2024-01-04 23:18
+2024-02-06 11:28
 
 
 
--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -1,36 +1,40 @@
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution {
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        for(int tc = 1; tc <= 10; tc++) {
-            int n = Integer.parseInt(br.readLine());
-            int answer = 1;
-            // 값을 입력 받을 때는 접근을 인덱스로 하기 위해서 배열로 한다.
-            // 연산자면 false, 피연산자라면 true
-            for(int i = 1; i <= n; i++){
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                st.nextToken();
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = 10;
 
-                char value = st.nextToken().charAt(0);
-                boolean digit = Character.isDigit(value);
-                if (st.hasMoreTokens()){
-                    // 리프 노드가 아니라면 연산자여야한다.
-                    if(digit){
-                        answer = 0;
-                    }
-                }else{
-                    // 리프 노드라면 숫자여야한다.
-                    if(!digit){
-                        answer = 0;
-                    }
-                }
-            }
-            sb.append("#" + tc + " " + answer).append("\n");
-        }
-        System.out.println(sb);
-    }
+		for (int tc = 1; tc <= T; tc++) {
+			int n = Integer.parseInt(br.readLine());
+			char[] tree = new char[n + 1];
+
+			StringTokenizer st;
+			for (int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				st.nextToken();
+				tree[i] = st.nextToken().charAt(0);
+			}
+
+			int result = 1;
+			for (int i = 1; i <= n; i++) {
+				if (Character.isDigit(tree[i])) {
+					if (i * 2 <= n && Character.isDigit(tree[i * 2])) {
+						result = 0;
+						break;
+					}
+					if (i * 2 + 1 <= n && Character.isDigit(tree[i * 2 + 1])) {
+						result = 0;
+						break;
+					}
+				}
+			}
+			sb.append('#').append(tc).append(' ').append(result).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
