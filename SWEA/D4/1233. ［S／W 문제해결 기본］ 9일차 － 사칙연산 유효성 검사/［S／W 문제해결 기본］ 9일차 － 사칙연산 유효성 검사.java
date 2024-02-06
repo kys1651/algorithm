4c3e00ac7e61e31594e0d4b212dfea94b623ab0b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-06 11:32:52 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 117 ms, Memory: 21,004 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -13,26 +13,20 @@ public class Solution {
 			int n = Integer.parseInt(br.readLine());
 			char[] tree = new char[n + 1];
 
+			int result = 1;
 			StringTokenizer st;
 			for (int i = 1; i <= n; i++) {
 				st = new StringTokenizer(br.readLine());
 				st.nextToken();
 				tree[i] = st.nextToken().charAt(0);
-			}
-
-			int result = 1;
-			for (int i = 1; i <= n; i++) {
-				if (Character.isDigit(tree[i])) {
-					if (i * 2 <= n && Character.isDigit(tree[i * 2])) {
+				if(Character.isDigit(tree[i])) {
+					if(st.hasMoreTokens()) {
 						result = 0;
-						break;
-					}
-					if (i * 2 + 1 <= n && Character.isDigit(tree[i * 2 + 1])) {
-						result = 0;
-						break;
 					}
 				}
 			}
+
+			
 			sb.append('#').append(tc).append(' ').append(result).append('\n');
 		}
 		System.out.println(sb);
