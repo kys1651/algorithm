f6746c729cc0f5994128277e0c8224c5531e42b2
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-06 17:57:08 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 208 ms, Memory: 32,720 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,9 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 문제: D3_9229_한빈이와 SpotMart
+ * 
+ * @author 김용수 실행시간: 153ms 메모리 : 29848kb
+ * 
+ *         접근 방법: 1. 정렬 후 투포인터를 이용하여 계산을 해준다.
+ */
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
@@ -15,6 +21,7 @@ public class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
+			// 과자를 담아주는 배열
 			int[] snack = new int[n];
 
 			st = new StringTokenizer(br.readLine());
@@ -22,24 +29,16 @@ public class Solution {
 				snack[i] = Integer.parseInt(st.nextToken());
 			}
 
-			Arrays.sort(snack);
-
-			int left = 0, right = n - 1;
-			int sum = -1;
-			while (left < right) {
-				int tmp = snack[left] + snack[right];
-				if (tmp < m) {
-					if (sum < tmp)
+			int sum = 0;
+			for (int i = 0; i < n - 1; i++) {
+				for (int j = i + 1; j < n; j++) {
+					int tmp = snack[i] + snack[j];
+					if (tmp <= m && tmp > sum)
 						sum = tmp;
-					left++;
-				} else if (tmp == m) {
-					sum = m;
-					break;
-				} else {
-					right--;
 				}
 			}
-			sb.append(String.format("#%d %d\n", tc, sum));
+
+			sb.append(String.format("#%d %d\n", tc, sum == 0 ? -1 : sum));
 		}
 		System.out.println(sb);
 	}
