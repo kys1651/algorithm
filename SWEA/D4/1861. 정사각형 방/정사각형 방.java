4a0ed389c92b85a1735c9b334c9957cd96510e2b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-07 13:15:37 +0900

    [D4] Title: 정사각형 방, Time: 562 ms, Memory: 91,348 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,88 +1,81 @@
-import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Solution {
-    static class Point{
-        int x;
-        int y;
-        int count;
-        
-        Point(int x, int y, int count){
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
-    static int[][] map;
-    static int n,resultCount,resultNum;
-    static int[] dirX = {-1,1,0,0};
-    static int[] dirY = {0,0,-1,1};
-	public static void main(String args[]) throws Exception {
+public class Solution {
+	static int n;
+	static int[][] map, visit;
+	static int[] dirX = { -1, 1, 0, 0 };
+	static int[] dirY = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc <= T; tc++) {
-            n = Integer.parseInt(br.readLine());
-            map = new int[n][n];
-            resultNum = 0;
-            resultCount = 0;
-            for(int i = 0; i < n; i++){
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < n; j++){
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            
-            // for문을 통해서 전부 bfs를 통해 각 값 확인
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    bfs(new Point(i,j,1));
-                }
-            }
-            sb.append("#" + tc + " " + resultNum + " " + resultCount).append("\n");
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			n = Integer.parseInt(br.readLine());
+
+			map = new int[n][n];
+			visit = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (visit[i][j] == 0) {
+						bfs(i, j);
+					}
+				}
+			}
+
+			int startNum = 0, maxCount = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (visit[i][j] > maxCount) {
+						maxCount = visit[i][j];
+						startNum = map[i][j];
+					}else if(visit[i][j] == maxCount) {
+						startNum = Math.min(map[i][j], startNum);
+					}
+				}
+			}
+			sb.append(String.format("#%d %d %d\n", tc, startNum, maxCount));
 		}
-        System.out.println(sb);
+		System.out.println(sb);
+
 	}
-    
-    private static void bfs(Point start){
-        // 큐 선언 후 시작점을 넣어줌
-        Queue<Point> rooms = new LinkedList<>();
-        rooms.offer(start);
-        
-        while(!rooms.isEmpty()){
-            Point cur = rooms.poll();
-            
-            // 상하좌우 확인 후 범위밖이면 continue
-            for(int i = 0; i < 4; i++){
-                int nX = cur.x + dirX[i];
-                int nY = cur.y + dirY[i];
-                
-                if(nX < 0 || nX >= n || nY < 0 || nY >= n){
-                    continue;
-                }
-                
-                // 다음 방의 번호가 현재 방의 번호 +1 이라면 갈 수 있음
-                if(map[nX][nY] == map[cur.x][cur.y] + 1){
-                    rooms.offer(new Point(nX,nY,cur.count+1));
-                }
-                // 못간다면 resultCount를 최신화 할 수 있다.
-                else if(resultCount <= cur.count){
-                    // 시작 방
-                    int startRoom = map[start.x][start.y];
-                    // 같다면 방의 번호가 더 작은 것으로 최신화
-                    if(resultCount == cur.count){
-                        resultNum = Math.min(resultNum, startRoom);
-                    }else{
-                        resultNum = startRoom;
-                    }
-                    resultCount = cur.count;
-                }
-            }
-        }
-    }
+
+	private static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { x, y });
+		visit[x][y] = 1;
+
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			for (int i = 0; i < 4; i++) {
+				int nX = cur[0] + dirX[i];
+				int nY = cur[1] + dirY[i];
+
+				if (!isCheck(nX, nY)) {
+					continue;
+				}
+				if (map[nX][nY] == map[cur[0]][cur[1]] - 1) {
+					visit[nX][nY] = visit[cur[0]][cur[1]] + 1;
+					queue.offer(new int[] { nX, nY });
+				}
+			}
+		}
+	}
+
+	private static boolean isCheck(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < n;
+	}
 }
