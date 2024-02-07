dc3e3c3abfff7feab08e60bc1b70a1d6e1afcabe
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-07 15:38:50 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,25 +1,31 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] map = new int[100][100];
-        int n = Integer.parseInt(br.readLine());
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int sX = Integer.parseInt(st.nextToken());
-            int sY = Integer.parseInt(st.nextToken());
-            for (int x = sX; x < sX + 10 && x < 100; x++) {
-                for (int y = sY; y < sY + 10 && y < 100; y++) {
-                    if (map[x][y] != 1) {
-                        map[x][y] = 1;
-                        result++;
-                    }
-                }
-            }
-        }
-        System.out.println(result);
-    }
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		boolean[][] map = new boolean[101][101];
+
+		int answer = 0;
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			for (int j = x; j < x + 10; j++) {
+				for (int k = y; k < y + 10; k++) {
+					if (!map[j][k]) {
+						answer++;
+					}
+					map[j][k] = true;
+				}
+			}
+		}
+
+		System.out.println(answer);
+	}
+
 }
