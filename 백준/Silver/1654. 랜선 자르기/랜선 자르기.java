9f101e03569fc3ca534e4071f0da13b7065b94bd
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-07 11:01:38 +0900

    [Silver II] Title: 랜선 자르기, Time: 128 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -3,55 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int K,N;
-    static int [] arr;
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] num = new int[n];
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		long upper = 0, lower = 1;
+		for (int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+			if (num[i] > upper)
+				upper = num[i];
+		}
 
-        // K : 보유중인 랜선의 개수 N : 필요한 랜선의 개수
-        K = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        arr = new int[K];
+		// 홀수인 경우를 대비하여 1을 증가시켜줌
+		upper++;
+		long mid,tmp;
+		while (lower < upper) {
+			mid = (upper + lower) >> 1;
 
-        long max = 0;
+			tmp = 0;
+			for (int number:num) {
+				tmp += (number / mid);
+			}
 
-        for (int i = 0; i < K; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-
-            // 랜선의 최대 길이를 구하기 위해서 for문 안에서 구해줌
-            if(max < arr[i])
-                max = arr[i];
-        }
-
-        max++;
-        long min = 0;
-        long mid = 0;
-        // 이분 탐색
-        while(min < max){
-            mid = (max + min) / 2;
-            long count = 0;
-
-
-            // 만들 수 있는 랜선의 개수 : 랜선의 길이 / 자르는 길이
-            for(int num : arr){
-                count += num/mid;
-            }
-
-            // 만들 수 있는 랜선의 개수가 N보다 크거나 같으면 break;
-            if(count >= N) {
-                min = mid + 1;
-            }else{
-                // 랜선의 개수가 더 적다는 것은 랜선의 길이를 더 줄여야 한다
-                // 즉, 상한을 줄여야 함.
-                max = mid;
-            }
-
-
-        }
-        System.out.println(min-1);
-
-    }
+			// tmp가 k보다 크다면 너무 조금 자른 것
+			if (tmp >= k) {
+				lower = mid + 1;
+			}
+			// tmp가 k보다 작다면 너무 크게 자른 것
+			else if (tmp < k) {
+				upper = mid;
+			}
+		}
+		System.out.println(lower - 1);
+	}
 }
