a64786d2dc3319e9ce2383a5a1c44f599b69875f
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-07 11:23:11 +0900

    [Silver I] Title: 절댓값 힙, Time: 420 ms, Memory: 28072 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,32 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> queue = new PriorityQueue<>(
+				(o1, o2) -> Math.abs(o1) == Math.abs(o2) ? o1 - o2 : Math.abs(o1) - Math.abs(o2));
+		
 		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-			int a1 = Math.abs(o1);
-			int a2 = Math.abs(o2);
-			if (a1 == a2) {
-				return Integer.compare(o1, o2);
-			}
-			return a1 - a2;
-		});
-
+		
 		while (n-- > 0) {
 			int tmp = Integer.parseInt(br.readLine());
 			if (tmp == 0) {
 				if (queue.isEmpty()) {
-					System.out.println(0);
+					sb.append(0);
 				} else {
-					System.out.println(queue.poll());
+					sb.append(queue.poll());
 				}
+				sb.append('\n');
 			} else {
 				queue.add(tmp);
 			}
 		}
+		System.out.println(sb);
 	}
 
 }
