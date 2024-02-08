cd0ff540a1a738b6a8841dfecb02e187cbb6040d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-08 11:37:24 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,101 ms, Memory: 23,732 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,41 +1,61 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution
-{
-    static int result = 0;
-    static int[][] ingredients;
-    static boolean[] visit;
-    static int n,l;
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-            result = -1;
-            n = sc.nextInt();
-            l = sc.nextInt();
-            ingredients = new int[n][2];           
-            visit = new boolean[n];
-            for(int i = 0; i < n; i++){
-                ingredients[i][0] = sc.nextInt();
-                ingredients[i][1] = sc.nextInt();
-            }
-			combination(0,0,0);
-            
-            System.out.println("#" + tc + " " + result);
+public class Solution {
+	static class Hamburger {
+		int taste;
+		int cal;
+
+		public Hamburger(int taste, int cal) {
+			this.taste = taste;
+			this.cal = cal;
 		}
 	}
-    private static void combination(int depth,int curCal, int score){
-        if(curCal > l){
-            return;
-        }
-        if(depth == n){
-            result = Math.max(result , score);
-            return;
-        }
-        combination(depth+1,curCal + ingredients[depth][1], score + ingredients[depth][0]);
-        combination(depth+1,curCal,score);
-    }
+
+	static int N, L, result;
+	static Hamburger[] hamburgers;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			result = -1;
+
+			hamburgers = new Hamburger[N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				hamburgers[i] = new Hamburger(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+
+			for (int i = 1; i < (1 << N); i++) {
+				calculate(i);
+			}
+
+			sb.append(String.format("#%d %d\n", tc, result));
+		}
+		System.out.println(sb);
+	}
+
+	private static void calculate(int flag) {
+		int calSum = 0;
+		int tasteSum = 0;
+		for (int i = 0; i < N; i++) {
+			if ((flag & 1 << i) != 0) {
+				calSum += hamburgers[i].cal;
+				tasteSum += hamburgers[i].taste;
+			}
+			if (calSum > L) {
+				return;
+			}
+		}
+		if (result < tasteSum) {
+			result = tasteSum;
+		}
+	}
 }
