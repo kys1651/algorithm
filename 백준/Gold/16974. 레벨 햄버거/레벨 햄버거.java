9255f12ea2196d13da3e61ee359eaeb758f28614
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-08 19:40:35 +0900

    [Gold V] Title: 레벨 햄버거, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Gold/16974. 레벨 햄버거/레벨 햄버거.java
+++ 백준/Gold/16974. 레벨 햄버거/레벨 햄버거.java
@@ -14,7 +14,6 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         long k = Long.parseLong(st.nextToken());
 
-
         b = new long[n + 1];
         p = new long[n + 1];
         b[0] = 1;
@@ -24,26 +23,32 @@ public class Main {
             p[i] = p[i - 1] * 2 + 1;
         }
 
-        long answer = divide(n, k);
-        System.out.println(answer);
+        System.out.println(divide(n, k));
     }
 
     private static long divide(int n, long k) {
+        // n이 0이면 1
         if (n == 0) {
             return 1;
-        } else if (k == 1) {
+        }
+        // k가 1보다 적거나 같으면 번
+        else if (k <= 1) {
             return 0;
-        } else if (k == b[n]) {
-            return p[n];
         }
 
-        // 앞부분일 때
-        if (k < b[n - 1] + 2) {
+        // 중간지점
+        long mid = b[n - 1] + 2;
+        // 중간지점보다 적은 곳(빵 개수 -1)
+        if (k < mid) {
             return divide(n - 1, k - 1);
-        } else if (k == b[n - 1] + 2) {
-            return 1 + divide(n - 1, k - 2);
-        } else {
-            return p[n - 1] + 1 + divide(n - 1, k - (2 + b[n - 1]));
+        }
+        // 가운데 지점인 경우 가운데 패티(1) + 이전지점
+        else if (k == mid) {
+            return 1 + p[n - 1];
+        }
+        // 그보다 큰 경우 가운데 패티(1) + 이전지점 + 현재 지점
+        else {
+            return p[n - 1] + 1 + divide(n - 1, k - mid);
         }
     }
 }
