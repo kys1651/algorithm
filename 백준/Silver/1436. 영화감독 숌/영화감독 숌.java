5cc02f59c81b100a3eec4a37d8c8cdafc751035a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-08 21:19:56 +0900

    [Silver V] Title: 영화감독 숌, Time: 340 ms, Memory: 158556 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,21 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-class Main {
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-
-        int N = Integer.parseInt(br.readLine());
-        int i = 0, count =0;
-        while(count != N){
-            i++;
-            String tmp = String.valueOf(i);
-            if(tmp.indexOf("666")!=-1) count ++;
+        int n = Integer.parseInt(br.readLine());
+        int value = 666;
+        int count = 1;
+        while(count != n){
+            value++;
+            if(String.valueOf(value).contains("666")){
+                count++;
+            }
         }
-        System.out.println(i);
+        System.out.println(value);
     }
 }
