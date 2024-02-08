5eed050a3c0dcd0cd9cfbc5acb785106377305e5
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-09 00:51:49 +0900

    [Gold V] Title: 리모컨, Time: 352 ms, Memory: 100548 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,53 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    static boolean[] check;
+    static boolean[] button = new boolean[10];
     static int N, M;
-    static int min;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        check = new boolean[10]; // 0 ~ 9까지 숫자 버튼
 
-        if(M != 0) { // 고장난 버튼이 있을 때
+        if (N == 100) {
+            System.out.println(0);
+            return;
+        } else if (M == 10) {
+            System.out.println(Math.abs(N - 100));
+            return;
+        }
+
+        if (M != 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            while (M-- > 0) {
-                // 고장난 버튼의 인덱스를 0으로 교채해줌
-                check[Integer.parseInt(st.nextToken())] = true;
+            for (int i = 0; i < M; i++) {
+                // true는 사용하지 못하는 버튼
+                button[Integer.parseInt(st.nextToken())] = true;
             }
         }
 
-        solution();
-
-        System.out.println(min);
+        combination();
     }
 
-    private static void solution() {
-        // 목표 위치에서 100을 뺀  +, -로만 간 횟수로 초기화
-        min = Math.abs(N - 100);
+    private static void combination() {
+        int min = Math.abs(N - 100);
 
-        // 최대값은 500,000이므로 6자리 최대값인 999,999까지 돌려줌
         for (int i = 0; i < 999999; i++) {
-            String str = String.valueOf(i);
-            boolean flag = false;
+            String num = String.valueOf(i);
+            boolean flag = true;
 
-            for(int j = 0 ; j < str.length(); j++){
-                if(check[str.charAt(j)-'0']) {
-                    flag = true;
+            for (char ch : num.toCharArray()) {
+                if(button[ch-'0']) {
+                    flag = false;
                     break;
                 }
             }
 
-            if (!flag) {
-                min = Math.min(str.length() + Math.abs(N - i), min);
+            if(flag){
+                min = Math.min(num.length() + Math.abs(N - i), min);
             }
         }
+        System.out.println(min);
     }
 }
