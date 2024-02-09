d7821fa66d8d2a4579aea256f1f64f32705c2633
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-09 15:12:43 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 12032 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
     // 배열의 길이
