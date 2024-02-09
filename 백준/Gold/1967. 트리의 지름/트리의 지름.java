dcacf3bca89ae612fd9518773aadfaf63a5bb0e7
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-09 14:40:56 +0900

    [Gold IV] Title: 트리의 지름, Time: 188 ms, Memory: 19972 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -17,15 +17,14 @@ public class Main {
     }
 
     static ArrayList<Node>[] tree;
-    static boolean[] isParent,visit;
-    static int N, answer;
+    static boolean[] visit;
+    static int N, max, maxNode = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        tree = new ArrayList[N + 1];
-        isParent = new boolean[N + 1];
 
+        tree = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             tree[i] = new ArrayList<>();
         }
@@ -39,32 +38,27 @@ public class Main {
 
             tree[parent].add(new Node(child, weight));
             tree[child].add(new Node(parent, weight));
-
-            isParent[parent] = true;
         }
+        visit = new boolean[N + 1];
+        visit[1] = true;
+        dfs(1, 0);
 
-        if(tree[1].size() == 1){
-            isParent[1] = false;
-        }
-        for (int i = 1; i <= N; i++) {
-            if (isParent[i]) continue;
+        visit = new boolean[N + 1];
+        visit[maxNode] = true;
+        dfs(maxNode, 0);
 
-            visit = new boolean[N + 1];
-            dfs(i, 0);
-        }
-
-        System.out.println(answer);
+        System.out.println(max);
     }
 
     private static void dfs(int node, int cost) {
-        if (!isParent[node] && cost != 0) {
-            if(answer < cost) answer = cost;
-            return;
+        if (max < cost) {
+            max = cost;
+            maxNode = node;
         }
 
-        visit[node] = true;
         for (Node next : tree[node]) {
-            if(visit[next.idx]) continue;
+            if (visit[next.idx]) continue;
+            visit[next.idx] = true;
             dfs(next.idx, cost + next.weight);
         }
     }
