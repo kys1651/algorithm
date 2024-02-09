f95ef7462fc8e020dc80ee42a84c3b5b5bb92e09
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-09 15:38:23 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 120384 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,69 +6,59 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
+    static int M, N, day, empty;
     static int[][] map;
-    static Queue<int[]> queue = new LinkedList<>();
+    static Queue<int[]> tomato = new LinkedList<>();
+
+    static int[] dirX = {-1, 1, 0, 0};
+    static int[] dirY = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        for (int i = 0; i < map.length; i++) {
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < map[i].length; j++) {
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if (map[i][j] == 1) {
-                    queue.offer(new int[] {i, j});
+                    tomato.add(new int[]{i, j});
+                } else if (map[i][j] == 0) {
+                    empty++;
                 }
             }
         }
+
         bfs();
-        int result = -1;
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (map[i][j] == 0) {
-                    result = 0;
-                    break;
-                } else {
-                    result = Math.max(result, map[i][j]);
-                }
-            }
-            if (result == 0) {
-                break;
-            }
-        }
-        System.out.println(result - 1);
+        System.out.println(day - 1);
     }
 
     private static void bfs() {
-        int[] dirX = {-1, 1, 0, 0};
-        int[] dirY = {0, 0, -1, 1};
+        while (!tomato.isEmpty()) {
+            int count = tomato.size();
+            day++;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
+            for (int c = 0; c < count; c++) {
+                int[] cur = tomato.poll();
 
-            for (int k = 0; k < size; k++) {
-                int[] tomato = queue.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nX = cur[0] + dirX[i];
+                    int nY = cur[1] + dirY[i];
 
-                for (int d = 0; d < 4; d++) {
-                    int nX = tomato[0] + dirX[d];
-                    int nY = tomato[1] + dirY[d];
-
-                    // 배열의 범위 밖인 경우
-                    if (nX < 0 || nX >= map.length || nY < 0 || nY >= map[nX].length) {
+                    if (nX < 0 || nX >= N || nY < 0 || nY >= M || map[nX][nY] != 0) {
                         continue;
                     }
-                    // 익지 않은 토마토인 경우
-                    if (map[nX][nY] == 0) {
-                        map[nX][nY] = map[tomato[0]][tomato[1]] + 1;
-                        queue.offer(new int[] {nX, nY});
-                    }
+                    map[nX][nY] = 1;
+                    tomato.add(new int[]{nX, nY});
+                    empty--;
                 }
             }
         }
+        if (empty != 0) {
+            day = 0;
+        }
     }
-
 }
