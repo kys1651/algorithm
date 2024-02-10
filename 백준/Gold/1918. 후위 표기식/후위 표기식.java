3b9bebee2d557d8b3ee4850d05fa384bc2412ae0
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-11 01:54:22 +0900

    [Gold II] Title: 후위 표기식, Time: 72 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -10,19 +10,33 @@ public class Main {
         String input = br.readLine();
         Stack<Character> operator = new Stack<>();
 
-        for (char ch : input.toCharArray()) {
-            if (Character.isAlphabetic(ch)) {
+        int len = input.length();
+        for (int i = 0; i < len; i++) {
+            char ch = input.charAt(i);
+            
+            // StringBuilder에 추가
+            if (ch >= 'A' && ch <= 'Z') {
                 sb.append(ch);
-            }else if(ch == '('){
+                continue;
+            }
+            // 스택이 비어있다면 push
+            if (operator.isEmpty()) {
+                operator.push(ch);
+                continue;
+            }
+
+            // 여는 괄호(연산자 0순위) - 추가
+            if (ch == '(') {
                 operator.push(ch);
             } else if (ch == ')') {
-                // 여는 괄호 만날 때 까지 Sb에 추가
-                while (!operator.isEmpty() && operator.peek() != '(') {
+                // 닫는 괄호(연산자 0순위) - 여는 괄호 만날 때 까지 sb에 추가
+                while (operator.peek() != '(') {
                     sb.append(operator.pop());
                 }
-                // 여는 괄호를 빼준다.
-                operator.pop();
-            } else if (ch == '*' || ch == '/') {
+                operator.pop(); // 여는 괄호를 빼준다.
+            }
+            // *,/ (연산자 1순위) - *,/만 제거
+            else if (ch == '*' || ch == '/') {
                 // 곱하기 혹은 나누기를 만날 때까지 전부 제거함
                 while (!operator.isEmpty() && (operator.peek() == '*' || operator.peek() == '/')) {
                     sb.append(operator.pop());
@@ -30,20 +44,21 @@ public class Main {
                 // 현재 연산자를 스택에 삽입
                 operator.push(ch);
             }
-            // 더하기 혹은 빼기일 경우
+            // 더하기 혹은 빼기(연산자 최하위) - ( 제외하고 모두 추가해줘야함
             else if (ch == '+' || ch == '-') {
                 // 여는 괄호가 아니면 전부 제거 함
-                while(!operator.isEmpty() && (operator.peek() != '(')){
+                while (!operator.isEmpty() && (operator.peek() != '(')) {
                     sb.append(operator.pop());
                 }
                 operator.push(ch);
             }
         }
 
-
+        // 남는 괄호가 있다면 Sb에 추가
         while (!operator.isEmpty()){
             sb.append(operator.pop());
         }
+
         System.out.println(sb);
     }
 }
