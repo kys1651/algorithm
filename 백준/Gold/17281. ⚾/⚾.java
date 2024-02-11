809bef125c0a0043256e69db0892fa9ec7fe545e
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-11 15:41:30 +0900

    [Gold IV] Title: ⚾, Time: 408 ms, Memory: 12564 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -32,12 +32,6 @@ public class Main {
     }
 
     private static void getPermutation(int depth) {
-        // 3번째는 구할 필요 X이미 정했기 때문에
-        if (depth == 3) {
-            getPermutation(depth + 1);
-            return;
-        }
-
         // 9명의 순열을 구했다면 게임 진행
         if (depth == 9) {
             playGame();
@@ -48,7 +42,13 @@ public class Main {
             if (visit[i]) continue;
             visit[i] = true;
             permutation[depth] = i;
-            getPermutation(depth + 1);
+
+            // 4번째타자[3]는 구할 필요 없음
+            if(depth == 2){
+                getPermutation(depth + 2);
+            }else{
+                getPermutation(depth + 1);
+            }
             visit[i] = false;
         }
     }
@@ -73,22 +73,20 @@ public class Main {
                 if (getScore == 0) {
                     outCount++;
                 }
-                // 아닌 경우 점수를 얻어온다.
+                // 안타인 경우 -> 각 주자 1루씩 진출
                 else if (getScore == 1) {
                     gameScore += base3;
-                    base3 = 0;
                     base3 = base2;
                     base2 = base1;
                     base1 = 1;
                 } else if (getScore == 2) {
                     gameScore += base3 + base2;
-                    base2 = base3 = 0;
                     base3 = base1;
                     base1 = 0;
                     base2 = 1;
                 } else if (getScore == 3) {
                     gameScore += base1 + base2 + base3;
-                    base1 = base2 = base3 = 0;
+                    base1 = base2 = 0;
                     base3 = 1;
                 } else if (getScore == 4) {
                     gameScore += base1 + base2 + base3 + 1;
@@ -99,63 +97,5 @@ public class Main {
         }
 
         if (result < gameScore) result = gameScore;
-    }
-
-    private static int isHit(int hit, int[] state) {
-        int score = 0;
-
-        // 홈런인 경우 각 주자들의 수 + 타자 수 점수 획득
-        if (hit == 4) {
-            for (int i = 1; i <= 3; i++) {
-                if (state[i] == 1) {
-                    score++;
-                    state[i] = 0;
-                }
-            }
-            score++;
-        }
-        // 3루타인 경우 주자들의 수만큼 점수
-        else if (hit == 3) {
-            for (int i = 1; i <= 3; i++) {
-                if (state[i] == 1) {
-                    score++;
-                    state[i] = 0;
-                }
-            }
-            // 3루에 타자 배치
-            state[3] = 1;
-        }
-        // 2루타인 경우 2루타부터 3루타 사람 수 만큼 점수 획득
-        else if (hit == 2) {
-            if (state[3] == 1) {
-                score++;
-                state[3] = 0;
-            }
-            if (state[2] == 1) {
-                score++;
-                state[2] = 0;
-            }
-            // 1루타에 사람이 있었다면 3루로 진출
-            if (state[1] == 1) {
-                state[3] = 1;
-            }
-            state[1] = 1;
-        }
-        // 안타라면 3루에 있는 타자는 점수 나머지는 한루씩 진루
-        else if (hit == 1) {
-            if (state[3] == 1) {
-                score++;
-                state[3] = 0;
-            }
-            for (int i = 2; i >= 1; i--) {
-                if (state[i] == 1) {
-                    state[i + 1] = 1;
-                    state[i] = 0;
-                }
-            }
-            state[1] = 1;
-        }
-
-        return score;
     }
 }
