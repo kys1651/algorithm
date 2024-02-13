a1ad8e6458caf0cbb3627b16d7ad5e68db7b5b4d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-13 11:44:21 +0900

    [Silver III] Title: 이전 순열, Time: 116 ms, Memory: 13468 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.java
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -3,55 +3,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < n ; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		if(!pp(arr)) {
-			sb.append(-1);
-		}else {
-			for(int a : arr) {
-				sb.append(a).append(' ');
-			}
+
+		int i = n - 1;
+		while (i - 1 >= 0 && arr[i - 1] <= arr[i]) {
+			i--;
+		}
+
+		if (i == 0) {
+			System.out.println(-1);
+			return;
+		}
+
+		int j = n-1;
+		for(; arr[i-1] < arr[j]; j--) {}
+		swap(arr, i - 1, j);
+
+		for (j = n - 1; i < j; i++, j--) {
+			swap(arr, i, j);
+		}
+
+		for (int a : arr) {
+			sb.append(a).append(' ');
 		}
 		System.out.println(sb);
 	}
-	
-	private static boolean pp(int[] p) {
-		int i = p.length - 1;
-		while(i - 1 >= 0 && p[i-1] <= p[i]) {
-			i--;
-		}
-		
-		if(i == 0) {
-			return false;
-		}
-		
-		int max = i;
-		for(int j = i; j < p.length; j++) {
-			if(p[i-1] > p[j] && p[max] < p[j]) {
-				max = j;
-			}
-		}
 
-		swap(p,i-1,max);
-		int j = p.length - 1;
-		while(i < j) {
-			swap(p,i++,j--);
-		}
-		return true;
-	}
-	
-	private static void swap(int[] p, int i , int j) {
+	private static void swap(int[] p, int i, int j) {
 		int tmp = p[i];
 		p[i] = p[j];
 		p[j] = tmp;
