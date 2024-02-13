020c60d12ba0acffc85984388fd99f027ef8a702
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-13 11:48:07 +0900

    [Silver III] Title: 이전 순열, Time: 116 ms, Memory: 13456 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.java
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -7,7 +7,6 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
 
@@ -16,30 +15,37 @@ public class Main {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
+		// 현재 상승하는 부분 찾기
 		int i = n - 1;
 		while (i - 1 >= 0 && arr[i - 1] <= arr[i]) {
 			i--;
 		}
 
+		// i가 0이라면 더 이상 찾을 수 없는 것
 		if (i == 0) {
 			System.out.println(-1);
 			return;
 		}
 
+		// 처음으로 만나는 i-1(swap 할 값)보다 작은 값과 교환
 		int j = n-1;
 		for(; arr[i-1] < arr[j]; j--) {}
 		swap(arr, i - 1, j);
 
+		// 뒷 부분 정렬을 위해서 전부 swap 
 		for (j = n - 1; i < j; i++, j--) {
 			swap(arr, i, j);
 		}
 
+		// Stringbuilder에 추가
+		StringBuilder sb = new StringBuilder();
 		for (int a : arr) {
 			sb.append(a).append(' ');
 		}
 		System.out.println(sb);
 	}
 
+	// 배열의 원소를 교환
 	private static void swap(int[] p, int i, int j) {
 		int tmp = p[i];
 		p[i] = p[j];
