9995f0aad8ee21b7be5ab023eb3177fa71422438
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-14 17:24:53 +0900

    [Gold IV] Title: 월드컵, Time: 688 ms, Memory: 17008 KB -BaekjoonHub

---
+++ 백준/Gold IV/6987. 월드컵/월드컵.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final int SIZE = 6;
+
+	static StringBuilder sb = new StringBuilder();
+
+	static int[] win = new int[SIZE];
+	static int[] draw = new int[SIZE];
+	static int[] lose = new int[SIZE];
+	static boolean result;
+	// 일어날 수 있는 경우의 수
+	static int[][] match = { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 }, { 0, 5 }, { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 },
+			{ 2, 3 }, { 2, 4 }, { 2, 5 }, { 3, 4 }, { 3, 5 }, { 4, 5 } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 6; j++) {
+				win[j] = Integer.parseInt(st.nextToken());
+				draw[j] = Integer.parseInt(st.nextToken());
+				lose[j] = Integer.parseInt(st.nextToken());
+			}
+
+			result = false;
+
+			combination(0);
+
+			if (result) {
+				sb.append(1);
+			} else {
+				sb.append(0);
+			}
+
+			sb.append(' ');
+		}
+		System.out.println(sb);
+	}
+
+	// 여기서는 승점만 따진다.
+	private static void combination(int depth) {
+		if (result)
+			return;
+
+		if (depth == 15) {
+			for(int i = 0; i < 6; i++) {
+				if(win[i] != 0 || draw[i] != 0 || lose[i] != 0) {
+					return;
+				}
+			}
+			result = true;
+			return;
+		}
+
+		int a = match[depth][0];
+		int b = match[depth][1];
+
+		// a가 이김
+		win[a]--;
+		lose[b]--;
+		combination(depth + 1);
+		win[a]++;
+		lose[b]++;
+
+		// 무승부
+		draw[a]--;
+		draw[b]--;
+		combination(depth + 1);
+		draw[a]++;
+		draw[b]++;
+
+		// b가 이긴 경우
+		win[b]--;
+		lose[a]--;
+		combination(depth + 1);
+		win[b]++;
+		lose[a]++;
+	}
+
+	private static boolean drawCheck() {
+		int score = 0;
+		for (int i = 0; i < SIZE; i++) {
+			if (draw[i] != 0) {
+				if (score < 0) {
+					score += draw[i];
+				} else {
+					score -= draw[i];
+				}
+			}
+		}
+		return score == 0;
+	}
+}
