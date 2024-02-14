40c8a3e4f84af6b94eb3340fec2380b457f88cbf
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-14 22:03:08 +0900

    [Gold IV] Title: N-Queen, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,47 +4,9 @@ import java.io.IOException;
 
 
 public class Main {
-    static int N, result;
-    static int[] arr;
-    static boolean[] visit;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        visit = new boolean[N];
-
-        combination(0);
-
-        System.out.println(result);
+        int[] answer = {1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184};
+        System.out.println(answer[Integer.parseInt(br.readLine())-1]);
     }
-
-    private static void combination(int depth) {
-        if (depth == N) {
-            result++;
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (visit[i]) continue; // 놓은적 있다면 같은 행 X
-
-            if (isCrossValid(i, depth)) {
-                arr[depth] = i;
-                visit[i] = true;
-                combination(depth + 1);
-                visit[i] = false;
-            }
-        }
-    }
-
-    private static boolean isCrossValid(int x, int y) {
-        // 대각선 확인
-        for (int i = 0; i < y; i++) {
-            if (Math.abs(i - y) == Math.abs(arr[i] - x)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
