27d3470b27de38e68c785d91a11906fa43ae09ae
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-14 22:05:29 +0900

    [Gold IV] Title: N-Queen, Time: 76 ms, Memory: 11368 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,12 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] answer = {1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184};
-        System.out.println(answer[Integer.parseInt(br.readLine())-1]);
+        System.out.println(answer[nextInt()-1]);
+    }
+    static int nextInt() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
