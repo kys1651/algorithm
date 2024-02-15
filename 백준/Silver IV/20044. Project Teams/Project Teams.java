ff15030c638c42cbdcdc8461c162ad3a47b5b97e
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-15 19:21:01 +0900

    [Silver IV] Title: Project Teams, Time: 120 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver IV/20044. Project Teams/Project Teams.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[2 * N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 2 * N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        long answer = Long.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            long nextValue = nums[i] + nums[2 * N - i - 1];
+            if (nextValue < answer) {
+                answer = nextValue;
+            }
+        }
+        System.out.println(answer);
+    }
+}
+
