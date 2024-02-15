734d22b56e23aa6c6d0eb282e3e38434ec0e68f9
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-15 18:45:44 +0900

    [D3] Title: 상호의 배틀필드, Time: 118 ms, Memory: 20,960 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,95 +1,109 @@
-import java.util.Scanner;
-class Solution
-{
-    // 상,하,좌,우
-    static int[] dirX = {-1,1,0,0};
-    static int[] dirY = {0,0,-1,1};
-    static int tankDir,tankX,tankY;
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int h = sc.nextInt();
-            int w = sc.nextInt();
-            tankDir = 0;tankX = 0;tankY = 0;
-            char[][] map = new char[h][w];
-            for(int i = 0; i < h; i++){
-                String line = sc.next();
-                for(int j = 0; j < w; j++){
-                    map[i][j] = line.charAt(j);
-                    if(map[i][j] == '^'){
-                        tankDir = 0;
-                        tankX = i; tankY = j;
-                    }else if(map[i][j] == 'v'){
-                        tankDir = 1;
-                        tankX = i; tankY = j;
-                    }else if(map[i][j] == '<'){
-                        tankDir = 2;
-                        tankX = i; tankY = j;
-                    }else if(map[i][j] == '>'){
-                        tankDir = 3;
-                        tankX = i; tankY = j;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int H, W;
+    static char[][] map;
+
+    static int[] dirX = {-1, 1, 0, 0};
+    static int[] dirY = {0, 0, -1, 1};
+    static int tankX, tankY, tankDir;
+    static String command;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            H = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            map = new char[H][W];
+
+            for (int i = 0; i < H; i++) {
+                String input = br.readLine();
+                for (int j = 0; j < W; j++) {
+                    map[i][j] = input.charAt(j);
+                    if (map[i][j] != '.' && map[i][j] != '*' && map[i][j] != '#' && map[i][j] != '-') {
+                        tankX = i;
+                        tankY = j;
+                        tankDir = getDir(map[i][j]);
+                        map[i][j] = '.';
                     }
                 }
             }
-            int n = sc.nextInt();
-            String line = sc.next();
-            for(int i = 0; i < n; i++){
-                if(line.charAt(i) == 'S'){
-                    shoot(map);
-                }else{
-                    forward(line.charAt(i),map);
+            br.readLine();
+            command = br.readLine();
+
+            execute();
+
+            map[tankX][tankY] = (tankDir == 0 ? '^' : tankDir == 1 ? 'v' : tankDir == 2 ? '<' : '>');
+            sb.append('#').append(tc).append(' ');
+            for (int i = 0; i < H; i++) {
+                for (int j = 0; j < W; j++) {
+                    sb.append(map[i][j]);
                 }
+                sb.append('\n');
             }
-            System.out.print("#" + tc + " ");
-            printMap(map);
         }
-	}
-    private static void printMap(char[][] map){
-        for(char[] str : map){
-                System.out.println(new String(str));
+        System.out.println(sb);
+    }
+
+    private static void execute() {
+        for (char c : command.toCharArray()) {
+            if (c == 'U' || c == 'D' || c == 'L' || c == 'R') {
+                tankDir = getDir(c);
+                go();
+            } else {
+                shoot();
             }
+        }
     }
-    private static boolean isCheck(int x, int y,char[][] map){
-        return x >= 0 && x < map.length && y >= 0 && y < map[x].length ;
-    }
-    
-    private static void shoot(char[][] map){
-        int nx = tankX + dirX[tankDir];
-        int ny = tankY + dirY[tankDir];
-        while(isCheck(nx,ny,map)){
-            if(map[nx][ny] == '*'){
-                map[nx][ny] = '.';
+
+    private static void shoot() {
+        int nX = tankX;
+        int nY = tankY;
+        while (isIn(nX, nY)) {
+            if (map[nX][nY] == '*') {
+                map[nX][nY] = '.';
                 return;
-            }else if(map[nx][ny] == '#'){
+            } else if (map[nX][nY] == '#') {
                 return;
             }
-            nx += dirX[tankDir];
-            ny += dirY[tankDir];
+            nX += dirX[tankDir];
+            nY += dirY[tankDir];
         }
     }
-    
-    private static void forward(char d, char[][] map){
-        char mark = ' ';
-        switch(d){
-            case 'U': tankDir = 0; mark = '^'; break;
-            case 'D': tankDir = 1; mark = 'v'; break;
-            case 'L': tankDir = 2; mark = '<'; break;
-            case 'R': tankDir = 3; mark = '>'; break;
-            default: break;
-        }
-        map[tankX][tankY] = mark;
-        int nx = dirX[tankDir] + tankX;
-        int ny = dirY[tankDir] + tankY;
-        if(!isCheck(nx,ny,map)){
-            return;
-        }
-        if(map[nx][ny] == '.'){
-            map[nx][ny] = mark;
+
+    private static void go() {
+        int nX = tankX + dirX[tankDir];
+        int nY = tankY + dirY[tankDir];
+
+        if (isIn(nX, nY) && map[nX][nY] == '.') {
+            map[nX][nY] = map[tankX][tankY];
             map[tankX][tankY] = '.';
-            tankX = nx;
-            tankY = ny;
+            tankX = nX;
+            tankY = nY;
+        }
+    }
+
+    private static boolean isIn(int x, int y) {
+        return x >= 0 && x < H && y >= 0 && y < W;
+    }
+
+    private static int getDir(char d) {
+        if (d == '^' || d == 'U') {
+            return 0;
+        } else if (d == 'v' || d == 'D') {
+            return 1;
+        } else if (d == '<' || d == 'L') {
+            return 2;
+        } else {
+            return 3;
         }
     }
 }
+
