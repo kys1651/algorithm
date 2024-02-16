3da3e6abe919e59a0fe4d0ce5494101375d6d3b1
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-16 09:33:20 +0900

    [Silver III] Title: Yonsei TOTO, Time: 92 ms, Memory: 12636 KB -BaekjoonHub

---
+++ 백준/Silver III/12018. Yonsei TOTO/Yonsei TOTO.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] lecture = new int[N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int p = Integer.parseInt(st.nextToken());
+			int l = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			if (p < l) {
+				lecture[i] = 1;
+				continue;
+			}
+				
+			
+			int[] arr = new int[p];
+			for(int j = 0; j < p; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr);
+			
+			lecture[i] = arr[p-l];
+		}
+		
+		Arrays.sort(lecture);
+		
+		int answer = 0;
+		int sum = 0;
+		for(int i = 0; i < N; i++) {
+			sum += lecture[i];
+			if(sum > M) {
+				break;
+			}
+			answer++;
+		}
+		System.out.println(answer);
+	}
+}
