1135ce97c1385028c4e151d5ca2acb60a55f724c
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-17 14:22:54 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12368 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,63 +1,57 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-        import java.util.ArrayDeque;
 
 public class Main {
-    static Queue<Integer> q = new LinkedList<>();
-    static int n;
-    static int m;
-    static int[][] maze;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        maze = new int[n][m];
+        // Input
+        char[][] map = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
 
-        for(int i = 0 ; i < n ; i++){
-            String s = br.readLine();
-            for(int j = 0 ; j < m ; j++){
-                maze[i][j] = s.charAt(j) - '0';
+
+
+        int[] dirX = {-1, 0, 1, 0};
+        int[] dirY = {0, 1, 0, -1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0,1});
+        map[0][0] = '0';
+
+        int result = 10000;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+
+            if (cur[0] == N - 1 && cur[1] == M - 1) {
+                result = Math.min(cur[2], result);
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nX = cur[0] + dirX[i];
+                int nY = cur[1] + dirY[i];
+
+                if (nX < 0 || nX >= N || nY < 0 || nY >= M || map[nX][nY] == '0') {
+                    continue;
+                }
+
+                map[nX][nY] = '0';
+                queue.offer(new int[]{nX, nY, cur[2] + 1});
             }
         }
 
-        visited = new boolean[n][m];
-        visited[0][0] = true;
-        bfs(0,0);
-        System.out.println(maze[n-1][m-1]);
-    }
-
-    private static void bfs(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {x,y});
-
-        while (!q.isEmpty()) {
-            int now[] = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
-
-            for(int i = 0 ; i < 4; i++){
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m)
-                    continue;
-                if(visited[nextX][nextY] || maze[nextX][nextY] == 0)
-                    continue;
-
-                q.add(new int[]{nextX, nextY});
-                maze[nextX][nextY] = maze[nowX][nowY] + 1;
-                visited[nextX][nextY] = true;
-            }
-        }
+        System.out.println(result);
     }
 }
+
