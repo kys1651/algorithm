38b79220f2fa37824f390c5c59be77de5ac1fa55
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-17 14:01:51 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 40696 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,36 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()) - 1;
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long nodeprice[] = new long[n - 1];//다음 노드로 가는 비용
-        long node[] = new long[n];// 노드 당 기름 비용
-        int i;
-        long sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long[] dist = new long[N];
+        for (int i = 0; i < N; i++) {
+            dist[i] = Long.parseLong(st.nextToken());
+        }
 
-        for (i = 0; i < nodeprice.length; i++) {
-            nodeprice[i] = sc.nextInt();
-        }
-        for (i = 0; i < node.length; i++) {
-            node[i] = sc.nextInt();
-        }
-        long min = node[0];
-        for(i = 0 ; i < n; i++){
-            if(min < node[i]){
-                node[i] = min;
+
+        st = new StringTokenizer(br.readLine());
+        long minCost = Long.parseLong(st.nextToken());
+        long distSum = 0;
+        long answer = 0;
+        for (int i = 0; i < N; i++) {
+            long tmp = Long.parseLong(st.nextToken());
+            distSum += dist[i];
+            if (minCost > tmp) {
+                answer += distSum * minCost;
+                minCost = tmp;
+                distSum = 0;
             }
-            else{
-                min = node[i];
-            }
         }
 
-        for(i = 0 ; i < nodeprice.length; i++){
-            sum += nodeprice[i] * node[i];
+        if (distSum != 0) {
+            answer += distSum * minCost;
         }
 
-
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
+
