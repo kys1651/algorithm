1b47279e643d6af56637a74d9853c280345f841f
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-20 14:15:40 +0900

    [Gold III] Title: 줄 세우기, Time: 380 ms, Memory: 42092 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -6,27 +6,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+	static class Node {
+		int to;
+		Node next;
+
+		public Node(int to, Node next) {
+			this.to = to;
+			this.next = next;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		ArrayList<Integer>[] graph = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
+		
+		Node[] nodes = new Node[N + 1];
 		int[] count = new int[N + 1];
 		int[] result = new int[N];
 
+		// Input
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int to = Integer.parseInt(st.nextToken());
 			int from = Integer.parseInt(st.nextToken());
-			graph[from].add(to);
+			nodes[from] = new Node(to,nodes[from]);
 			count[to]++;
 		}
 
+		// 0인 것 받기
 		Queue<Integer> queue = new LinkedList<>();
 		for (int i = 1; i <= N; i++) {
 			if (count[i] == 0) {
@@ -34,18 +44,11 @@ public class Main {
 			}
 		}
 
-		for (int i = N-1; i >= 0; i--) {
-			if (queue.isEmpty()) {
-				System.out.println("Wrong Input");
-				return;
-			}
-
-			int x = queue.poll();
-			result[i] = x;
-			for (int to : graph[x]) {
-				count[to]--;
-				if (count[to] == 0) {
-					queue.add(to);
+		for (int i = N - 1; i >= 0; i--) {
+			result[i] = queue.poll();
+			for(Node tmp = nodes[result[i]]; tmp != null ; tmp = tmp.next) {
+				if(--count[tmp.to]== 0) {
+					queue.add(tmp.to);
 				}
 			}
 		}
