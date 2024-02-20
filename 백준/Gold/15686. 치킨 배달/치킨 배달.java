7a6a24c53325fc1e282bc58e25ca4026fd470df9
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-20 17:30:11 +0900

    [Gold V] Title: 치킨 배달, Time: 156 ms, Memory: 12616 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -5,77 +5,72 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Point {
-        int x;
-        int y;
+	static int N, M, K, result = Integer.MAX_VALUE;
+	static int[][] map;
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static ArrayList<int[]> chicken = new ArrayList<>();
+	static ArrayList<int[]> house = new ArrayList<>();
+	static boolean[] check;
 
-    static int n, m, result;
-    static ArrayList<Point> chicken = new ArrayList<>();
-    static ArrayList<Point> house = new ArrayList<>();
-    static boolean[] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		// Input
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 2) {
+					chicken.add(new int[] { i, j });
+				} else if (map[i][j] == 1) {
+					house.add(new int[] { i, j });
+				}
+			}
+		} // Input End
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                int value = Integer.parseInt(st.nextToken());
-                // 집
-                if (value == 1) {
-                    house.add(new Point(i, j));
-                }
-                // 치킨집
-                else if (value == 2) {
-                    chicken.add(new Point(i, j));
-                }
-            }
-        }
-        // 치킨집 사이즈만큼 배열 생성 -> M개를 뽑아주기 위해서
-        visit = new boolean[chicken.size()];
-        result = Integer.MAX_VALUE;
-        dfs(0, 0);
-        System.out.println(result);
-    }
+		K = chicken.size();
+		check = new boolean[K];
 
-    // M개의 치킨집을 뽑는 메서드
-    private static void dfs(int depth, int pos) {
-        // M를 뽑아준 뒤 계산해준다.
-        if (depth == m) {
-            calc();
-            return;
-        }
-        // pos부터 하는 이유는 M개를 뽑을거니까 0에서부터 할 필요없고 전에 뽑은 치킨집을 또 뽑을 일 없음
-        for (int i = pos; i < chicken.size(); i++) {
-            if (visit[i]) continue;
+		combination(0, 0);
 
-            visit[i] = true;
-            dfs(depth + 1, i + 1);
-            visit[i] = false;
-        }
-    }
+		System.out.println(result);
+	}
 
-    // 치킨 거리를 구해주는 함수
-    private static void calc() {
-        int sumDistance = 0;
-        for (Point h : house) {
-            int distance = Integer.MAX_VALUE;
-            for (int i = 0; i < chicken.size(); i++) {
-                if (!visit[i]) continue;
+	private static void combination(int depth, int at) {
+		if (depth == M) {
+			calc();
+			return;
+		}
 
-                Point c = chicken.get(i);
-                distance = Math.min(distance, Math.abs(h.x - c.x) + Math.abs(h.y - c.y));
-            }
-            sumDistance += distance;
-        }
-        result = Math.min(sumDistance, result);
-    }
+		for (int i = at; i < K; i++) {
+			if (check[i])
+				continue;
+			check[i] = true;
+			combination(depth + 1, i + 1);
+			check[i] = false;
+		}
+	}
+
+	private static void calc() {
+		int sum = 0;
+		for (int[] h : house) {
+			int tmp = result;
+			for (int i = 0; i < K; i++) {
+				if (!check[i])
+					continue;
+				tmp = Math.min(tmp, getDistance(h, chicken.get(i)));
+			}
+			sum += tmp;
+			if (sum > result)
+				return;
+		}
+		result = sum;
+	}
+
+	private static int getDistance(int[] h, int[] c) {
+		return Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
+	}
 }
