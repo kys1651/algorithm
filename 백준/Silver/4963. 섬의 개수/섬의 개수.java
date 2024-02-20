dbaadb92f7a67aba65f563db87939b6ed88deb60
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-20 13:21:37 +0900

    [Silver II] Title: 섬의 개수, Time: 120 ms, Memory: 13384 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,78 +1,66 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    static int[][] map;
-    static boolean[][] visit;
-    static StringBuilder sb = new StringBuilder();
-    static int[] dirX = {0, 0, -1, 1, -1, -1, 1, 1};
-    static int[] dirY = {-1, 1, 0, 0, 1, -1, 1, -1};
+public class Main {
+	static int W, H;
+	static int[][] map;
+	static boolean[][] visit;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h, w;
-        while (true) {
-            // h, w 입력
-            h = sc.nextInt();
-            w = sc.nextInt();
-            // 둘 다 0이면 종료
-            if (h == 0 && w == 0) {
-                break;
-            }
+	static int dirX[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int dirY[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-            // 섬과 바다를 입력 받을 map
-            map = new int[w][h];
-            // 방문 확인을 위한 visit
-            visit = new boolean[w][h];
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[i].length; j++) {
-                    map[i][j] = sc.nextInt();
-                }
-            }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			W = Integer.parseInt(st.nextToken());
+			H = Integer.parseInt(st.nextToken());
+			if (H == 0 && W == 0) {
+				break;
+			}
 
-            int result = 0;
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[i].length; j++) {
-                    // map가 섬이고 방문하지 않았다면 result 증가 후 섬 방문 확인
-                    if (map[i][j] == 1 && !visit[i][j]) {
-                        result++;
-                        bfs(i, j);
-                    }
-                }
-            }
-            sb.append(result).append("\n");
-        }
-        System.out.println(sb.toString());
-    }
+			map = new int[H][W];
+			visit = new boolean[H][W];
+			for (int i = 0; i < H; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < W; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-    private static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        visit[x][y] = true;
-        queue.offer(new int[] {x, y});
+			int count = 0;
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					if (map[i][j] == 1 && !visit[i][j]) {
+						count++;
+						dfs(i, j);
+					}
+				}
+			}
+			sb.append(count).append('\n');
+		}
+		System.out.println(sb);
+	}
 
-        while (!queue.isEmpty()) {
-            // 현재 섬의 위치
-            int cur[] = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
+	private static void dfs(int x, int y) {
+		visit[x][y] = true;
 
-            // 주변 상,하,좌,우,각 대각를 기준으로 확인
-            for (int i = 0; i < dirX.length; i++) {
-                int nextX = curX + dirX[i];
-                int nextY = curY + dirY[i];
+		for (int i = 0; i < 8; i++) {
+			int nX = x + dirX[i];
+			int nY = y + dirY[i];
+			if (!isIn(nX, nY) || map[nX][nY] != 1 || visit[nX][nY]) {
+				continue;
+			}
+			dfs(nX, nY);
+		}
 
-                // 배열의 크기 밖이라면 continue;
-                if (nextX < 0 || nextX >= map.length || nextY < 0 || nextY >= map[nextX].length) {
-                    continue;
-                }
-                // 방문할 곳이 바다거나 방문했다면 continue;
-                if (map[nextX][nextY] == 0 || visit[nextX][nextY]) {
-                    continue;
-                }
-                visit[nextX][nextY] = true;
-                queue.offer(new int[] {nextX, nextY});
-            }
-        }
-    }
+	}
+
+	private static boolean isIn(int x, int y) {
+		return x >= 0 && x < H && y >= 0 && y < W;
+	}
 }
