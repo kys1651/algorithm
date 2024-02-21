bf315ce460f0d7374ab4133a185b10e275ebbb0b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-21 17:21:27 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 13376 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,65 +2,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int L, C;
-    static char[] list;
-    static char[] code;
+	static int L, C;
+	static char[] alphabet;
+	static char[] answer;
+	static StringBuilder sb = new StringBuilder();
+	static HashSet<Character> set = new HashSet<>();
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		answer = new char[L];
+		alphabet = new char[C];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < C; i++) {
+			alphabet[i] = st.nextToken().charAt(0);
+		}
+		Arrays.sort(alphabet);
+		set.add('a');
+		set.add('e');
+		set.add('i');
+		set.add('o');
+		set.add('u');
+		combination(0, 0, 0, 0);
+		System.out.println(sb);
+	}
 
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+	private static void combination(int depth, int at, int j, int m) {
+		if (depth == L) {
+			if (j >= 2 && m >= 1) {
+				sb.append(answer).append('\n');
+			}
+			return;
+		}
 
-        list = new char[C];
-        code = new char[L];
+		for (int i = at; i < C; i++) {
+			answer[depth] = alphabet[i];
+			if (set.contains(alphabet[i])) {
+				combination(depth + 1, i + 1, j, m + 1);
+			} else {
+				combination(depth + 1, i + 1, j + 1, m);
+			}
 
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < C; i++) {
-            list[i] = st.nextToken().charAt(0);
-        }
-
-        Arrays.sort(list);
-        
-        tracking(0,0);
-
-    }
-
-    private static void tracking(int idx,int depth) {
-        if(depth == L){
-            if(check()){
-                System.out.println(code);
-            }
-            return;
-        }
-
-        for(int i = idx; i < C; i++){
-            code[depth] = list[i];
-            tracking(i + 1, depth + 1);
-        }
-    }
-
-    private static boolean check() {
-        int a = 0;
-        int b = 0;
-
-        for (char c : code) {
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-                a++;
-            }else{
-                b++;
-            }
-        }
-
-        if (a >= 1 && b >= 2) {
-            return true;
-        }
-        return false;
-    }
-
+		}
+	}
 }
