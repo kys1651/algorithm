d8ce8054d262c6fdfc0d65ac27099493f7684780
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-21 17:24:29 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -29,26 +29,36 @@ public class Main {
 		set.add('i');
 		set.add('o');
 		set.add('u');
-		combination(0, 0, 0, 0);
+		combination(0, 0);
 		System.out.println(sb);
 	}
 
-	private static void combination(int depth, int at, int j, int m) {
+	private static void combination(int depth, int at) {
 		if (depth == L) {
-			if (j >= 2 && m >= 1) {
-				sb.append(answer).append('\n');
-			}
+			check();
 			return;
 		}
 
 		for (int i = at; i < C; i++) {
 			answer[depth] = alphabet[i];
-			if (set.contains(alphabet[i])) {
-				combination(depth + 1, i + 1, j, m + 1);
-			} else {
-				combination(depth + 1, i + 1, j + 1, m);
-			}
+			combination(depth + 1, i + 1);
 
+		}
+	}
+
+	private static void check() {
+		int j = 0;
+		int m = 0;
+		for (char ch : answer) {
+			if (set.contains(ch)) {
+				m++;
+			} else {
+				j++;
+			}
+			if (j >= 2 && m >= 1) {
+				sb.append(answer).append('\n');
+				return;
+			}
 		}
 	}
 }
