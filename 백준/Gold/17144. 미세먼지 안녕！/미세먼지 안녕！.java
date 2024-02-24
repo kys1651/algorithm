edeed3ee25f956e4256e832bd8cae34c79849bc4
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-24 18:31:17 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 320 ms, Memory: 150356 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -7,10 +7,10 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int R, C, T;
-    static int[][] map;
-    static int[] airCleanUp;
-    static int[] airCleanDown;
-    static Queue<int[]> dustList = new LinkedList<>();
+    static int[][] map; // 배열 상태
+    static int[] airCleanUp; // 위를 보는 에어컨
+    static int[] airCleanDown; // 아래를 보는 에어컨
+    static Queue<int[]> dustList = new LinkedList<>(); // 미세먼지 큐
 
     static int[] dirX = {0, 1, 0, -1};
     static int[] dirY = {1, 0, -1, 0};
@@ -40,11 +40,10 @@ public class Main {
 
         while (T-- > 0) {
             dust();
-//            print();
             airClean();
-//            print();
         }
 
+        // 마지막에 미세먼지를 Count
         int answer = 0;
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
@@ -54,14 +53,15 @@ public class Main {
             }
         }
         System.out.println(answer);
-
     }
 
+    // 에어컨이 공기 작동 메서드
     private static void airClean() {
         rotate(airCleanUp[0], airCleanUp[1], true);
         rotate(airCleanDown[0], airCleanDown[1], false);
     }
 
+    // 회전하도록 한다. 행 열
     private static void rotate(int x, int y, boolean up) {
         int dir = 0;
         int nX = x + dirX[dir];
@@ -83,48 +83,39 @@ public class Main {
         }
     }
 
-    private static void print() {
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
+    // 미세먼지를 퍼뜨린다.
     private static void dust() {
-        int[][] tmp = new int[R][C];
 
+        // 미세먼지 위치를 세어주고 저장한다.
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 if (map[i][j] > 0) {
-                    dustList.add(new int[]{i, j});
+                    dustList.add(new int[]{i, j, map[i][j]});
+                    map[i][j] = 0;
                 }
             }
         }
 
+        // 배열이 빌 때 까지 확인
         while (!dustList.isEmpty()) {
             int[] cur = dustList.poll();
-            int minus = map[cur[0]][cur[1]] / 5;
+            int minus = cur[2] / 5;
             int count = 0;
+
             for (int i = 0; i < 4; i++) {
                 int nX = cur[0] + dirX[i];
                 int nY = cur[1] + dirY[i];
                 if (!isIn(nX, nY) || map[nX][nY] == -1) {
                     continue;
                 }
-                tmp[nX][nY] += minus;
+                map[nX][nY] += minus;
                 count++;
             }
-            tmp[cur[0]][cur[1]] += map[cur[0]][cur[1]] - (count * minus);
+            map[cur[0]][cur[1]] += cur[2] - (count * minus);
         }
-
-        tmp[airCleanUp[0]][airCleanUp[1]] = -1;
-        tmp[airCleanDown[0]][airCleanDown[1]] = -1;
-        map = tmp;
     }
 
+    // 배열이 내부에 있는지 확인
     private static boolean isIn(int x, int y) {
         return x >= 0 && x < R && y >= 0 && y < C;
     }
