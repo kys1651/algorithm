82bf31f1646acd6f7d9c2ef9f452cf288d65262a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-27 20:22:50 +0900

    [Silver I] Title: RGB거리, Time: 84 ms, Memory: 12068 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,41 +1,28 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][3];
 
-        int arr[][] = new int[N][3];
-        int dp[][] = new int[N][3];
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        // 처음 기준 잡아줌
-        dp[0][0] = arr[0][0];
-        dp[0][1] = arr[0][1];
-        dp[0][2] = arr[0][2];
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        dp[0][0] = Integer.parseInt(st.nextToken());
+        dp[0][1] = Integer.parseInt(st.nextToken());
+        dp[0][2] = Integer.parseInt(st.nextToken());
         for (int i = 1; i < N; i++) {
-
-            // Red로 색칠 하는 경우
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+            st = new StringTokenizer(br.readLine());
+            int R = Integer.parseInt(st.nextToken());
+            int G = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + R;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + G;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + B;
         }
 
-        int answer = dp[N - 1][0];
-        answer = Math.min(Math.min(dp[N - 1][1], dp[N - 1][2]), answer);
-        System.out.println(answer);
-
+        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
     }
 }
