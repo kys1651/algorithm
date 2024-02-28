aa1a3abd939329ce4dcc74443617eeb2182ce61f
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-28 14:20:08 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 456 ms, Memory: 85240 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,89 +1,79 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Monkey{
-        int x,y;
-        int jump;
-        int count;
+	static int H, W, result = Integer.MAX_VALUE;
+	static int[][] map;
+	static boolean[][][] visit;
 
-        public Monkey(int x, int y, int jump, int count) {
-            this.x = x;
-            this.y = y;
-            this.jump = jump;
-            this.count = count;
-        }
-    }
-    static int K,H,W;
-    static int[][] map;
-    static boolean[][][] visited;
-    static int answer = Integer.MAX_VALUE;
+	static int[] dirX = { 0, 1, 0, -1, 1, 2, -1, -2, 1, 2, -1, -2 };
+	static int[] dirY = { 1, 0, -1, 0, 2, 1, 2, 1, -2, -1, -2, -1 };
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		visit = new boolean[K + 1][H][W];
+		map = new int[H][W];
+		for (int i = 0; i < H; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < W; j++) {
+				map[i][j] = input.charAt(j * 2) - '0';
+			}
+		}
+		
+		if(W == 1 && H == 1) {
+			System.out.println(0);
+			return;
+		}
+		
+		bfs(K);
+		System.out.println(-1);
+	}
 
-        K = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
+	private static void bfs(int K) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] { 0, 0, K, 0 });
+		visit[K][0][0] = true;
 
-        map = new int[H][W];
-        visited = new boolean[H][W][K+1];
-        for (int i = 0; i < H; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < W; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 1){
-                    map[i][j] = -1;
-                }
-            }
-        }
-        bfs();
-        System.out.println(answer == Integer.MAX_VALUE ? "-1" : answer);
-    }
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int jump = cur[2];
+			
+			for (int i = 0; i < 12; i++) {
+				int nX = cur[0] + dirX[i];
+				int nY = cur[1] + dirY[i];
 
-    private static void bfs() {
-        Queue<Monkey> q = new LinkedList<>();
-        q.add(new Monkey(0, 0, K, 0));
+				if (i == 4) {
+					if (jump == 0) {
+						break;
+					}
+					jump--;
+				}
+				// 배열 범위 밖이거나 || 벽이면 넘어간다.
+				if (!isIn(nX, nY) || map[nX][nY] == 1 || visit[jump][nX][nY]) {
+					continue;
+				}
+				
+				visit[jump][nX][nY] = true;
+				queue.add(new int[] { nX, nY, jump, cur[3] + 1 });
+				
+				if (nX == H - 1 && nY == W - 1) {
+					System.out.println(cur[3] + 1);
+					System.exit(0);
+				}
+			}
+		}
+	}
 
-        // 말의 움직임
-        int[][] night = {{-2, -1}, {-1, -2}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}};
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
-        while (!q.isEmpty()) {
-            Monkey m = q.poll();
-            if (m.x == H - 1 && m.y == W - 1) {
-                answer = Math.min(answer, m.count);
-            }
-
-            if(m.jump > 0) {
-                for (int i = 0; i < 8; i++) {
-                    int nx = m.x + night[i][0];
-                    int ny = m.y + night[i][1];
-
-                    // 범위가 아니거나 점프 횟수가 없을 때 그 곳이 장애물일 때
-                    if (!isCheck(nx, ny) || map[nx][ny] == -1 || visited[nx][ny][m.jump - 1]) continue;
-                    visited[nx][ny][m.jump - 1] = true;
-                    q.add(new Monkey(nx, ny, m.jump - 1, m.count + 1));
-                }
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = m.x + dx[i];
-                int ny = m.y + dy[i];
-
-                if(!isCheck(nx,ny) || map[nx][ny] == -1 || visited[nx][ny][m.jump]) continue;
-
-                visited[nx][ny][m.jump] = true;
-                q.add(new Monkey(nx, ny, m.jump, m.count+1));
-            }
-        }
-    }
-
-    private static boolean isCheck(int x, int y) {
-        return x >= 0 && y >= 0 && x < H && y < W;
-    }
+	private static boolean isIn(int x, int y) {
+		return x >= 0 && x < H && y >= 0 && y < W;
+	}
 }
