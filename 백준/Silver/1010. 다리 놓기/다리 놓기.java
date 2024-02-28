0a8b1e47b581ff3295b7177cbaaca96eca18ba42
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-28 12:00:53 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 11812 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,38 +1,31 @@
 import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.io.IOException;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] dp = new int[30][30];
+	static int[][] dp = new int[30][30];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			sb.append(solve(M, N)).append('\n');
+		}
+		System.out.println(sb);
+	}
 
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0 ; i < T ; i++){
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-
-            sb.append(solution(m, n)).append('\n');
-        }
-
-        System.out.println(sb);
-    }
-
-    private static int solution(int n, int r) {
-        if(dp[n][r] > 0 )
-            return dp[n][r];
-
-        if(n==r||r==0)
-            return dp[n][r] = 1;
-
-        return dp[n][r] = solution(n - 1, r - 1) + solution(n - 1, r);
-    }
+	private static int solve(int n, int r) {
+		if (dp[n][r] > 0) {
+			return dp[n][r];
+		}
+		if (n == r || r == 0) {
+			return dp[n][r] = 1;
+		}
+		return dp[n][r] = solve(n - 1, r - 1) + solve(n - 1, r);
+	}
 }
-
-
