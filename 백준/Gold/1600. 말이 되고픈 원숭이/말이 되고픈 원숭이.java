6037199d2f8d13c2e43ea877df70387b088f2be3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-28 21:19:46 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 436 ms, Memory: 84172 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -6,74 +6,74 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int H, W, result = Integer.MAX_VALUE;
-	static int[][] map;
-	static boolean[][][] visit;
+    static int H, W;
+    static int[][] map;
+    static boolean[][][] visit;
 
-	static int[] dirX = { 0, 1, 0, -1, 1, 2, -1, -2, 1, 2, -1, -2 };
-	static int[] dirY = { 1, 0, -1, 0, 2, 1, 2, 1, -2, -1, -2, -1 };
+    static int[] dirX = {0, 1, 0, -1, 1, 2, -1, -2, 1, 2, -1, -2};
+    static int[] dirY = {1, 0, -1, 0, 2, 1, 2, 1, -2, -1, -2, -1};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int K = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		W = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-		
-		visit = new boolean[K + 1][H][W];
-		map = new int[H][W];
-		for (int i = 0; i < H; i++) {
-			String input = br.readLine();
-			for (int j = 0; j < W; j++) {
-				map[i][j] = input.charAt(j * 2) - '0';
-			}
-		}
-		
-		if(W == 1 && H == 1) {
-			System.out.println(0);
-			return;
-		}
-		
-		bfs(K);
-		System.out.println(-1);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-	private static void bfs(int K) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[] { 0, 0, K, 0 });
-		visit[K][0][0] = true;
+        visit = new boolean[K + 1][H][W];
+        map = new int[H][W];
+        for (int i = 0; i < H; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < W; j++) {
+                map[i][j] = input.charAt(j * 2) - '0';
+            }
+        }
 
-		while (!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int jump = cur[2];
-			
-			for (int i = 0; i < 12; i++) {
-				int nX = cur[0] + dirX[i];
-				int nY = cur[1] + dirY[i];
+        if (W == 1 && H == 1) {
+            System.out.println(0);
+            return;
+        }
 
-				if (i == 4) {
-					if (jump == 0) {
-						break;
-					}
-					jump--;
-				}
-				// 배열 범위 밖이거나 || 벽이면 넘어간다.
-				if (!isIn(nX, nY) || map[nX][nY] == 1 || visit[jump][nX][nY]) {
-					continue;
-				}
-				
-				visit[jump][nX][nY] = true;
-				queue.add(new int[] { nX, nY, jump, cur[3] + 1 });
-				
-				if (nX == H - 1 && nY == W - 1) {
-					System.out.println(cur[3] + 1);
-					System.exit(0);
-				}
-			}
-		}
-	}
+        bfs(K);
+        System.out.println(-1);
+    }
 
-	private static boolean isIn(int x, int y) {
-		return x >= 0 && x < H && y >= 0 && y < W;
-	}
+    private static void bfs(int K) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, K, 0});
+        visit[K][0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int jump = cur[2];
+
+            for (int i = 0; i < 12; i++) {
+                int nX = cur[0] + dirX[i];
+                int nY = cur[1] + dirY[i];
+
+                if (i == 4) {
+                    if (jump == 0) {
+                        break;
+                    }
+                    jump--;
+                }
+                // 배열 범위 밖이거나 || 벽이면 넘어간다.
+                if (!isIn(nX, nY) || map[nX][nY] == 1 || visit[jump][nX][nY]) {
+                    continue;
+                }
+
+                visit[jump][nX][nY] = true;
+                queue.add(new int[]{nX, nY, jump, cur[3] + 1});
+
+                if (nX == H - 1 && nY == W - 1) {
+                    System.out.println(cur[3] + 1);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private static boolean isIn(int x, int y) {
+        return x >= 0 && x < H && y >= 0 && y < W;
+    }
 }
