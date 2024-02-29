3c692b32653fcc59faaf09e1b8be84d3fe7cdcef
김용수 <43926186+kys1651@users.noreply.github.com>
2024-02-29 11:31:48 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 1,572 ms, Memory: 24,216 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -4,113 +4,98 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Solution {
-    static class Core {
-        int x;
-        int y;
+	static int N, K, maxCoreCount, minWireDist;
+	static int[][] map;
+	static ArrayList<int[]> coreList;
 
-        public Core(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static int[] dirX = { -1, 1, 0, 0 };
+	static int[] dirY = { 0, 0, -1, 1 };
 
-    static ArrayList<Core> coreList;
-    static int[][] map;
-    static int N, K, coreCount, wireDist;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			coreList = new ArrayList<>();
+			maxCoreCount = 0;
+			minWireDist = 144;
 
-    static int[] dirX = {-1, 0, 1, 0};
-    static int[] dirY = {0, 1, 0, -1};
+			// Input End
+			map = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				String input = br.readLine();
+				for (int j = 0; j < N; j++) {
+					map[i][j] = input.charAt(j * 2) - '0';
+					if (map[i][j] == 1) {
+						coreList.add(new int[] { i, j });
+					}
+				}
+			} // Input End
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            coreList = new ArrayList<>();
+			K = coreList.size();
+			solve(0, 0, 0);
+			sb.append(String.format("#%d %d\n", tc, minWireDist));
+		}
+		System.out.println(sb);
+	}
 
-            // Input
-            for (int i = 0; i < N; i++) {
-                String input = br.readLine();
-                for (int j = 0; j < N; j++) {
-                    int tmp = input.charAt(j * 2) - '0';
-                    // 벽에 붙어있지 않은 코어 위치 기억
-                    if (tmp == 1) {
-                        map[i][j] = 1;
-                        if (i == 0 || j == 0 || i == N - 1 || j == N - 1) {
-                            continue;
-                        }
-                        coreList.add(new Core(i, j));
-                    }
-                }
-            } // Input End
+	private static void solve(int depth, int coreCount, int wireDist) {
+		if (depth == K) {
+			if (maxCoreCount < coreCount) {
+				maxCoreCount = coreCount;
+				minWireDist = wireDist;
+			} else if (maxCoreCount == coreCount) {
+				if (minWireDist > wireDist) {
+					minWireDist = wireDist;
+				}
+			}
+			return;
+		}
 
-            coreCount = 0;
-            wireDist = 144;
-            K = coreList.size();
+		int[] curCore = coreList.get(depth);
+		for (int i = 0; i < 4; i++) {
+			if (isLink(curCore[0], curCore[1], i)) {
+				int dist = fill(curCore[0], curCore[1], i, 1);
+				solve(depth + 1, coreCount + 1, wireDist + dist);
+				fill(curCore[0], curCore[1], i, 0);
+			}
+		}
+		solve(depth + 1, coreCount, wireDist);
+	}
 
-            permutation(0, 0, 0);
+	private static int fill(int x, int y, int d, int value) {
+		int count = 0;
+		x += dirX[d];
+		y += dirY[d];
+		while (true) {
+			if (!isIn(x, y)) {
+				break;
+			}
+			map[x][y] = value;
+			x += dirX[d];
+			y += dirY[d];
+			count++;
+		}
+		return count;
+	}
 
-            sb.append(String.format("#%d %d\n", tc, wireDist));
-        }
+	private static boolean isLink(int x, int y, int d) {
+		x += dirX[d];
+		y += dirY[d];
+		while (true) {
+			if (!isIn(x, y)) {
+				return true;
+			}
+			if (map[x][y] == 1) {
+				return false;
+			}
+			x += dirX[d];
+			y += dirY[d];
+		}
+	}
 
-        System.out.println(sb);
-    }
-
-    private static void permutation(int depth, int count, int dist) {
-        if (depth == K) {
-            if (count > coreCount) {
-                coreCount = count;
-                wireDist = dist;
-            } else if (count == coreCount) {
-                if (wireDist > dist) {
-                    wireDist = dist;
-                }
-            }
-            return;
-        }
-
-        Core c = coreList.get(depth);
-        for (int d = 0; d < 4; d++) {
-            if (isLink(c, d)) {
-                int tmpDist = fill(c, d, 1);
-                permutation(depth + 1, count + 1, dist + tmpDist);
-                fill(c, d, 0);
-            }
-        }
-        permutation(depth + 1, count, dist);
-    }
-
-    private static int fill(Core c, int d, int value) {
-        int tmp = 0;
-        int x = c.x + dirX[d];
-        int y = c.y + dirY[d];
-        while (isIn(x, y)) {
-            map[x][y] = value;
-            tmp++;
-            x += dirX[d];
-            y += dirY[d];
-        }
-        return tmp;
-    }
-
-    private static boolean isLink(Core c, int d) {
-        int x = c.x + dirX[d];
-        int y = c.y + dirY[d];
-
-        while (isIn(x, y)) {
-            if (map[x][y] != 0) {
-                return false;
-            }
-            x += dirX[d];
-            y += dirY[d];
-        }
-        return true;
-    }
-
-    private static boolean isIn(int x, int y) {
-        return x >= 0 && x < N && y >= 0 && y < N;
-    }
+	private static boolean isIn(int x, int y) {
+		return x >= 0 && x < N && y >= 0 && y < N;
+	}
 }
-
