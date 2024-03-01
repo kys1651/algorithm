6068c75703a219211773499822d2d0c04cc76c02
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-01 22:21:21 +0900

    [Gold III] Title: 다리 만들기, Time: 172 ms, Memory: 76760 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,120 +1,116 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static class Point{
-        int x, y,count;
+    static int N, result;
+    static int[][] map;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-            this.count = 0;
-        }
+    static ArrayList<int[]> boundaryList = new ArrayList<>();
 
-        public Point(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
-    static int[][] map ;
-    static boolean[][] visited;
-    static int N;
-    static int idx = 1;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-    static int answer = Integer.MAX_VALUE;
+    static int[] dirX = {-1, 1, 0, 0};
+    static int[] dirY = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         N = Integer.parseInt(br.readLine());
+
+        // Input
         map = new int[N][N];
-        visited = new boolean[N][N];
-
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            String input = br.readLine();
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = input.charAt(2 * j) - '0';
             }
-        }
+        }// Input End
 
-        // 섬에 값을 매겨줌
+        // 각 섬에 인덱스를 부여한다.
+        int idx = 2;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(!visited[i][j] && map[i][j] != 0){
+                if (map[i][j] == 1) {
+                    map[i][j] = idx++;
                     bfs(i, j);
-                    idx++;
+                }
+
+                // 경계인지 확인
+                if (map[i][j] != 0 && isBoundary(i, j)) {
+                    boundaryList.add(new int[]{i, j});
                 }
             }
         }
 
-        // 섬을 이어준다.
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] >= 1) {
-                    visited = new boolean[N][N];
-                    linkLand(i, j);
-                }
-            }
+        result = N * N + 1;
+        for (int[] b : boundaryList) {
+            linkBoundary(b[0], b[1]);
         }
 
-        System.out.println(answer);
+        System.out.println(result);
     }
-    
-    // 각 섬을 다른 섬과 이어준다. 기존 번호와 다른지 확인하여 판단함
-    private static void linkLand(int x, int y) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x, y, 0));
-        visited[x][y] = true;
 
-        int currentNum = map[x][y];
-        while (!q.isEmpty()) {
-            Point tmp = q.poll();
+    private static void linkBoundary(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y, 0});
 
+        boolean[][] visit = new boolean[N][N];
+        visit[x][y] = true;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            if (cur[2] >= result) {
+                continue;
+            }
             for (int i = 0; i < 4; i++) {
-                int nx = tmp.x + dx[i];
-                int ny = tmp.y + dy[i];
+                int nX = cur[0] + dirX[i];
+                int nY = cur[1] + dirY[i];
 
-                if(!isCheck(nx,ny) || visited[nx][ny] || map[nx][ny] == currentNum) continue;
+                if (!isIn(nX, nY) || map[nX][nY] == map[x][y] || visit[nX][nY]) {
+                    continue;
+                }
 
-                visited[nx][ny] = true;
-                if (map[nx][ny] == 0) {
-                    q.add(new Point(nx, ny, tmp.count + 1));
-                }else{
-                    answer = Math.min(answer, tmp.count);
+                visit[nX][nY] = true;
+                if (map[nX][nY] == 0) {
+                    queue.add(new int[]{nX, nY, cur[2] + 1});
+                } else {
+                    result = Math.min(result, cur[2]);
                 }
             }
         }
     }
 
-    // bfs를 통해 섬에 숫자를 매겨줌
+    private static boolean isBoundary(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int nX = x + dirX[i];
+            int nY = y + dirY[i];
+            if (isIn(nX, nY) && map[nX][nY] != map[x][y]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isIn(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
+    }
+
+    // 현재 섬의 숫자를 전부 퍼뜨려준다.
     private static void bfs(int x, int y) {
-        visited[x][y] = true;
-        map[x][y] = idx;
-
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x,y));
-        while (!q.isEmpty()) {
-            Point tmp = q.poll();
-
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
             for (int i = 0; i < 4; i++) {
-                int nx = tmp.x + dx[i];
-                int ny = tmp.y + dy[i];
-
-                if (!isCheck(nx, ny) || visited[nx][ny] || map[nx][ny] == 0) continue;
-
-                map[nx][ny] = map[tmp.x][tmp.y];
-                visited[nx][ny] = true;
-                q.add(new Point(nx, ny));
+                int nX = cur[0] + dirX[i];
+                int nY = cur[1] + dirY[i];
+                if (!isIn(nX, nY) || map[nX][nY] != 1) {
+                    continue;
+                }
+                map[nX][nY] = map[cur[0]][cur[1]];
+                queue.add(new int[]{nX, nY});
             }
         }
-    }
- 
-    // 범위인지 아닌지 판단
-    private static boolean isCheck(int x, int y) {
-        return x >= 0 && y >= 0 && x < N && y < N;
     }
 }
