d42d7b58c6614839dce44b0f0228cf0e7a0ff7aa
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-02 00:09:44 +0900

    [Gold I] Title: 다리 만들기 2, Time: 80 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -0,0 +1,179 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int weight;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return weight - o.weight;
+        }
+    }
+
+    static int N, M, edgeCount;
+    static int[] parent, rank;
+    static int[][] graph, map;
+
+    static int[] dirX = {-1, 1, 0, 0};
+    static int[] dirY = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // Input
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = input.charAt(j * 2) - '0';
+                // 섬을 -1로 초기화
+                if (map[i][j] == 1) {
+                    map[i][j] = -1;
+                }
+            }
+        }// Input End
+
+        // 고유 번호 부여
+        int idx = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == -1) {
+                    map[i][j] = ++idx;
+                    dfs(i, j);
+                }
+            }
+        }// 고유 번호 부여 끝
+
+        // 그래프 생성
+        edgeCount = idx;
+        graph = new int[edgeCount + 1][edgeCount + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] != 0) {
+                    for (int k = 0; k < 4; k++) {
+                        int nX = i + dirX[k];
+                        int nY = j + dirY[k];
+                        if (isIn(nX, nY) && map[nX][nY] == 0) {
+                            getDist(nX, nY, k, map[i][j], 1);
+                        }
+                    }
+                }
+            }
+        } // 그래프 생성 끝
+
+
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        for (int i = 1; i <= edgeCount; i++) {
+            for (int j = 1; j <= edgeCount; j++) {
+                if (graph[i][j] > 1) {
+                    queue.add(new Edge(i, j, graph[i][j]));
+                }
+            }
+        }
+
+        makeSet();
+
+        int answer = 0;
+        int count = 0;
+        while (count < edgeCount - 1) {
+            if (queue.isEmpty()) {
+                answer = -1;
+                break;
+            }
+
+            Edge e = queue.poll();
+            if (!union(e.from, e.to)) {
+                continue;
+            }
+            answer += e.weight;
+            count++;
+        }
+        System.out.println(answer);
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    private static boolean union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a==b){
+            return false;
+        }
+
+        if(rank[a] > rank[b]){
+            parent[b] = a;
+            rank[a] += rank[b];
+        }else{
+            parent[a] = b;
+            rank[b] += rank[a];
+        }
+        return true;
+    }
+
+    private static void makeSet() {
+        // 자신을 가르키는 트리를 만듬
+        parent = new int[edgeCount + 1];
+        rank = new int[edgeCount + 1];
+        for (int i = 1; i <= edgeCount; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+    }
+
+    private static void getDist(int x, int y, int d, int from, int dist) {
+        if (map[x][y] == 0) {
+            if (isIn(x + dirX[d], y + dirY[d])) {
+                getDist(x + dirX[d], y + dirY[d], d, from, dist + 1);
+            }
+        }
+        // 다른 섬이라면
+        else if (map[x][y] != from) {
+            // 길이가 1이면 안된다.
+            if (dist - 1 <= 1) {
+                return;
+            }
+
+            int idx = map[x][y];
+            if (graph[from][idx] == 0 || graph[from][idx] > dist - 1) {
+                graph[from][idx] = graph[idx][from] = dist - 1;
+            }
+        }
+    }
+
+    // 섬에 고유 번호를 부여한다.
+    private static void dfs(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int nX = x + dirX[i];
+            int nY = y + dirY[i];
+            if (isIn(nX, nY) && map[nX][nY] == -1) {
+                map[nX][nY] = map[x][y];
+                dfs(nX, nY);
+            }
+        }
+    }
+
+    private static boolean isIn(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
+    }
+}
