272da027b22bfbf52fca7d92e1c159c6a7b6402b
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-03 16:49:10 +0900

    [D4] Title: 준환이의 양팔저울, Time: 1,042 ms, Memory: 21,920 KB -BaekjoonHub

---
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int N, sum, result;
+    static int[] arr, weight;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            result = 0;
+            weight = new int[N];
+            arr = new int[N];
+            visit = new boolean[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                weight[i] = Integer.parseInt(st.nextToken());
+                sum += weight[i];
+            }
+            
+            permutation(0);
+
+            sb.append(String.format("#%d %d\n",tc,result));
+        }
+        System.out.println(sb);
+    }
+
+    private static void permutation(int depth) {
+        if (depth == N) {
+            combination(1, arr[0], 0, sum - arr[0]);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visit[i]) continue;
+            visit[i] = true;
+            arr[depth] = weight[i];
+            permutation(depth + 1);
+            visit[i] = false;
+        }
+    }
+
+    private static void combination(int depth, int left, int right, int sum) {
+        if (depth == N) {
+            result++;
+            return;
+        }
+
+        if (left >= right + sum) {
+            int i = N - depth;
+            int count = 1 << i;
+            result += count;
+            return;
+        }
+
+        if (right + arr[depth] <= left) {
+            combination(depth + 1, left, right + arr[depth], sum - arr[depth]);
+        }
+        combination(depth + 1, left + arr[depth], right, sum - arr[depth]);
+    }
+}
