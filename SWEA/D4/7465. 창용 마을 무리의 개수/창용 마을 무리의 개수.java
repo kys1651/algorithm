d6f8302f1388a24b3b066387479976591fd3cab5
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-03 16:08:58 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 130 ms, Memory: 27,036 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,55 +1,60 @@
-import java.util.StringTokenizer;
-import java.util.Stack;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Solution {
-    static boolean[][] graph;
+public class Solution {
+    static class Node {
+        int idx;
+        Node next;
+
+        public Node(int idx, Node next) {
+            this.idx = idx;
+            this.next = next;
+        }
+    }
+
+    static int N, M;
+    static Node[] graph;
     static boolean[] visit;
-    static int n;
-	public static void main(String args[]) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            graph = new boolean[n+1][n+1];
-            visit = new boolean[n+1];
-            
-            for(int i = 0; i < m; i++){
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            // Input
+            visit = new boolean[N + 1];
+            graph = new Node[N + 1];
+            for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                graph[a][b] = graph[b][a] = true;
-            }
-            
-            int result = 0;
-            for(int i = 1; i <= n; i++){
-                if(!visit[i]){
-                    result++;
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                graph[from] = new Node(to, graph[from]);
+                graph[to] = new Node(from, graph[to]);
+            } // Input End
+
+            int answer = 0;
+            for (int i = 1; i <= N; i++) {
+                if (!visit[i]) {
                     dfs(i);
+                    answer++;
                 }
             }
-			sb.append("#" + tc + " " + result).append("\n");
-		}
+            sb.append(String.format("#%d %d\n", tc, answer));
+        }
         System.out.println(sb);
-	}
-    private static void dfs(int start){
-        Stack<Integer> stack = new Stack<>();
-        visit[start] = true;
-        stack.push(start);
-        
-        while(!stack.isEmpty()){
-            int cur = stack.pop();
-            for(int i = 1; i <= n; i++){
-                if(graph[cur][i] && !visit[i]){
-                    stack.push(i);
-                    visit[i] = true;
-                }
+    }
+
+    private static void dfs(int pos) {
+        visit[pos] = true;
+        for (Node tmp = graph[pos]; tmp != null; tmp = tmp.next) {
+            if(!visit[tmp.idx]){
+                dfs(tmp.idx);
             }
         }
     }
