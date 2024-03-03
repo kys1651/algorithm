39daaca2bddfbea87e595a194b6fb1c02745d283
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-03 23:40:14 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 124 ms, Memory: 17152 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -1,144 +1,143 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-class Main{ 
-    static class Point{
+public class Main {
+    static class Ball {
         int x;
         int y;
-        public Point(int x, int y) {
+
+        public Ball(int x, int y) {
             this.x = x;
-            this.y= y;
+            this.y = y;
         }
     }
-    static class Game{
-        Point red;
-        Point blue;
+
+    static class Game {
+        Ball red;
+        Ball blue;
         int count;
 
-        public Game(Point red, Point blue, int count) {
+        public Game(Ball red, Ball blue, int count) {
             this.red = red;
             this.blue = blue;
             this.count = count;
         }
     }
-    static int n,m;
+
+    static int result = 11;
     static char[][] map;
-    static int result;
-    static int[] dirX = {-1,1,0,0};
-    static int[] dirY = {0,0,-1,1};
-    public static void main(String[] args) throws IOException{
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        map = new char[n][m];
-        result = Integer.MAX_VALUE;
-        Point blue = null;
-        Point red = null;
-        for(int i = 0; i < n; i++){
-            String line = sc.next();
-            for(int j = 0; j < m; j++){
-                map[i][j] = line.charAt(j);
-                if(map[i][j] == 'B'){
+
+    // 상,하,좌,우
+    static int[] dirX = {-1, 1, 0, 0};
+    static int[] dirY = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // Input
+        map = new char[N][M];
+        Ball red = null;
+        Ball blue = null;
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = input.charAt(j);
+                if (map[i][j] == 'B') {
                     map[i][j] = '.';
-                    blue = new Point(i, j);
-                }else if(map[i][j] == 'R'){
+                    blue = new Ball(i, j);
+                } else if (map[i][j] == 'R') {
                     map[i][j] = '.';
-                    red = new Point(i, j);
+                    red = new Ball(i, j);
                 }
             }
-        }
+        }// Input End
+
         bfs(new Game(red, blue, 0));
-        System.out.println(result == Integer.MAX_VALUE ? -1 : result);
+        System.out.println(result == 11 ? -1 : result);
     }
+
     private static void bfs(Game game) {
         Queue<Game> queue = new LinkedList<>();
         queue.offer(game);
 
-        while(!queue.isEmpty()){
-            Game curGame = queue.poll();
-            Point curRed = curGame.red;
-            Point curBlue = curGame.blue;
-            if(curGame.count > 9){
+        while (!queue.isEmpty()) {
+            Game cur = queue.poll();
+            Ball curRed = cur.red;
+            Ball curBlue = cur.blue;
+            if (cur.count > 9) {
                 continue;
             }
-            
 
-            // 방향별로 한번씩 기울여줌
-            for(int i = 0; i < 4; i++){
-
-                // 게임 시작 할  때 초기화 해
+            for (int i = 0; i < 4; i++) {
                 map[curRed.x][curRed.y] = 'R';
                 map[curBlue.x][curBlue.y] = 'B';
                 int nRX = curRed.x;
                 int nRY = curRed.y;
                 int nBX = curBlue.x;
                 int nBY = curBlue.y;
-                boolean blueGoal = false;
                 boolean redGoal = false;
-                while(true){
+                boolean blueGoal = false;
+
+                while (true) {
                     boolean change = false;
-                    // 움직였는지 확인하는 상태 
-                    if(!redGoal){
-                        char nextRed = map[nRX + dirX[i]][nRY+dirY[i]];
-                        if(nextRed == '.'){
+
+                    if (!redGoal) {
+                        char nextRed = map[nRX + dirX[i]][nRY + dirY[i]];
+                        if (nextRed == '.') {
                             change = true;
-                            map[nRX+dirX[i]][nRY+dirY[i]] = 'R';
+                            map[nRX + dirX[i]][nRY + dirY[i]] = 'R';
                             map[nRX][nRY] = '.';
                             nRX += dirX[i];
                             nRY += dirY[i];
-                        } else if(nextRed == 'O'){
+                        } else if (nextRed == 'O') {
                             map[nRX][nRY] = '.';
                             redGoal = true;
                             change = true;
                         }
                     }
-                    if(!blueGoal){
-                        char nextBlue = map[nBX + dirX[i]][nBY+dirY[i]];
-                        if(nextBlue == '.'){
+                    if (!blueGoal) {
+                        char nextBlue = map[nBX + dirX[i]][nBY + dirY[i]];
+                        if (nextBlue == '.') {
                             change = true;
-                            map[nBX+dirX[i]][nBY+dirY[i]] = 'B';
+                            map[nBX + dirX[i]][nBY + dirY[i]] = 'B';
                             map[nBX][nBY] = '.';
                             nBX += dirX[i];
                             nBY += dirY[i];
-                        } else if(nextBlue == 'O'){
+                        } else if (nextBlue == 'O') {
                             map[nBX][nBY] = '.';
                             blueGoal = true;
                             break;
                         }
                     }
 
-                    if(!change) break;
+                    if (!change) break;
                 }
 
-                
-                // 원상복구
                 map[nRX][nRY] = '.';
                 map[nBX][nBY] = '.';
 
-                if(blueGoal){
+                if (blueGoal) {
                     continue;
                 }
-                if(redGoal){
-                    result = Math.min(result,curGame.count+1);
+
+                if (redGoal) {
+                    result = Math.min(result, cur.count + 1);
                 }
-                
+
+
                 if(nRX == curRed.x && nRY == curRed.y && nBX == curBlue.x && nBY == curBlue.y){
                     continue;
                 }
-                queue.offer(new Game(new Point(nRX, nRY), new Point(nBX, nBY), curGame.count+1));
-            }
 
-        }
-    }
-    private static void printMap() {
-        for(char[] ms: map){
-            for(char m : ms){
-                System.out.print(m);
+                queue.offer(new Game(new Ball(nRX, nRY), new Ball(nBX, nBY), cur.count+1));
             }
-            System.out.println();
         }
-        System.out.println();
     }
 }
