da3a6736a53d5371b92ef41371d2b145e0503ee1
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-03 21:02:01 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 213 ms, Memory: 27,140 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -5,159 +5,155 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static class Bc {
-		int x;
-		int y;
-		int c;
-		int p;
+    static class BC {
+        int x;
+        int y;
+        int c;
+        int p;
 
-		public Bc(int x, int y, int c, int p) {
-			this.x = x;
-			this.y = y;
-			this.c = c;
-			this.p = p;
-		}
-	}
+        public BC(int x, int y, int c, int p) {
+            this.x = x;
+            this.y = y;
+            this.c = c;
+            this.p = p;
+        }
+    }
 
-	static class Person {
-		int x;
-		int y;
-		PriorityQueue<Bc> queue;
+    static int result;
+    static int[] user1, user2;
+    static BC[] BCs;
+    static int[] C, P;
 
-		public Person(int x, int y) {
-			this.x = x;
-			this.y = y;
-			queue = new PriorityQueue<>((o1, o2) -> o2.p - o1.p);
-		}
+    static PriorityQueue<BC> user1PQ, user2PQ;
 
-		public void move(int n) {
-			x += dirX[n];
-			y += dirY[n];
-		}
 
-		public void getBc() {
-			queue.clear();
-			for (int i = 0; i < K; i++) {
-				if (isIn(x, y, bcs[i])) {
-					queue.add(bcs[i]);
-				}
-			}
-		}
-	}
+    // 0:이동 X, 1: 상, 2: 우, 3: 하, 좌:4
+    static int[] dirX = {0, -1, 0, 1, 0};
+    static int[] dirY = {0, 0, 1, 0, -1};
 
-	static int[] dirX = { 0, 0, 1, 0, -1 };
-	static int[] dirY = { 0, -1, 0, 1, 0 };
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int A = Integer.parseInt(st.nextToken());
+            result = 0;
+            setPQ();
 
-	static Person A = new Person(1,1), B = new Person(10,10);
-	static Bc[] bcs;
-	static int M, K, result;
-	static int[] moveA, moveB;
+            int[] user1Move = new int[M];
+            user1 = new int[]{0, 0};
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                user1Move[i] = Integer.parseInt(st.nextToken());
+            }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			result = 0;
-			A.x = 1;
-			A.y = 1;
-			B.x = 10;
-			B.y = 10;
+            int[] user2Move = new int[M];
+            user2 = new int[]{9, 9};
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                user2Move[i] = Integer.parseInt(st.nextToken());
+            }
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());
-			moveA = new int[M + 1];
-			moveB = new int[M + 1];
+            BCs = new BC[A];
+            C = new int[A];
+            P = new int[A];
+            for (int i = 0; i < A; i++) {
+                st = new StringTokenizer(br.readLine());
+                int y = Integer.parseInt(st.nextToken()) - 1;
+                int x = Integer.parseInt(st.nextToken()) - 1;
+                int c = Integer.parseInt(st.nextToken());
+                int p = Integer.parseInt(st.nextToken());
+                BCs[i] = new BC(x, y, c, p);
+            }
 
-			K = Integer.parseInt(st.nextToken());
-			bcs = new Bc[K];
 
-			// A Input
-			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= M; i++) {
-				moveA[i] = Integer.parseInt(st.nextToken());
-			}
+//            System.out.println("초기 : ");
+            getCharge();
+            for (int i = 0; i < M; i++) {
+                userMove(user1, user1Move[i]);
+                userMove(user2, user2Move[i]);
+//                System.out.println(i + 1 + " 초");
+//                System.out.println("user1: " + user1[0] + " " + user1[1]);
+//                System.out.println("user2: " + user2[0] + " " + user2[1]);
+                getCharge();
+            }
 
-			// B Input
-			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= M; i++) {
-				moveB[i] = Integer.parseInt(st.nextToken());
-			}
+            sb.append('#').append(tc).append(' ').append(result).append('\n');
+        }
+        System.out.println(sb);
+    }
 
-			// BC Input
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-				int p = Integer.parseInt(st.nextToken());
+    private static void getCharge() {
+        user1PQ.clear();
+        user2PQ.clear();
+        for (BC b : BCs) {
+            if (getDist(b, user1) <= b.c) {
+                user1PQ.add(b);
+            }
+            if (getDist(b, user2) <= b.c) {
+                user2PQ.add(b);
+            }
+        }
 
-				bcs[i] = new Bc(x, y, c, p);
-			}
+        BC bc1 = null;
+        if (!user1PQ.isEmpty()) {
+            bc1 = user1PQ.poll();
+        }
 
-			start();
+        BC bc2 = null;
+        if (!user2PQ.isEmpty()) {
+            bc2 = user2PQ.poll();
+        }
 
-			sb.append(String.format("#%d %d\n", tc, result));
-		}
+        if (bc1 == null && bc2 == null) {
+//            System.out.println("충전 할 곳 없음");
+            return;
+        } else if (bc1 != null && bc2 == null) {
+//            System.out.println("user1 충전: " + bc1.p);
+            result += bc1.p;
+        } else if (bc1 == null && bc2 != null) {
+//            System.out.println("user2 충전: " + bc2.p);
+            result += bc2.p;
+        } else {
+            // 같은 BC가 아니라면
+            if (bc1 != bc2) {
+//                System.out.println("user1 , user2 : " + bc1.p + " " + bc2.p);
+                result += bc1.p + bc2.p;
+            }
+            // 같다면
+            else {
+                if (user1PQ.isEmpty() && user2PQ.isEmpty()) {
+//                    System.out.println("나눠 먹음 : " + (bc1.p / 2));
+                    result += (bc1.p / 2) * 2;
+                } else if (user1PQ.isEmpty()) {
+//                    System.out.println("user1, user2: " + bc1.p + " " + user2PQ.peek().p);
+                    result += bc1.p + user2PQ.poll().p;
+                } else if (user2PQ.isEmpty()) {
+//                    System.out.println("user2, user1: " + bc2.p + " " + user1PQ.peek().p);
+                    result += bc2.p + user1PQ.poll().p;
+                } else {
+                    int a = user1PQ.peek().p;
+                    int b = user2PQ.peek().p;
+//                    System.out.println(bc1.p + " " + Math.max(a, b));
+                    result += bc1.p + Math.max(a, b);
+                }
+            }
+        }
+    }
 
-		System.out.println(sb);
-	}
+    private static void setPQ() {
+        user1PQ = new PriorityQueue<>((o1, o2) -> o2.p - o1.p);
+        user2PQ = new PriorityQueue<>((o1, o2) -> o2.p - o1.p);
+    }
 
-	private static void start() {
-		for (int i = 0; i <= M; i++) {
-			// Move
-			A.move(moveA[i]);
-			B.move(moveB[i]);
+    private static void userMove(int[] user, int dir) {
+        user[0] += dirX[dir];
+        user[1] += dirY[dir];
+    }
 
-			A.getBc();
-			B.getBc();
-
-			if (A.queue.isEmpty() && B.queue.isEmpty()) {
-				continue;
-			}
-
-			getCharge();
-		}
-	}
-
-	private static void getCharge() {
-		if (A.queue.isEmpty()) {
-			result += B.queue.poll().p;
-			return;
-		}
-
-		if (B.queue.isEmpty()) {
-			result += A.queue.poll().p;
-			return;
-		}
-
-		Bc bcA = A.queue.poll();
-		Bc bcB = B.queue.poll();
-
-		if (bcA != bcB) {
-			result += bcA.p + bcB.p;
-			return;
-		}
-
-		int tmp = 0;
-		if (A.queue.isEmpty() && B.queue.isEmpty()) {
-			tmp += (bcA.p / 2) * 2;
-		} else if (A.queue.isEmpty()) {
-			tmp += B.queue.poll().p + bcA.p;
-		} else if (B.queue.isEmpty()) {
-			tmp = A.queue.poll().p + bcB.p;
-		} else {
-			if (A.queue.peek().p > B.queue.peek().p) {
-				tmp = A.queue.poll().p + bcB.p;
-			} else {
-				tmp = B.queue.poll().p + bcA.p;
-			}
-		}
-
-		result += tmp;
-	}
-
-	private static boolean isIn(int x, int y, Bc bc) {
-		return (Math.abs(x - bc.x) + Math.abs(y - bc.y)) <= bc.c;
-	}
+    private static int getDist(BC a, int[] user) {
+        return Math.abs(a.x - user[0]) + Math.abs(a.y - user[1]);
+    }
 }
