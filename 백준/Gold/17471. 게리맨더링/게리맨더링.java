4f2832bc181ede20a2c9fb1f23b730841c0f0d55
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-03 19:40:51 +0900

    [Gold IV] Title: 게리맨더링, Time: 80 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,109 +1,102 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class Node {
+        int idx;
+        Node next;
 
-	static boolean[] visit;
-	static int[] peoples;
-	static boolean[][] graph;
-	static int result = Integer.MAX_VALUE, N;
+        public Node(int idx, Node next) {
+            this.idx = idx;
+            this.next = next;
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+    static int N, result = Integer.MAX_VALUE;
+    static Node[] graph;
+    static boolean[] visit;
+    static int[] cost;
 
-		// make
-		peoples = new int[N + 1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			peoples[i] = Integer.parseInt(st.nextToken());
-		} // make End
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-		// 그래프 만들어줌
-		graph = new boolean[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int size = Integer.parseInt(st.nextToken());
-			while (size-- > 0) {
-				int next = Integer.parseInt(st.nextToken());
-				graph[i][next] = graph[next][i] = true;
-			}
-		}
+        // Input
+        cost = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+        graph = new Node[N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < k; j++) {
+                int to = Integer.parseInt(st.nextToken());
+                graph[i] = new Node(to, graph[i]);
+            }
+        } // Input End
 
-		visit = new boolean[N + 1];
-		combination(1, 0);
+        visit = new boolean[N + 1];
+        solve(0);
 
-		System.out.println(result == Integer.MAX_VALUE ? -1 : result);
-	}
+        System.out.println(result == Integer.MAX_VALUE ? -1 : result);
+    }
 
-	private static void combination(int depth, int at) {
-		if (depth == N + 1) {
-			if (at == 0 || at == N + 1) {
-				return;
-			}
+    private static void solve(int depth) {
+        if (depth == N) {
+            isValid();
+            return;
+        }
+        visit[depth] = true;
+        solve(depth + 1);
+        visit[depth] = false;
+        solve(depth + 1);
+    }
 
-			if (!check()) {
-				return;
-			}
+    private static void isValid() {
+        boolean[] aCheck = new boolean[N + 1];
+        boolean[] bCheck = new boolean[N + 1];
+        int aSum = 0, bSum = 0, aStart = 0, bStart = 0;
+        for (int i = 1; i <= N; i++) {
+            if (visit[i]) {
+                aSum += cost[i];
+                aCheck[i] = true;
+                aStart = i;
+            } else {
+                bSum += cost[i];
+                bCheck[i] = true;
+                bStart = i;
+            }
+        }
 
-			int a = 0;
-			int b = 0;
-			for (int i = 1; i <= N; i++) {
-				if (visit[i]) {
-					a += peoples[i];
-				} else {
-					b += peoples[i];
-				}
-			}
+        if (aSum == 0 || bSum == 0) {
+            return;
+        }
 
-			if (result > Math.abs(a - b)) {
-				result = Math.abs(a - b);
-			}
-			return;
-		}
+        int gap = Math.abs(aSum - bSum);
+        if (gap >= result) {
+            return;
+        }
+        dfs(aStart, aCheck);
+        dfs(bStart, bCheck);
+        for (int i = 1; i <= N; i++) {
+            if (aCheck[i] || bCheck[i]) {
+                return;
+            }
+        }
+        result = Math.min(result, gap);
+    }
 
-		visit[depth] = true;
-		combination(depth + 1, at + 1);
-		visit[depth] = false;
-
-		combination(depth + 1, at);
-
-	}
-
-	private static boolean check() {
-		boolean[] aVisit = new boolean[N + 1];
-		boolean[] bVisit = new boolean[N + 1];
-
-		int aStart = 0, bStart = 0;
-		for (int i = 1; i <= N; i++) {
-			if (visit[i]) {
-				aStart = i;
-				aVisit[i] = true;
-			} else {
-				bStart = i;
-				bVisit[i] = true;
-			}
-		}
-
-		dfs(aStart, aVisit);
-		dfs(bStart, bVisit);
-		for (int i = 1; i <= N; i++) {
-			if (aVisit[i] || bVisit[i])
-				return false;
-		}
-		return true;
-	}
-
-	private static void dfs(int cur, boolean[] v) {
-		v[cur] = false;
-		for (int i = 1; i <= N; i++) {
-			if (!v[i] || !graph[cur][i]) {
-				continue;
-			}
-			dfs(i, v);
-		}
-	}
+    private static void dfs(int pos, boolean[] check) {
+        check[pos] = false;
+        for (Node tmp = graph[pos]; tmp != null; tmp = tmp.next) {
+            if (check[tmp.idx]) {
+                dfs(tmp.idx, check);
+            }
+        }
+    }
 }
