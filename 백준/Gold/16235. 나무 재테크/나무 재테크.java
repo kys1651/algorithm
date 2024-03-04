5632ffd4996829ae9dc144388a7d5a20671692e3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-04 17:43:31 +0900

    [Gold III] Title: 나무 재테크, Time: 956 ms, Memory: 296324 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -2,9 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -13,7 +15,6 @@ public class Main {
 		int x;
 		int y;
 		int age;
-		boolean die;
 
 		public Tree(int x, int y, int age) {
 			this.x = x;
@@ -29,7 +30,7 @@ public class Main {
 
 	static int N, M;
 	static int[][] A, addA;
-	static ArrayList<Tree> treeList = new ArrayList<>();
+	static Queue<Tree> trees = new LinkedList<>();
 	static Queue<Tree> dieTree = new LinkedList<>();
 
 	// CW
@@ -60,8 +61,9 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken()) - 1;
 			int y = Integer.parseInt(st.nextToken()) - 1;
 			int age = Integer.parseInt(st.nextToken());
-			treeList.add(new Tree(x, y, age));
+			trees.add(new Tree(x, y, age));
 		} // tree Input End
+		Collections.sort((List<Tree>) trees);
 
 		for (int i = 0; i < K; i++) {
 			spring();
@@ -69,20 +71,18 @@ public class Main {
 			fall();
 			winter();
 		}
-		System.out.println(treeList.size());
+		System.out.println(trees.size());
 	}
 
 	private static void spring() {
-		Collections.sort(treeList);
-		for (int i = 0; i < treeList.size(); i++) {
-			Tree t = treeList.get(i);
-
+		int size = trees.size();
+		while (size-- > 0) {
+			Tree t = trees.poll();
 			if (A[t.x][t.y] - t.age >= 0) {
 				A[t.x][t.y] -= t.age;
-				t.age++;
+				trees.add(new Tree(t.x, t.y, t.age + 1));
 			} else {
 				dieTree.add(t);
-				t.die = true;
 			}
 		}
 	}
@@ -95,22 +95,26 @@ public class Main {
 	}
 
 	private static void fall() {
+		int size = trees.size();
 		ArrayList<Tree> tmp = new ArrayList<>();
-		for (Tree t : treeList) {
-			if (t.die)
-				continue;
+
+		while (size-- > 0) {
+			Tree t = trees.poll();
+			tmp.add(t);
 			if (t.age % 5 == 0) {
 				for (int i = 0; i < 8; i++) {
 					int nX = t.x + dirX[i];
 					int nY = t.y + dirY[i];
 					if (isIn(nX, nY)) {
-						tmp.add(new Tree(nX, nY, 1));
+						trees.add(new Tree(nX, nY, 1));
 					}
 				}
 			}
-			tmp.add(t);
 		}
-		treeList = tmp;
+
+		for (Tree t : tmp) {
+			trees.add(t);
+		}
 
 	}
 
