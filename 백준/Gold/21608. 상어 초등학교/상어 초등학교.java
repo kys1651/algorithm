0a2f75a9e275f5f4645e3967942eacb8c269919a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-05 17:55:34 +0900

    [Gold V] Title: 상어 초등학교, Time: 132 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
@@ -4,35 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[][] map;
+	static int N, maxLike, maxEmpty, maxX, maxY;
+	static int[][] map, likeStudent;
 
-	static int[][] likeStudent;
 	static int[] dirX = { -1, 1, 0, 0 };
 	static int[] dirY = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		int result = 0;
+
+		// Input
 		map = new int[N][N];
 		likeStudent = new int[N * N + 1][4];
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int idx = Integer.parseInt(st.nextToken());
-		map[1][1] = idx;
-		likeStudent[idx][0] = Integer.parseInt(st.nextToken());
-		likeStudent[idx][1] = Integer.parseInt(st.nextToken());
-		likeStudent[idx][2] = Integer.parseInt(st.nextToken());
-		likeStudent[idx][3] = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < N * N; i++) {
-			st = new StringTokenizer(br.readLine());
-			idx = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N * N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int idx = Integer.parseInt(st.nextToken());
 			for (int j = 0; j < 4; j++) {
 				likeStudent[idx][j] = Integer.parseInt(st.nextToken());
 			}
-			sitStudent(idx);
-		}
+			
+			if (i == 0) {
+				map[1][1] = idx;
+			} else {
+				sitStudent(idx);
+			}
+		} // Input End
+
+//		print();
+		
+		int result = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				int count = 0;
@@ -40,10 +41,11 @@ public class Main {
 					int nX = i + dirX[k];
 					int nY = j + dirY[k];
 					if (isIn(nX, nY)) {
-						int cur = map[i][j];
-						if (map[nX][nY] == likeStudent[cur][0] || map[nX][nY] == likeStudent[cur][1]
-								|| map[nX][nY] == likeStudent[cur][2] || map[nX][nY] == likeStudent[cur][3]) {
-							count++;
+						for (int l : likeStudent[map[i][j]]) {
+							if (l == map[nX][nY]) {
+								count++;
+								break;
+							}
 						}
 					}
 				}
@@ -54,62 +56,68 @@ public class Main {
 	}
 
 	private static void print() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				System.out.print(map[i][j] + " ");
+		for(int i = 0; i< N; i++) {
+			for(int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " " );
 			}
 			System.out.println();
 		}
-		System.out.println();
 	}
 
 	private static void sitStudent(int idx) {
-		int maxLike = 0, maxEmpty = 0;
-		int x = 21, y = 21;
+		maxX = maxY = 21;
+		maxEmpty = maxLike = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				int tmpLike = 0, tmpEmpty = 0;
+				if(map[i][j] == 0) {
+					sitCheck(idx,i,j);
+				}
+			}
+		}
+		map[maxX][maxY] = idx;
+	}
 
-				if (map[i][j] == 0) {
-					for (int k = 0; k < 4; k++) {
-						int nX = i + dirX[k];
-						int nY = j + dirY[k];
-						if (!isIn(nX, nY)) {
-							continue;
-						}
-						if (map[nX][nY] == 0) {
-							tmpEmpty++;
-						} else if (map[nX][nY] == likeStudent[idx][0] || map[nX][nY] == likeStudent[idx][1]
-								|| map[nX][nY] == likeStudent[idx][2] || map[nX][nY] == likeStudent[idx][3]) {
-							tmpLike++;
-						}
-					}
-					if (maxLike < tmpLike) {
-						maxLike = tmpLike;
-						maxEmpty = tmpEmpty;
-						x = i;
-						y = j;
-					} else if (maxLike == tmpLike) {
-						if (maxEmpty < tmpEmpty) {
-							maxEmpty = tmpEmpty;
-							x = i;
-							y = j;
-						} else if (maxEmpty == tmpEmpty) {
-							if (x > i) {
-								x = i;
-								y = j;
-							} else if (x == i) {
-								if (y > j) {
-									y = j;
-								}
-							}
-						}
+	private static void sitCheck(int idx,int x, int y) {
+		int tmpLike = 0, tmpEmpty = 0;
+
+		for (int i = 0; i < 4; i++) {
+			int nX = x + dirX[i];
+			int nY = y + dirY[i];
+			if (!isIn(nX, nY)) {
+				continue;
+			}
+			
+			if (map[nX][nY] == 0) {
+				tmpEmpty++;
+			} else {
+				for (int like : likeStudent[idx]) {
+					if (map[nX][nY] == like) {
+						tmpLike++;
+						break;
 					}
 				}
 			}
 		}
-
-		map[x][y] = idx;
+		
+		if (tmpLike > maxLike) {
+			maxLike = tmpLike;
+			maxEmpty = tmpEmpty;
+			maxX = x;
+			maxY = y;
+		} else if (tmpLike == maxLike) {
+			if (tmpEmpty > maxEmpty) {
+				maxEmpty = tmpEmpty;
+				maxX = x;
+				maxY = y;
+			} else if (tmpEmpty == maxEmpty) {
+				if (x < maxX) {
+					maxX = x;
+					maxY = y;
+				} else if (x == maxX) {
+					maxY = Math.min(y, maxY);
+				}
+			}
+		}
 	}
 
 	private static int getSatisfaction(int x) {
