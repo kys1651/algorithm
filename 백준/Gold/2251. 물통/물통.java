4c0c9f9d55f547a92780ac0f036365edc3be5a4e
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-09 22:21:16 +0900

    [Gold V] Title: 물통, Time: 80 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
     static int A, B, C;
-    static boolean[][][] visit = new boolean[201][201][201];
+    static boolean[][] visit = new boolean[201][201];
+    static Set<Integer> answer = new TreeSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,82 +19,53 @@ public class Main {
         dfs(0, 0, C);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= C; i++) {
-            if (visit[0][C - i][i]) {
-                sb.append(i).append(' ');
-            }
+        for (int a : answer) {
+            sb.append(a).append(' ');
         }
         System.out.println(sb);
     }
 
-    private static void dfs(int x, int y, int z) {
-        visit[x][y][z] = true;
+    private static void dfs(int a, int b, int c) {
+        if (visit[a][b]) return;
+        visit[a][b] = true;
 
-        // X에서 갈 수 있는 경우 확인
-        if (x != 0) {
-            int nX = 0;
-            int nY = y + x;
-            if (nY > B) {
-                nX = nY - B;
-                nY = B;
-            }
-            if (!visit[nX][nY][z]) {
-                dfs(nX, nY, z);
-            }
-
-            nX = 0;
-            int nZ = z + x;
-            if (nZ > C) {
-                nX = nZ - C;
-                nZ = C;
-            }
-            if (!visit[nX][y][nZ]) {
-                dfs(nX, y, nZ);
-            }
+        if (a == 0) {
+            answer.add(c);
         }
 
-        if (y != 0) {
-            int nX = x + y;
-            int nY = 0;
-            if (nX > A) {
-                nY = nX - A;
-                nX = A;
-            }
-            if (!visit[nX][nY][z]) {
-                dfs(nX, nY, z);
-            }
-
-            nY = 0;
-            int nZ = z + y;
-            if (nZ > C) {
-                nY = nZ - A;
-                nZ = C;
-            }
-            if (!visit[x][nY][nZ]) {
-                dfs(nX, nY, z);
-            }
+        // a -> b
+        if (a + b > B) {
+            dfs((a + b) - B, B, c);
+        } else {
+            dfs(0, a + b, c);
         }
 
-        if (z != 0) {
-            int nX = x + z;
-            int nZ = 0;
-            if (nX > A) {
-                nZ = nX - A;
-                nX = A;
-            }
-            if (!visit[nX][y][nZ]) {
-                dfs(nX, y, nZ);
-            }
-
-            nZ = 0;
-            int nY = y + z;
-            if (nY > B) {
-                nZ = nY - B;
-                nY = B;
-            }
-            if (!visit[x][nY][nZ]) {
-                dfs(x, nY, nZ);
-            }
+        // b -> a
+        if (a + b > A) {
+            dfs(A, a + b - A, c);
+        } else {
+            dfs(a + b, 0, c);
         }
+
+        // c -> a
+        if (c + a > A) {
+            dfs(A, b, a + c - A);
+        } else {
+            dfs(c + a, b, 0);
+        }
+
+        // c -> b
+        if (c + b > B) {
+            dfs(a, B, c + b - B);
+        } else {
+            dfs(a, c + b, 0);
+        }
+
+
+        // a -> c
+        dfs(0, b, a + c);
+        // b -> c
+        dfs(a, 0, b + c);
+
     }
 }
