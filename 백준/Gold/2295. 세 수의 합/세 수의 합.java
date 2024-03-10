acde7116463a285143c2b3e7003edd20ecdf4afe
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-10 14:29:36 +0900

    [Gold IV] Title: 세 수의 합, Time: 268 ms, Memory: 44940 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,39 +11,24 @@ public class Main {
 
         // Input
         int[] arr = new int[N];
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
 
-        // Input End
-        ArrayList<Integer> two = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                two.add(arr[i] + arr[j]);
+            for (int j = i; j >= 0; j--) {
+                set.add(arr[i] + arr[j]);
             }
         }
-        Collections.sort(two);
+        Arrays.sort(arr);
 
         for (int i = N - 1; i >= 1; i--) {
             for (int j = 0; j < i; j++) {
                 int searchValue = arr[i] - arr[j];
-                if (search(searchValue, two)) {
+                if (set.contains(searchValue)) {
                     System.out.println(arr[i]);
                     return;
                 }
             }
         }
-    }
-
-    private static boolean search(int value, ArrayList<Integer> arr) {
-        int start = 0, end = arr.size() - 1;
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if(arr.get(mid) == value) return true;
-            else if (arr.get(mid) > value) end = mid;
-            else start = mid + 1;
-        }
-        return false;
     }
 }
