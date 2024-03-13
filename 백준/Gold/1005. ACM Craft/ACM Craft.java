a2c5fd1dcf30bf0befb2c6f076cbf54662e3f61c
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-13 14:38:57 +0900

    [Gold III] Title: ACM Craft, Time: 592 ms, Memory: 237604 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class Main {
 	static class Node {
 		int idx;
-		int degree;
 		Node next;
 
 		public Node(int idx, Node next) {
@@ -18,68 +17,56 @@ public class Main {
 		}
 	}
 
-	static int N, K,W;
-	static int[] D;
-	static int[] indegree;
+	static int N, K;
+	static int[] D,dp;
 	static Node[] graph;
-	static long[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-			D = new int[N + 1];
-			dp = new long[N + 1];
-			graph = new Node[N + 1];
-			indegree = new int[N + 1];
-
+			
+			dp = new int[N + 1];
 			Arrays.fill(dp, -1);
 
+			// 걸리는 시간 Input
+			D = new int[N + 1];
 			st = new StringTokenizer(br.readLine());
 			for (int i = 1; i <= N; i++) {
 				D[i] = Integer.parseInt(st.nextToken());
-			}
+			}// Input End
 
+			// graph Input
+			graph = new Node[N + 1];
 			for (int i = 0; i < K; i++) {
 				st = new StringTokenizer(br.readLine());
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
-				indegree[end]++;
-				graph[start] = new Node(end, graph[start]);
-			}
-			W = Integer.parseInt(br.readLine());
+				
+				graph[end] = new Node(start, graph[end]);
+			}// graph Input End
 			
-			solve();
-			
-			System.out.println(dp[W]);
+			int W = Integer.parseInt(br.readLine());
+			sb.append(solve(W)).append('\n');
 		}
+		System.out.println(sb);
 	}
 
-	private static void solve() {
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 1; i <= N; i++) {
-			if (indegree[i] == 0) {
-				queue.add(i);
-				dp[i] = D[i];
-			}
+	private static int solve(int x) {
+		if(dp[x] != -1) {
+			return dp[x];
 		}
-
-		while (!queue.isEmpty()) {
-			int idx = queue.poll();
-			long value = dp[idx];
-			for (Node tmp = graph[idx]; tmp != null; tmp = tmp.next) {
-				indegree[tmp.idx]--;
-				if(value > dp[tmp.idx]) {
-					dp[tmp.idx]= value; 
-				}
-				if (indegree[tmp.idx] == 0) {
-					queue.add(tmp.idx);
-					dp[tmp.idx] += D[tmp.idx]; 
-				}
-			}
+		int ret = 0;
+		for(Node tmp = graph[x]; tmp != null; tmp = tmp.next) {
+			ret = Math.max(ret, solve(tmp.idx));
 		}
+		ret += D[x];
+		return dp[x] = ret;
 	}
 }
