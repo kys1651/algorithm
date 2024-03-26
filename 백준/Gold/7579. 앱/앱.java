8fe43c16b79b0865415bbafd92b781f4fa3fa9d7
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-26 13:34:21 +0900

    [Gold III] Title: 앱, Time: 108 ms, Memory: 12300 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -8,37 +8,33 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int size = N * 100;
-		int[][] dp = new int[N + 1][size + 1];
+		int[] dp = new int[size + 1];
 
-		int[] memory = new int[N+1];
+		int[] memory = new int[N + 1];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
+		for (int i = 1; i <= N; i++) {
 			memory[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int[] cost = new int[N+1];
+
+		int[] cost = new int[N + 1];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= N; i++) {
+		for (int i = 1; i <= N; i++) {
 			cost[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 0; j <= size; j++) {
-				if (j < cost[i])
-					dp[i][j] = dp[i - 1][j];
-				else
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost[i]] + memory[i]);
-			}
-		}
 
-		int result = Integer.MAX_VALUE;
-		for (int i = 0; i <= N; i++) {
-			for (int j = 0; j <= size; j++) {
-				if (dp[i][j] >= M) {
-					result = Math.min(j, result);
-				}
+		for (int i = 1; i <= N; i++) {
+			int m = memory[i];
+			int c = cost[i];
+			for (int j = size; j >= c; j--) {
+				dp[j] = Math.max(dp[j], dp[j - c] + m);
 			}
 		}
-		System.out.println(result);
+		
+		for(int i = 0 ; i <= size; i++) {
+			if(dp[i] >= M) {
+				System.out.println(i);
+				break;
+			}
+		}
 	}
 }
