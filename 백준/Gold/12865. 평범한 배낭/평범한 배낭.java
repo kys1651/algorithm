191248163d57a16bb8c597091d6d5c5e74077bb3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-26 11:05:48 +0900

    [Gold V] Title: 평범한 배낭, Time: 124 ms, Memory: 13352 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,21 +4,20 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		int[] dp = new int[K + 1];
+		int[] item = new int[K + 1];
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int w = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			for (int j = K; j >= w; j--) {
-				dp[j] = Math.max(dp[j], dp[j - w] + v);
+				item[j] = Math.max(item[j], item[j - w] + v);
 			}
 		}
-		System.out.println(dp[K]);
+		System.out.println(item[K]);
 	}
 }
