2cf9ed4e0517834370ddc0054b980ea7fe78be92
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-27 11:27:07 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 125 ms, Memory: 33,960 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,26 +1,46 @@
-import java.util.Scanner;
-class Solution
-{
+import java.util.*;
+import java.io.*;
+
+class Solution {
 	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int n = sc.nextInt();
-            int[][] dp = new int[n+1][2];
-            int result = 0;
-            for(int i = 1; i <= n; i++){
-                dp[i][0] = sc.nextInt();
-            }
-            for(int i = 1; i <=n; i++){
-                int maxIdx = 0;
-                for(int j = 0; j < i; j++){
-                    if(dp[i][0] > dp[j][0] && dp[j][1] > dp[maxIdx][1]) maxIdx = j;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+            int[] LIS = new int[N];
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int count = 0;
+            for(int i = 0 ; i <N; i++){
+                int value = Integer.parseInt(st.nextToken());
+                if(i == 0){
+                	LIS[count++] = value;    
+                }else{
+                    if(LIS[count-1] < value){
+                    	LIS[count++] = value;
+                    }else if(LIS[0] > value){
+                        LIS[0] = value;
+                    }else{
+                        LIS[binary(0,count-1,LIS,value)] = value;
+                    }
                 }
-                dp[i][1] = dp[maxIdx][1] + 1;
-                result = Math.max(dp[i][1] , result);
             }
-            System.out.println("#" + tc + " " + result);
+            sb.append(String.format("#%d %d\n",tc,count));
 		}
+        System.out.println(sb);
 	}
+    private static int binary(int left, int right, int[] LIS, int key){
+    	while(left <= right){
+        	int mid = (left + right) >> 1;
+            if(LIS[mid] < key){
+                left = mid + 1;
+            }else if(LIS[mid] > key){
+            	right = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return left;
+    }
 }
