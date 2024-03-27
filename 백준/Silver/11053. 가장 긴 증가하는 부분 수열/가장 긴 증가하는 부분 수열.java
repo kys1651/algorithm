9ab90fef4b533560c15e1de4d765190026747b1e
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-27 10:33:55 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -8,22 +8,40 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
+		// Input
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] value = new int[N];
-		int[] dp = new int[N];
-		int answer = 1;
+		int[] seq = new int[N];
 		for (int i = 0; i < N; i++) {
-			value[i] = Integer.parseInt(st.nextToken());
-			dp[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if(value[i] > value[j] && dp[j] + 1 > dp[i]){
-					dp[i] = dp[j] + 1;
-					if(answer < dp[i]) {
-						answer = dp[i];
+			seq[i] = Integer.parseInt(st.nextToken());
+		} // Input End
+
+		int[] lis = new int[N];
+		lis[0] = seq[0];
+		int idx = 1;
+		for (int i = 1; i < N; i++) {
+			if (lis[idx - 1] < seq[i]) {
+				lis[idx++] = seq[i];
+			} else if (lis[0] > seq[i]) {
+				lis[0] = seq[i];
+			} else {
+				int left = 0, right = idx - 1;
+				int key = -1;
+				while (left <= right) {
+					int mid = (left + right) >> 1;
+
+					if (lis[mid] < seq[i]) {
+						left = mid + 1;
+					} else if (lis[mid] > seq[i]) {
+						right = mid - 1;
+					} else {
+						key = mid;
+						break;
 					}
 				}
+				lis[key == -1 ? left : key] = seq[i];
 			}
 		}
-		System.out.println(answer);
+
+		System.out.println(idx);
 	}
 }
