35db40029ad2f5bc4b3951da8461c233f8dc3eac
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-27 15:49:43 +0900

    [Gold IV] Title: 스도쿠, Time: 296 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,68 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[][] map = new int[9][9];
-    static int[][] pos = new int[81][2];
-    static int limit;
+	static int[] row = new int[9];
+	static int[] col = new int[9];
+	static int[][] sec = new int[3][3];
+	static int[][] map = new int[9][9];
+	static ArrayList<int[]> emptyList = new ArrayList<>();
+	static int K;
 
-    public static void main(String[] a) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 9; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < 9; j++) {
-                map[i][j] = input.charAt(j) - '0';
-                if (map[i][j] == 0) {
-                    pos[limit][0] = i;
-                    pos[limit++][1] = j;
-                }
-            }
-        }
-        solve(0);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int i = 0; i < 9; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < 9; j++) {
+				map[i][j] = input.charAt(j) - '0';
+				if (map[i][j] == 0) {
+					emptyList.add(new int[] { i, j });
+				} else {
+					int val = 1 << map[i][j];
+					row[i] |= val;
+					col[j] |= val;
+					sec[i / 3][j / 3] |= val;
+				}
+			}
+		}
+		
+		K = emptyList.size();
 
-    private static void solve(int depth) {
-        if (depth == limit) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sb.append(map[i][j]);
-                }
-                sb.append('\n');
-            }
-            System.out.println(sb);
-            System.exit(0);
-        }
+		solve(0);
+	}
 
-        int[] curPos = pos[depth];
-        for (int i = 1; i <= 9; i++) {
-            if (isValid(curPos, i)) {
-                map[curPos[0]][curPos[1]] = i;
-                solve(depth + 1);
-                map[curPos[0]][curPos[1]] = 0;
-            }
-        }
-    }
+	private static void solve(int depth) {
+		if (depth == K) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(map[i][j]);
+				}
+				sb.append('\n');
+			}
+			System.out.println(sb);
+			System.exit(0);
+		}
 
-    private static boolean isValid(int[] curPos, int value) {
-        int x = curPos[0];
-        int y = curPos[1];
-        for (int i = 0; i < 9; i++) {
-            if (map[x][i] == value || map[i][y] == value) {
-                return false;
-            }
-        }
-
-        int dX = (x / 3) * 3;
-        int dY = (y / 3) * 3;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (map[dX + i][dY + j] == value) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+		int[] cur = emptyList.get(depth);
+		int x = cur[0];
+		int y = cur[1];
+		int check = row[x] | col[y] | sec[x/3][y/3];
+		for (int i = 1; i <= 9; i++) {
+			if ((check & 1 << i) == 0) {
+				map[x][y] = i;
+				
+				int val = 1 << i;
+				row[x] |= val;
+				col[y] |= val;
+				sec[x / 3][y / 3] |= val;
+				
+				solve(depth + 1);
+				
+				row[x] -= val;
+				col[y] -= val;
+				sec[x / 3][y / 3] -= val;
+			}
+		}
+		map[x][y] = 0;
+	}
 }
