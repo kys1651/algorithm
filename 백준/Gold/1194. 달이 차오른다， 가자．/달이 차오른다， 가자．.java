038383b1e1cfeba3d86b47e4aeddf9fc9e8a45b2
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-27 14:12:33 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 112 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	static class Player {
@@ -20,46 +16,42 @@ public class Main {
 		}
 	}
 
-	static int N, M, result = Integer.MAX_VALUE;
 	static boolean[][][] visit;
 	static char[][] map;
+	static int N, M, sX, sY;
 
-	static int[] dirX = { -1, 1, 0, 0 };
-	static int[] dirY = { 0, 0, -1, 1 };
+	static int[] dirX = { 1, 0, -1, 0 };
+	static int[] dirY = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		visit = new boolean[N][M][(1 << 7)];
 
 		// Input
-		int startX = 0, startY = 0;
-		visit = new boolean[1 << 6][N][M];
 		map = new char[N][M];
 		for (int i = 0; i < N; i++) {
 			String input = br.readLine();
 			for (int j = 0; j < M; j++) {
-				char ch = input.charAt(j);
-				map[i][j] = ch;
-				// 출발지점
-				if (ch == '0') {
+				map[i][j] = input.charAt(j);
+				if (map[i][j] == '0') {
 					map[i][j] = '.';
-					startX = i;
-					startY = j;
+					sX = i;
+					sY = j;
 				}
 			}
 		} // Input End
 
-		bfs(startX, startY);
-
-		System.out.println(-1);
+		int result = bfs();
+		System.out.println(result);
 	}
 
-	private static void bfs(int startX, int startY) {
+	private static int bfs() {
 		Queue<Player> queue = new LinkedList<>();
-		queue.add(new Player(startX, startY, 0, 0));
-		visit[0][startX][startY] = true;
+		queue.offer(new Player(sX, sY, 0, 0));
+		visit[sX][sY][0] = true;
 
 		while (!queue.isEmpty()) {
 			Player cur = queue.poll();
@@ -68,36 +60,68 @@ public class Main {
 				int nX = cur.x + dirX[i];
 				int nY = cur.y + dirY[i];
 
-				// 배열 범위 밖이거나 || 벽이면 넘어감
-				if (!isIn(nX, nY) || map[nX][nY] == '#' || visit[cur.key][nX][nY]) {
+				if (!isIn(nX, nY) || visit[nX][nY][cur.key] || map[nX][nY] == '#') {
 					continue;
 				}
+				visit[nX][nY][cur.key] = true;
 
-				visit[cur.key][nX][nY] = true;
-				char nextPos = map[nX][nY];
-				if (nextPos == '.') {
-					queue.add(new Player(nX, nY, cur.key, cur.count + 1));
-				} else if ('A' <= nextPos && nextPos <= 'F') {
-					int keyIdx = nextPos - 'A';
-					// 열쇠가 있는 경우
-					if ((cur.key & (1 << keyIdx)) != 0) {
-						queue.add(new Player(nX, nY, cur.key, cur.count + 1));
+				char val = map[nX][nY];
+				// 도착시 카운트 값 리턴
+				if (val == '1') {
+					return cur.count + 1;
+				}
+
+				if (val == '.') {
+					queue.offer(new Player(nX, nY, cur.key, cur.count + 1));
+				}
+				// 문인 경우
+				else if (Character.isUpperCase(val)) {
+					// 열쇠가 있다면 진행
+					if ((cur.key | getKey(val)) == cur.key) {
+						queue.offer(new Player(nX, nY, cur.key, cur.count + 1));
 					}
 				}
-				// 열쇠를 만난 경우
-				else if ('a' <= nextPos && nextPos <= 'f') {
-					int nextKey = cur.key | (1 << nextPos - 'a');
-					queue.add(new Player(nX, nY, nextKey, cur.count + 1));
-				} else if (nextPos == '1') {
-					System.out.println(cur.count + 1);
-					System.exit(0);
+				// 열쇠인 경우
+				else if (Character.isLowerCase(val)) {
+					int nextKey = cur.key;
+					// 열쇠를 보유하지 않았었다면 가져간다.
+					if ((nextKey | getKey(val)) != cur.key) {
+						nextKey = nextKey | getKey(val);
+						visit[nX][nY][nextKey] = true;
+					}
+					queue.offer(new Player(nX, nY, nextKey, cur.count + 1));
 				}
 			}
 		}
+		return -1;
 	}
 
-	// 배열 범위내인지 확인
 	private static boolean isIn(int x, int y) {
 		return x >= 0 && x < N && y >= 0 && y < M;
+	}
+
+	private static int getKey(char key) {
+		switch (key) {
+		case 'a':
+		case 'A':
+			return 1 << 1;
+		case 'b':
+		case 'B':
+			return 1 << 2;
+		case 'c':
+		case 'C':
+			return 1 << 3;
+		case 'd':
+		case 'D':
+			return 1 << 4;
+		case 'e':
+		case 'E':
+			return 1 << 5;
+		case 'f':
+		case 'F':
+			return 1 << 6;
+		default:
+			return 0;
+		}
 	}
 }
