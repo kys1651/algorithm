cd182531fd67f0a707d4654b12d27f5d61820b16
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-27 10:54:11 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 472 ms, Memory: 122060 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -8,47 +8,37 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		// Input
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] seq = new int[N];
+		int[] LIS = new int[N];
+		int count = 0;
 		for (int i = 0; i < N; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
-		} // Input End
-
-		int[] lis = new int[N];
-		lis[0] = seq[0];
-		int idx = 1;
-
-		for (int i = 1; i < N; i++) {
-			// lis값 중 가장 큰 값이 현재 들어온 값보다 작은 경우
-			if (lis[idx - 1] < seq[i]) {
-				// lis에 추가해주면 된다.
-				lis[idx++] = seq[i];
-			}
-			// lis첫번째보다 작은 경우
-			else if (lis[0] > seq[i]) {
-				// 대치
-				lis[0] = seq[i];
-			}
-			// 그 외에 중간에 넣어야하는 경우
-			else {
-				// 0 ~ idx - 1(lis범위내에서 어디에 넣어야할지 찾아준 뒤 그곳에 넣어준다.)
-				int left = 0, right = idx - 1;
-				while (left <= right) {
-					int mid = (left + right) >> 1;
-					if (lis[mid] < seq[i]) {
-						left = mid + 1;
-					} else if (lis[mid] > seq[i]) {
-						right = mid - 1;
-					} else {
-						// 같은 값일 경우 그냥 위치 기억 후 종료해주면 된다.
-						left = mid;
-						break;
-					}
+			int value = Integer.parseInt(st.nextToken());
+			if (i == 0) {
+				LIS[count++] = value;
+			} else {
+				if (LIS[count - 1] < value) {
+					LIS[count++] = value;
+				} else if (LIS[0] > value) {
+					LIS[0] = value;
+				} else {
+					LIS[bineary(0, count - 1, LIS, value)] = value;
 				}
-				lis[left] = seq[i];
 			}
 		}
-		System.out.println(idx);
+		System.out.println(count);
+	}
+
+	private static int bineary(int left, int right, int[] LIS, int value) {
+		while (left <= right) {
+			int mid = (left + right) >> 1;
+			if (LIS[mid] < value) {
+				left = mid + 1;
+			} else if (LIS[mid] > value) {
+				right = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return left;
 	}
 }
