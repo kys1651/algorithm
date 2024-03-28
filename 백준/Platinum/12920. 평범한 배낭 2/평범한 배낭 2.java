fde3db90cdea4458e24c67f6c4087f7b855d99cc
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-28 17:57:01 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 148 ms, Memory: 13464 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.java
@@ -1,32 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int answer = 0;
+		
 		int[] dp = new int[M + 1];
+		int answer = 0;
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int v = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
-			for (int j = 0; k > 0; j++) {
-				int tmp = Math.min(1 << j, k);
+			for (int j = 1; k > 0; j = j << 1) {
+				int tmp = Math.min(j, k);
 				int weight = tmp * v;
 				int cost = tmp * c;
 				for(int l = M; l >= weight; l--) {
 					if(dp[l] < dp[l-weight] + cost) {
 						dp[l] = dp[l-weight] + cost;
-						answer = Math.max(answer, dp[l]);
+						if(dp[l] > answer) answer = dp[l];
 					}
 				}
 				k -= tmp;
 			}
 		}
+
 		System.out.println(answer);
 	}
 }
