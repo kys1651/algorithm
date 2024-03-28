aa88708049a35aad0c28b82574140b3d9b1180cb
김용수 <43926186+kys1651@users.noreply.github.com>
2024-03-28 17:23:52 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 96 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
+	// 위치를 저장하는 클래스
 	static class Point {
 		int x;
 		int y;
@@ -12,22 +13,20 @@ public class Main {
 		}
 	}
 
-	static int N;
-	static Point[] spot;
+	static int N; // 편의점 개수
+	static Point[] spot; // 위치를 저장하는 배열
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		
+		int T = read();
 		while (T-- > 0) {
-			N = Integer.parseInt(br.readLine());
+			N = read();
 			spot = new Point[N + 2];
-
 			// Input
 			for (int i = 0; i <= N + 1; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				spot[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			}
+				spot[i] = new Point(read(), read());
+			}// Input End
 
 			if (bfs()) {
 				sb.append("happy");
@@ -42,27 +41,48 @@ public class Main {
 	private static boolean bfs() {
 		Queue<Point> queue = new LinkedList<>();
 		boolean[] visit = new boolean[N + 1];
+		
 		queue.add(spot[0]);
 		while (!queue.isEmpty()) {
 			Point c = queue.poll();
 
+			// 페스티벌 갈 수 있다면 들어간다.
 			if (getDist(c, spot[N + 1]) <= 1000) {
 				return true;
 			}
 
+			// 편의점을 중간에 확인해준다. 방문했다면 넘어감
 			for (int i = 1; i <= N; i++) {
 				if (visit[i])
 					continue;
+				// 갈 수 있는 거리라면 간다.
 				if (getDist(c, spot[i]) <= 1000) {
 					visit[i] = true;
 					queue.add(spot[i]);
 				}
 			}
 		}
+
+		// 못간다면 false return
 		return false;
 	}
-
+	
+	// 맨해튼 거리
 	private static int getDist(Point a, Point b) {
 		return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
 	}
+	
+    static int read() throws Exception {
+        int n = 0;
+        boolean isNegative = false;
+        while (true) {
+            int input = System.in.read();
+            if (input <= 32)
+                return isNegative ? n * -1 : n;
+            else if (input == '-')
+                isNegative = true;
+            else
+                n = (n << 3) + (n << 1) + (input & 15);
+        }
+    }
 }
