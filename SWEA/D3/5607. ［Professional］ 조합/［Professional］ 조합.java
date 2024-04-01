249c089c4f2fcfa03b0280548dcdd8514de4d984
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-01 14:29:54 +0900

    [D3] Title: [Professional] 조합, Time: 125 ms, Memory: 34,552 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,35 +1,52 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 class Solution{
-    static final long div = 1234567891L;
+    static final long div = 1234567891;
+	static long[] fac = new long[1000001];
+
 	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T= sc.nextInt();
-		for(int tc = 1; tc <= T; tc++) {
-            long n = sc.nextInt();
-            long r = sc.nextInt();
-            
-            long numer = factorial(n);
-            long denom = factorial(r) * factorial(n-r) % div;
-            long result = numer * pow(denom, div - 2) % div;
-            System.out.println("#" + tc + " " + result);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		fac[0] = fac[1] = 1;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+
+			long numerator = factorial(n);
+			long a = factorial(n - r);
+			long b = factorial(r);
+			long denominator = (a * b) % div;
+			
+			denominator = pow(denominator, div - 2);
+			long result = numerator * denominator % div;
+			sb.append(String.format("#%d %d\n", tc, result));
 		}
+		System.out.println(sb);
 	}
-    private static long pow(long base, long expo){
-        long ret = 1L;
-        while(expo > 0){
-            if(expo % 2 != 0){
-                ret = (ret * base) % div;
-            }
-            base = (base * base) % div;
-            expo /= 2;
-        }
-        return ret;
-    }
-    private static long factorial(long n){
-        long fac = 1L;
-        for(long i = 1; i <= n; i++){
-            fac = (fac * i) % div;
-        }
-        return fac;
-    }
+
+	private static long factorial(int n) {
+		if (fac[n] != 0)
+			return fac[n];
+		for (int i = 1; i <= n; i++) {
+			if (fac[i] != 0)
+				continue;
+			fac[i] = (fac[i - 1] * i) % div;
+		}
+		return fac[n];
+	}
+
+	private static long pow(long base, long exponent) {
+		long result = 1;
+		while (exponent != 0) {
+			if (exponent % 2 != 0) {
+				result = result * base % div;
+				exponent--;
+			}
+			exponent = (exponent / 2);
+			base = base * base % div;
+		}
+		return result;
+	}
 }
