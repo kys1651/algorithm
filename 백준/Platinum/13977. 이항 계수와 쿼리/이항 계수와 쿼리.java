94b173891ee3a652ae139a68a206a2249ce993d0
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-01 14:50:15 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 500 ms, Memory: 76300 KB -BaekjoonHub

--- 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static final long mod = 1_000_000_007L;
 	static long[] fac = new long[4_000_001];
+	static int max = 1;
 
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,10 +33,13 @@ public class Main {
 			return 1;
 		if (fac[n] != 0)
 			return fac[n];
-		for (int i = 1; i <= n; i++) {
+		for (int i = max; i <= n; i++) {
 			if (fac[i] != 0)
 				continue;
 			fac[i] = fac[i - 1] * i % mod;
+		}
+		if(n > max) {
+			max = n;
 		}
 		return fac[n];
 	}
