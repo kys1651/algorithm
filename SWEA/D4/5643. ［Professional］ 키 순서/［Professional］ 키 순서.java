aa67879f5e7248e40a031506172cadff4742c1e3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-03 15:59:53 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,874 ms, Memory: 99,424 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,59 +2,57 @@ import java.util.*;
 import java.io.*;
 
 class Solution{
-	static class Node {
-		int idx;
-		Node next;
-
-		public Node(int idx, Node next) {
-			this.idx = idx;
-			this.next = next;
-		}
-	}
-
-	static boolean[] visit;
+	static boolean[][] graph;
+	static int N;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine());
 			int M = Integer.parseInt(br.readLine());
-			int[] inDegree = new int[N + 1];
-			int[] outDegree = new int[N + 1];
-			Node[] inGraph = new Node[N + 1];
-			Node[] outGraph = new Node[N + 1];
+			graph = new boolean[N + 1][N + 1];
 
 			// Input
 			for (int i = 0; i < M; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				inGraph[from] = new Node(to, inGraph[from]);
-				outGraph[to] = new Node(from, outGraph[to]);
+				graph[from][to] = true;
 			} // Input End
 
 			int answer = 0;
 			for (int i = 1; i <= N; i++) {
-				visit = new boolean[N + 1];
-				dfs(i, i, inDegree, inGraph);
-				visit = new boolean[N + 1];
-				dfs(i, i, outDegree, outGraph);
-				if(inDegree[i] + outDegree[i] - 1 == N) answer++;
+				int gt = getGt(i, new boolean[N + 1]);
+				int lt = getLt(i, new boolean[N + 1]);
+				if (gt + lt == N - 1)
+					answer++;
 			}
 			sb.append(String.format("#%d %d\n", tc, answer));
 		}
 		System.out.println(sb);
 	}
 
-	private static void dfs(int idx, int origin, int[] degree, Node[] graph) {
-		visit[idx] = true;
-		degree[origin]++;
-		for (Node next = graph[idx]; next != null; next = next.next) {
-			if (visit[next.idx])
-				continue;
-			dfs(next.idx, origin, degree, graph);
+	private static int getLt(int from, boolean[] visit) {
+		int answer = 0;
+		for (int i = 1; i <= N; i++) {
+			if (!visit[i] && graph[from][i]) {
+				visit[i] = true;
+				answer += getLt(i, visit) + 1;
+			}
 		}
+		return answer;
+	}
+
+	private static int getGt(int to, boolean[] visit) {
+		int answer = 0;
+		for (int i = 1; i <= N; i++) {
+			if (!visit[i] && graph[i][to]) {
+				visit[i] = true;
+				answer += getGt(i, visit) + 1;
+			}
+		}
+		return answer;
 	}
 }
