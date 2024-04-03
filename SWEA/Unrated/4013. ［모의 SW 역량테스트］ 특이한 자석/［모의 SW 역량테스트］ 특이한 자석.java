ee0369a808c7a652863ed0e2bc2d28950f51bee0
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-03 13:55:16 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 115 ms, Memory: 25,100 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -4,86 +4,70 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int[][] arr;
-    static boolean[] visit;
+  static int[][] mag = new int[4][8];
+	static boolean[] visit = new boolean[4];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            int K = Integer.parseInt(br.readLine());
-            arr = new int[4][8];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			int K = Integer.parseInt(br.readLine());
 
-            // Input
-            for (int i = 0; i < 4; i++) {
-                String input = br.readLine();
-                for (int j = 0; j < 8; j++) {
-                    arr[i][j] = input.charAt(j * 2) - '0';
-                }
-            }// Input End
+			// Input
+			for (int i = 0; i < 4; i++) {
+				String input = br.readLine();
+				for (int j = 0; j < 8; j++)
+					mag[i][j] = input.charAt(j * 2) - '0';
+			} // Input End
 
-            for (int i = 0; i < K; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int idx = Integer.parseInt(st.nextToken()); // 돌리는 배열
-                int dir = Integer.parseInt(st.nextToken()); // 방향
-                visit = new boolean[4]; // 방문 처리
-                rotate(idx - 1, dir);
-            }
-            sb.append(String.format("#%d %d\n", tc, getResult()));
-        }
-        System.out.println(sb);
-    }
+			for (int i = 0; i < K; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int pos = Integer.parseInt(st.nextToken()) - 1;
+				int dir = Integer.parseInt(st.nextToken());
+				visit[0] = visit[1] = visit[2] = visit[3] = false;
+				rotate(pos, dir);
+			}
 
-    // 결과를 구해준다.
-    private static int getResult() {
-        int sum = 0;
-        for (int i = 0; i < 4; i++) {
-            sum += (1 << i) * arr[i][0];
-        }
-        return sum;
-    }
+			sb.append(String.format("#%d %d\n", tc, getResult()));
+		}
+		System.out.println(sb);
+	}
+	private static int getResult() {
+		int answer = 0;
+		for (int i = 0; i < 4; i++) {
+			answer += (1 << i) * mag[i][0];
+		}
+		return answer;
+	}
 
-    private static void rotate(int idx, int dir) {
-        visit[idx] = true;
-        // [6]: 왼쪽 날, [2]: 오른쪽 날
-        if (idx > 0) {
-            // 왼쪽날과 왼쪽 톱니바퀴의 오른쪽 날이 다르다면 돌린다.
-            if (arr[idx][6] != arr[idx - 1][2] && !visit[idx - 1]) {
-                rotate(idx - 1, dir == 1 ? -1 : 1);
-            }
-        }
-        if (idx < 3) {
-            // 오른쪽 톱니바퀴의 왼쪽날과 현재 위치에 오른쪽날이 다르다면 돌린다.
-            if (arr[idx][2] != arr[idx + 1][6] && !visit[idx + 1]) {
-                rotate(idx + 1, dir == 1 ? -1 : 1);
-            }
-        }
-        // 1이면 Clock Wise
-        if (dir == 1) {
-            CW(idx);
-        }
-        // 아니라면 Counter Clock Wise
-        else {
-            CCW(idx);
-        }
-    }
+	private static void rotate(int idx, int cw) {
+		if (visit[idx])
+			return;
+		visit[idx] = true;
+		if (idx != 0 && mag[idx][6] != mag[idx - 1][2]) {
+			rotate(idx - 1, -cw);
+		}
+		if (idx != 3 && mag[idx][2] != mag[idx + 1][6]) {
+			rotate(idx + 1, -cw);
+		}
+		if (cw == 1) cw(idx);
+		else ccw(idx);
+	}
 
-    // 시계 반대방향으로 돌려준다.
-    private static void CCW(int idx) {
-        int tmp = arr[idx][0];
-        for (int i = 0; i <= 6; i++) {
-            arr[idx][i] = arr[idx][i + 1];
-        }
-        arr[idx][7] = tmp;
-    }
+	private static void ccw(int idx) {
+		int tmp = mag[idx][0];
+		for (int i = 0; i < 7; i++) {
+			mag[idx][i] = mag[idx][i + 1];
+		}
+		mag[idx][7] = tmp;
+	}
 
-    // 시계 방향으로 배열을 돌린다.
-    private static void CW(int idx) {
-        int tmp = arr[idx][7];
-        for (int i = 7; i >= 1; i--) {
-            arr[idx][i] = arr[idx][i - 1];
-        }
-        arr[idx][0] = tmp;
-    }
+	private static void cw(int idx) {
+		int tmp = mag[idx][7];
+		for (int i = 7; i >= 1; i--) {
+			mag[idx][i] = mag[idx][i-1];
+		}
+		mag[idx][0] = tmp;
+	}
 }
