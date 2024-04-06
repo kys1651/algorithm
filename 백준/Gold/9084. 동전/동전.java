5fb0509e27e38afcc0937bb2b373b83e140154e8
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-06 18:06:09 +0900

    [Gold V] Title: 동전, Time: 84 ms, Memory: 11912 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] unit = new int[N + 1];
+            // Input
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                unit[i] = Integer.parseInt(st.nextToken());
+            }// Input End
+
+            int total = Integer.parseInt(br.readLine());
+            int[] dp = new int[total + 1];
+            for (int i = 1; i <= N; i++) {
+                int coin = unit[i];
+                for (int j = coin; j <= total; j++) {
+                    if(j == coin) dp[j]++;
+                    else dp[j] = dp[j] + dp[j - coin];
+                }
+            }
+            sb.append(dp[total]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
