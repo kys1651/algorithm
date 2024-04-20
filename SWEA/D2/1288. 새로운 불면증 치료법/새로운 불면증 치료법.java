9068cc830a30079006eae50f9a7ec5977460cb70
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-20 16:36:18 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 121 ms, Memory: 26,024 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -7,26 +7,20 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+        int total = (1 << 10) - 1;
         for (int tc = 1; tc <= T; tc++) {
             int N = Integer.parseInt(br.readLine());
             int answer = 0;
             int count = 0;
-            boolean zero = false;
-            while (!(zero && answer == 511)) {
-                count++;
-                int tmp = N * count;
-                while (tmp != 0) {
-                    if (tmp % 10 == 0) {
-                        zero = true;
-                        tmp /= 10;
-                        continue;
-                    }
-                    int digit = tmp % 10;
-                    answer |= (1 << (digit - 1));
-                    tmp /= 10;
+            for (count = 1; ; count++) {
+                char[] arr = String.valueOf(N * count).toCharArray();
+                for (char c : arr) {
+                    int digit = c - '0';
+                    answer |= (1 << digit);
                 }
+                if(answer == total) break;
             }
-            sb.append(String.format("#%d %d\n",tc,count * N));
+            sb.append(String.format("#%d %d\n", tc, count * N));
         }
         System.out.println(sb);
     }
