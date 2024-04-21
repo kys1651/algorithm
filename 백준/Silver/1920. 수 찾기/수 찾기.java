639f2de59e64216313f070af939b6b2b44afbd64
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-21 15:26:02 +0900

    [Silver IV] Title: 수 찾기, Time: 480 ms, Memory: 58640 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,30 +5,29 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		HashSet<Integer> set = new HashSet<>();
+    static final String one = "1\n";
+    static final String zero = "0\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			set.add(Integer.parseInt(st.nextToken()));
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
 
-		int M = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		st = new StringTokenizer(br.readLine());
-		while (M-- > 0) {
-			int tmp = Integer.parseInt(st.nextToken());
-			if(set.contains(tmp)) {
-				sb.append(1);
-			}else {
-				sb.append(0);
-			}
-			sb.append('\n');
-		}
-		
-		System.out.println(sb);
-	}
-
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            if (set.contains(tmp)) {
+                sb.append(one);
+            }else{
+                sb.append(zero);
+            }
+        }
+        System.out.print(sb);
+    }
 }
