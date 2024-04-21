9ee8af0301123c04cfb806ac1b18c4571bf0c704
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-21 16:54:32 +0900

    [Bronze II] Title: 3대 측정, Time: 740 ms, Memory: 152356 KB -BaekjoonHub

--- 백준/Bronze/20299. 3대 측정/3대 측정.java
+++ 백준/Bronze/20299. 3대 측정/3대 측정.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,31 +11,27 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> pq = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
         int count = 0;
         for (int i = 0; i < N; i++) {
             int sum = 0;
-            st = new StringTokenizer(br.readLine());
+            String input = br.readLine();
+            st = new StringTokenizer(input);
             int[] arr = new int[3];
             for (int j = 0; j < 3; j++) {
                 arr[j] = Integer.parseInt(st.nextToken());
                 if(arr[j] < L) {
                     sum = -1;
                     break;
-                };
+                }
                 sum += arr[j];
             }
             if (sum >= K) {
                 count++;
-                pq.add(arr[0]);
-                pq.add(arr[1]);
-                pq.add(arr[2]);
+                sb.append(input).append(' ');
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(count).append('\n');
-        while(!pq.isEmpty()) sb.append(pq.poll()).append(' ');
+        System.out.println(count);
         System.out.println(sb);
     }
 }
