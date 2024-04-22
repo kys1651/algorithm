60da6e92d9748fd41e86ac47c307b965364105e9
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-22 11:26:17 +0900

    [Gold II] Title: 통나무 옮기기, Time: 92 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.java
+++ 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.java
@@ -9,14 +9,14 @@ public class Main {
         Point s;
         Point m;
         Point e;
-        int isRow;
+        int row;
         int count;
 
-        public Tree(Point s, Point m, Point e, int isRow, int count) {
+        public Tree(Point s, Point m, Point e, int row, int count) {
             this.s = s;
             this.m = m;
             this.e = e;
-            this.isRow = isRow;
+            this.row = row;
             this.count = count;
         }
     }
@@ -29,30 +29,24 @@ public class Main {
             this.x = x;
             this.y = y;
         }
-
-        public boolean equals(Point o) {
-            return x == o.x && y == o.y;
-        }
     }
 
-    static boolean[][][] visit;
-    static boolean[][] map;
-
+    // 상,하,좌,우 왼쪽 위 대각, 오른쪽 위 대각, 오른쪽 아래 대각, 왼쪽 아래 대각
     static int[] dirX = {-1, 1, 0, 0, -1, -1, 1, 1};
     static int[] dirY = {0, 0, -1, 1, -1, 1, 1, -1};
-
-    static Point endS, endE;
+    static boolean[][][] visit;
+    static boolean[][] map;
+    static Point endS, endM, endE;
     static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new boolean[N][N];
         visit = new boolean[2][N][N];
-        endE = new Point(0, 0);
-        Tree start = new Tree(null, null, null, 0, 0);
+        map = new boolean[N][N];
 
         // Input
+        Tree start = new Tree(null, null, null, 0, 0);
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
             for (int j = 0; j < N; j++) {
@@ -61,81 +55,76 @@ public class Main {
                     if (start.s == null) start.s = new Point(i, j);
                     else if (start.m == null) start.m = new Point(i, j);
                     else start.e = new Point(i, j);
-                }
-                if (ch == 'E') {
+                } else if (ch == 'E') {
                     if (endS == null) endS = new Point(i, j);
-                    endE.x = i;
-                    endE.y = j;
-                }
-                if (ch != '1') {
-                    map[i][j] = false;
-                } else {
+                    else if (endM == null) endM = new Point(i, j);
+                    else endE = new Point(i, j);
+                } else if (ch == '1') {
                     map[i][j] = true;
                 }
             }
-        }// Input End
+        } // Input End
 
-        if (start.s.y == start.e.y) start.isRow = 1;
+        if (start.s.y == start.e.y) start.row = 1;
         System.out.println(bfs(start));
     }
 
     private static int bfs(Tree start) {
         Queue<Tree> queue = new LinkedList<>();
         queue.add(start);
-        visit[start.isRow][start.s.x][start.s.y] = true;
-        visit[start.isRow][start.e.x][start.e.y] = true;
+        visit[start.row][start.s.x][start.s.y] = visit[start.row][start.e.x][start.e.y] = true;
         while (!queue.isEmpty()) {
             Tree cur = queue.poll();
-
-            if (endS.equals(cur.s) && endE.equals(cur.e)) {
+            if (cur.s.x == endS.x && cur.s.y == endS.y && cur.e.x == endE.x && cur.e.y == endE.y) {
                 return cur.count;
             }
-            // 상,하,좌,우 먼저 확인
+            // 상,하,좌,우
             for (int i = 0; i < 4; i++) {
-                Point nS = new Point(cur.s.x + dirX[i], cur.s.y + dirY[i]);
-                Point nM = new Point(cur.m.x + dirX[i], cur.m.y + dirY[i]);
-                Point nE = new Point(cur.e.x + dirX[i], cur.e.y + dirY[i]);
-                if (!isIn(nS) || !isIn(nM) || !isIn(nE) || (visit[cur.isRow][nS.x][nS.y] && visit[cur.isRow][nE.x][nE.y])) {
+                int nSX = cur.s.x + dirX[i];
+                int nSY = cur.s.y + dirY[i];
+                int nEX = cur.e.x + dirX[i];
+                int nEY = cur.e.y + dirY[i];
+                int nMX = cur.m.x + dirX[i];
+                int nMY = cur.m.y + dirY[i];
+                // 나무가 있거나 범위밖이거나 방문한 적 있으면 갈 필요 X
+                if (!isIn(nSX, nSY) || !isIn(nEX, nEY) || !isIn(nMX, nMY) || (visit[cur.row][nSX][nSY] && visit[cur.row][nEX][nEY])) {
                     continue;
                 }
-                visit[cur.isRow][nS.x][nS.y] = visit[cur.isRow][nE.x][nE.y] = true;
-                queue.add(new Tree(nS, nM, nE, cur.isRow, cur.count + 1));
+                visit[cur.row][nSX][nSY] = visit[cur.row][nEX][nEY] = true;
+                queue.add(new Tree(new Point(nSX, nSY), new Point(nMX, nMY), new Point(nEX, nEY), cur.row, cur.count + 1));
             }
-            // 회전 확인
-            if (!isPossible(cur.m)) {
-                continue;
+
+            boolean can = true;
+            for (int i = 0; i < 8; i++) {
+                int nX = cur.m.x + dirX[i];
+                int nY = cur.m.y + dirY[i];
+                if (!isIn(nX, nY)) {
+                    can = false;
+                    break;
+                }
             }
-            Point nS = null;
-            Point nE = null;
-            int nextRow = (cur.isRow + 1) % 2;
-            if (cur.isRow == 0) {
-                nS = new Point(cur.m.x - 1, cur.m.y);
-                nE = new Point(cur.m.x + 1, cur.m.y);
+
+            if (!can) continue;
+            int nSX = cur.m.x, nSY = cur.m.y;
+            int nEX = cur.m.x, nEY = cur.m.y;
+            int nRow = (cur.row + 1) % 2;
+            // 현재 행이라면 열로 변경
+            if (cur.row == 0) {
+                nSX = cur.m.x - 1;
+                nEX = cur.m.x + 1;
             } else {
-                nS = new Point(cur.m.x, cur.m.y - 1);
-                nE = new Point(cur.m.x, cur.m.y + 1);
+                nSY = cur.m.y - 1;
+                nEY = cur.m.y + 1;
             }
-            if (!isIn(nS) || !isIn(nE) || (visit[nextRow][nS.x][nS.y] && visit[nextRow][nE.x][nE.y])) {
-                continue;
+            if (!(visit[nRow][nSX][nSY] && visit[nRow][nEX][nEY])) {
+                visit[nRow][nSX][nSY] = visit[nRow][nEX][nEY] = true;
+                queue.add(new Tree(new Point(nSX, nSY), cur.m, new Point(nEX, nEY), nRow, cur.count + 1));
             }
-            visit[nextRow][nS.x][nS.y] = visit[nextRow][nE.x][nE.y] = true;
-            queue.add(new Tree(nS, cur.m, nE, nextRow, cur.count + 1));
         }
         return 0;
     }
 
-    private static boolean isIn(Point p) {
-        return p.x >= 0 && p.x < N && p.y >= 0 && p.y < N && !map[p.x][p.y];
-    }
-
-    private static boolean isPossible(Point p) {
-        for (int i = 0; i < 8; i++) {
-            int nX = p.x + dirX[i];
-            int nY = p.y + dirY[i];
-            if (!isIn(new Point(nX, nY))) {
-                return false;
-            }
-        }
-        return true;
+    private static boolean isIn(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N && !map[x][y];
     }
 }
