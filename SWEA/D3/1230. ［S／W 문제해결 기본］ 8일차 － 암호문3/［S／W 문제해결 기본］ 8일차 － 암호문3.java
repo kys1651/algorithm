5d6a30696f2970757a8e9226bc6483dc19adcffa
김용수 <43926186+kys1651@users.noreply.github.com>
2024-04-23 20:28:17 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 356 ms, Memory: 90,420 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -1,36 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Scanner;
-class Solution
-{
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = 10;
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int n = sc.nextInt();
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= 10; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            // Input
             LinkedList<Integer> list = new LinkedList<>();
-            for(int i = 0; i < n; i++){ list.add (sc.nextInt()); }
-            int m = sc.nextInt();
-            for(int i = 0; i < m; i++){
-                String command = sc.next();
-                if(command.equals("I")){
-                    int idx = sc.nextInt();
-                    int count = sc.nextInt();
-                    while(count --> 0){ list.add(idx++, sc.nextInt()); }
-                }else if(command.equals("D")){
-                    int idx = sc.nextInt();
-                    int count = sc.nextInt();
-                    while(count --> 0){ list.remove(idx); }
-                }else{
-                    int count = sc.nextInt();
-                    while(count --> 0){ list.add(sc.nextInt()); }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                list.add(Integer.parseInt(st.nextToken()));
+            }// Input End
+
+            int M = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                String command = st.nextToken();
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                if (command.equals("I")) {
+                    for (int j = 0; j < y; j++) {
+                        int s = Integer.parseInt(st.nextToken());
+                        list.add(x + j, s);
+                    }
+                } else if (command.equals("D")) {
+                    while (y-- > 0) {
+                        list.remove(x);
+                    }
+                } else {
+                    list.addLast(y);
+                    for (int j = 1; j < x; j++) {
+                        y = Integer.parseInt(st.nextToken());
+                        list.addLast(y);
+                    }
                 }
             }
-            System.out.print("#" + tc + " ");
-            for(int i = 0; i < 10; i++){
-                System.out.print(list.get(i) + " " );
+
+            sb.append('#').append(tc).append(' ');
+            for (int i = 0; i < 10; i++) {
+                sb.append(list.get(i)).append(' ');
             }
-            System.out.println();
-		}
-	}
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
 }
