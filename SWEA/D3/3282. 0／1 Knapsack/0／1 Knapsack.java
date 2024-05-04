dd3836e8a84269f6f923c08b6e57e82c14801c67
김용수 <43926186+kys1651@users.noreply.github.com>
2024-05-04 18:03:38 +0900

    [D3] Title: 0/1 Knapsack, Time: 171 ms, Memory: 31,384 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,31 +1,27 @@
-import java.util.Scanner;
-class Solution
-{
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++) {
-            int n = sc.nextInt();
-            int k = sc.nextInt();
-            int[] weight = new int[n+1];
-            int[] cost = new int[n+1];
-            
-            for(int i = 1; i <= n; i++){
-                weight[i] = sc.nextInt();
-                cost[i] = sc.nextInt();
-            }
-            int[][] dp = new int[n+1][k+1];
-            for(int i = 1; i <= n; i++){
-                for(int j =1; j <= k; j++){
-                    if(weight[i] > j){
-                        dp[i][j] = dp[i-1][j];
-                    }else{
-                        dp[ i ][ j ] = Math.max(dp[ i - 1 ][ j - weight [ i ] ] + cost[i]
-                                                , dp[ i - 1 ][ j ] );
-                    }
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int[] dp = new int[K + 1];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int weight = Integer.parseInt(st.nextToken());
+                int value = Integer.parseInt(st.nextToken());
+                for (int j = K; j >= weight; j--) {
+                    dp[j] = Math.max(dp[j], dp[j - weight] + value);
                 }
             }
-            System.out.println("#" + tc + " " + dp[n][k]);
-		}
+            sb.append(String.format("#%d %d\n", tc, dp[K]));
+        }
+        System.out.println(sb);
     }
 }
