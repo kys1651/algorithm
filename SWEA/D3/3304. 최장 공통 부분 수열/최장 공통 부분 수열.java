f58a5f2ddd255b912ba8087d3ce04649f398041a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-05-04 17:09:27 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 189 ms, Memory: 39,240 KB -BaekjoonHub

--- SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
-class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++) {
-            String a = sc.next();
-            String b = sc.next();
-            int[][] dp = new int[a.length()+1][b.length() + 1];
-            for(int i = 1; i <= a.length(); i++){
-                for(int j = 1; j <= b.length(); j++){
-                    if(a.charAt(i-1) == b.charAt(j-1)){
-                        dp[i][j] = dp[i-1][j-1] + 1;
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            String[] input = br.readLine().split(" ");
+            String A = input[0];
+            String B = input[1];
+            int lenA = A.length();
+            int lenB = B.length();
+            int[][] dp = new int[lenA + 1][lenB + 1];
+            for (int i = 1; i <= lenA; i++) {
+                for (int j = 1; j <= lenB; j++) {
+                    if (A.charAt(i-1) == B.charAt(j-1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
                     }else{
-                        dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                     }
                 }
             }
-            System.out.println("#" + tc + " " + dp[a.length()][b.length()]);
-		}
-	}
+            sb.append(String.format("#%d %d\n", tc, dp[lenA][lenB]));
+        }
+        System.out.println(sb);
+    }
 }
