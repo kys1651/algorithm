ccffb4436733c50c67d85b326970d5ffba9db9b5
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-02 20:41:49 +0900

    [Gold II] Title: 스크루지 민호, Time: 412 ms, Memory: 50504 KB -BaekjoonHub

--- 백준/Gold/12896. 스크루지 민호/스크루지 민호.java
+++ 백준/Gold/12896. 스크루지 민호/스크루지 민호.java
@@ -2,34 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] graph;
+    static List<Integer>[] graph;
     static boolean[] visit;
-    static int maxCount, max;
+    static int maxCount, max, N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        graph = new List[N + 1];
+        visit = new boolean[N + 1];
+        init();
 
-        graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        StringTokenizer st;
 
         // Input
         for (int i = 1; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
+
             graph[u].add(v);
             graph[v].add(u);
         }// Input End
 
-        maxCount = -1;
-        visit = new boolean[N + 1];
         dfs(1, 0);
 
-        maxCount = -1;
         visit = new boolean[N + 1];
+        maxCount = -1;
         dfs(max, 0);
 
         System.out.println((maxCount + 1) / 2);
@@ -40,11 +39,19 @@ public class Main {
             maxCount = count;
             max = cur;
         }
+        
         visit[cur] = true;
 
         for (int next : graph[cur]) {
             if (visit[next]) continue;
             dfs(next, count + 1);
+        }
+    }
+
+
+    private static void init() {
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
     }
 }
