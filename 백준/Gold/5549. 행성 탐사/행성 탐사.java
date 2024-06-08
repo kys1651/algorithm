f593dbd0fc01a78b7110afb9fd51d567e2b23f58
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-08 13:05:09 +0900

    [Gold V] Title: 행성 탐사, Time: 672 ms, Memory: 81060 KB -BaekjoonHub

--- 백준/Gold/5549. 행성 탐사/행성 탐사.java
+++ 백준/Gold/5549. 행성 탐사/행성 탐사.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,25 +7,27 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][][] map = new int[3][N + 1][M + 1];
-
         int K = Integer.parseInt(br.readLine());
 
-        HashMap<Character, Integer> getIdx = new HashMap<>();
-        getIdx.put('J', 0);
-        getIdx.put('O', 1);
-        getIdx.put('I', 2);
-
+        // Input
+        int[][][] map = new int[3][N + 1][M + 1];
         for (int i = 1; i <= N; i++) {
             String input = br.readLine();
             for (int j = 1; j <= M; j++) {
-                char ch = input.charAt(j - 1);
                 for (int k = 0; k < 3; k++) {
                     map[k][i][j] = map[k][i - 1][j] + map[k][i][j - 1] - map[k][i - 1][j - 1];
                 }
-                map[getIdx.get(ch)][i][j]++;
+
+                char ch = input.charAt(j - 1);
+                if (ch == 'J') {
+                    map[0][i][j]++;
+                } else if (ch == 'O') {
+                    map[1][i][j]++;
+                } else {
+                    map[2][i][j]++;
+                }
             }
-        }
+        }// Input End
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= K; i++) {
@@ -36,11 +36,11 @@ public class Main {
             int sY = Integer.parseInt(st.nextToken());
             int eX = Integer.parseInt(st.nextToken());
             int eY = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < 3; j++) {
-                int result = map[j][eX][eY] - map[j][sX - 1][eY] - map[j][eX][sY - 1] + map[j][sX - 1][sY - 1];
-                sb.append(result).append(' ');
-            }
-            sb.append('\n');
+
+            int jungle = map[0][eX][eY] - map[0][sX - 1][eY] - map[0][eX][sY - 1] + map[0][sX - 1][sY - 1];
+            int ocean = map[1][eX][eY] - map[1][sX - 1][eY] - map[1][eX][sY - 1] + map[1][sX - 1][sY - 1];
+            int ice = map[2][eX][eY] - map[2][sX - 1][eY] - map[2][eX][sY - 1] + map[2][sX - 1][sY - 1];
+            sb.append(jungle).append(' ').append(ocean).append(' ').append(ice).append('\n');
         }
 
         System.out.println(sb);
