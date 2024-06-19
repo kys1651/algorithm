9044bc823217cb2f004b0ad041b7f36ea72be665
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-19 14:39:50 +0900

    [Gold IV] Title: 샘터, Time: 428 ms, Memory: 57152 KB -BaekjoonHub

--- 백준/Gold/18513. 샘터/샘터.java
+++ 백준/Gold/18513. 샘터/샘터.java
@@ -2,7 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static final int SIZE = 100_000_000;
+    static class Point {
+        int idx;
+        int count;
+
+        public Point(int idx, int count) {
+            this.idx = idx;
+            this.count = count;
+        }
+    }
+
+    static Set<Integer> visit = new HashSet<>();
+    static Queue<Point> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,35 +21,36 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> queue = new LinkedList<>();
-        HashMap<Integer, Integer> visit = new HashMap<>();
         // Input
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int idx = Integer.parseInt(st.nextToken());
-            queue.add(idx);
-            visit.put(idx, 0);
+            visit.add(idx);
+            queue.add(new Point(idx, 0));
         }// Input End
 
+        System.out.println(solve(M));
+    }
+
+    private static long solve(int count) {
+        long answer = 0;
 
         int[] dir = {-1, 1};
-        while (visit.size() != (M + N)) {
-            int cur = queue.poll();
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
 
-            for (int i = 0; i < 2 && visit.size() != M + N; i++) {
-                int nX = cur + dir[i];
-
-                if (visit.getOrDefault(nX, SIZE * 2) <= visit.get(cur) + 1) continue;
-
-                queue.add(nX);
-                visit.put(nX, visit.get(cur) + 1);
+            for (int i = 0; i <= 1; i++) {
+                int nX = cur.idx + dir[i];
+                if(!visit.contains(nX)) {
+                    queue.add(new Point(nX, cur.count + 1));
+                    visit.add(nX);
+                    count--;
+                    answer += cur.count + 1;
+                    if(count == 0) return answer;
+                }
             }
         }
 
-        long answer = 0;
-        for (int v : visit.values()) {
-            answer += v;
-        }
-        System.out.println(answer);
+        return answer;
     }
 }
