d194484c912c9501e5ca53a39758943b23c98689
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-20 18:01:49 +0900

    [Silver I] Title: 피아노 체조, Time: 448 ms, Memory: 59004 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.java
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -4,30 +4,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int[] num = new int[n + 2];
+        int N = Integer.parseInt(br.readLine());
+        int[] value = new int[N + 2];
 
+        // Input
+        int prev = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int prev = Integer.parseInt(st.nextToken());
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
             int cur = Integer.parseInt(st.nextToken());
-            if(prev > cur){
-                num[i-1]++;
-            }
-            num[i] = num[i-1];
+            if (prev > cur) value[i - 1]++;
+            value[i] = value[i - 1];
             prev = cur;
-        }
+        } //Input End
 
-        int q = Integer.parseInt(br.readLine());
-        while (q-- > 0) {
+        StringBuilder sb = new StringBuilder();
+        int Q = Integer.parseInt(br.readLine());
+        while (Q-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken())-1;
-            int y = Integer.parseInt(st.nextToken())-1;
-            sb.append(num[y]-num[x]).append('\n');
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            sb.append(value[b] - value[a]).append('\n');
         }
         System.out.println(sb);
     }
