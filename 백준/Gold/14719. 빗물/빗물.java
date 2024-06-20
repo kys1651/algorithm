269023823dd8d5dc1f421950bfbedd1d9c2a1085
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-20 18:29:07 +0900

    [Gold V] Title: 빗물, Time: 68 ms, Memory: 11700 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -7,27 +7,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int H = Integer.parseInt(st.nextToken());
-        int W = Integer.parseInt(st.nextToken());
+        st.nextToken();
 
+        int W = Integer.parseInt(st.nextToken());
         int[] height = new int[W];
+        int[] leftHeight = new int[W];
+        int[] rightHeight = new int[W];
+
         // Input
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < W; i++) {
             height[i] = Integer.parseInt(st.nextToken());
         }// Input End
 
-        int answer = 0;
-        for (int i = 1; i < W - 1; i++) {
-            int left = height[i], right = height[i];
-            for(int j = i -1; j >= 0; j--) left = Math.max(left, height[j]);
-            for(int j = i +1; j < W; j++) right = Math.max(right, height[j]);
-            if(left != height[i] && right != height[i]) {
-                answer += Math.min(left, right) - height[i];
-            }
+        leftHeight[0] = height[0];
+        for (int i = 1; i < W; i++) {
+            leftHeight[i] = Math.max(leftHeight[i - 1], height[i]);
         }
 
-        System.out.println(answer);
+        rightHeight[W - 1] = height[W - 1];
+        for (int i = W - 2; i >= 0; i--) {
+            rightHeight[i] = Math.max(rightHeight[i + 1], height[i]);
+        }
 
+        int answer = 0;
+        for (int i = 1; i < W - 1; i++) {
+            answer += Math.min(leftHeight[i], rightHeight[i]) - height[i];
+        }
+        System.out.println(answer);
     }
 }
