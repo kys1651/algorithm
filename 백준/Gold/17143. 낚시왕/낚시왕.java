80eb108476ef8ce3bc65b99e54f4ab34bfcce73a
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-20 17:34:24 +0900

    [Gold I] Title: 낚시왕, Time: 300 ms, Memory: 19580 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -6,47 +6,68 @@ import java.util.StringTokenizer;
 public class Main {
     static class Shark {
         int idx;
-        int x;
-        int y;
+        int x, y;
         int speed;
-        int d;
+        int dir;
         int size;
-        boolean die;
 
-        public Shark(int idx, int x, int y, int speed, int d, int size) {
+        public Shark(int idx, int x, int y, int speed, int dir, int size) {
             this.idx = idx;
             this.x = x;
             this.y = y;
             this.speed = speed;
-            this.d = d;
+            this.dir = dir;
             this.size = size;
         }
 
-        public int getDist() {
-            // 위를 보는 경우
-            if (d == 0) {
-                return x;
-            } else if (d == 1) {
-                return R - x - 1;
-            } else if (d == 2) {
-                return C - y - 1;
-            } else {
-                return y;
+        private void move() {
+            int totalSpeed = speed;
+            while (true) {
+                int dist = getDist();
+
+                // 거리가 0이라면 벽을 보고 있는 상황 반대를 보게 함
+                if (dist == 0) {
+                    other();
+                }
+                // 현재 가야하는 거리보다 남아있는 거리가 더 긴 경우 보내주면 된다.
+                else if (dist >= totalSpeed) {
+                    x = x + dirX[dir] * totalSpeed;
+                    y = y + dirY[dir] * totalSpeed;
+                    break;
+                } else {
+                    totalSpeed -= dist;
+                    x = x + dirX[dir] * dist;
+                    y = y + dirY[dir] * dist;
+                    other();
+                }
             }
         }
 
-        public void otherDir() {
-            if (d == 0) d = 1;
-            else if (d == 1) d = 0;
-            else if (d == 2) d = 3;
-            else d = 2;
+        private int getDist() {
+            switch (dir) {
+                case 0: return x;
+                case 1: return R - x - 1;
+                case 2: return C - y - 1;
+                default: return y;
+            }
+        }
+
+        private void other() {
+            switch (dir) {
+                case 0: dir = 1;break;
+                case 1: dir = 0;break;
+                case 2: dir = 3;break;
+                case 3: dir = 2;
+            }
         }
     }
 
-    static int R, C, M;
-    static Shark[] sharks;
+    static int R, C, M, answer, count;
     static int[][][] map;
+    static boolean[] die;
+    static Shark[] sharks;
 
+    // 위, 아래, 오른쪽, 왼쪽
     static int[] dirX = {-1, 1, 0, 0};
     static int[] dirY = {0, 0, 1, -1};
 
@@ -55,123 +76,76 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        M = count = Integer.parseInt(st.nextToken());
+        map = new int[2][R][C];
+        die = new boolean[M + 1];
+        sharks = new Shark[M + 1];
 
         // Input
-        sharks = new Shark[M + 1];
-        map = new int[R][C][2];
         for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken()) - 1;
-            int c = Integer.parseInt(st.nextToken()) - 1;
-            int s = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken()) - 1;
-            int z = Integer.parseInt(st.nextToken());
-            sharks[i] = new Shark(i, r, c, s, d, z);
-            if (map[r][c][0] == 0) {
-                map[r][c][0] = i;
-            } else {
-                int sIdx = map[r][c][0];
-                if (sharks[sIdx].size > z) {
-                    sharks[i].die = true;
-                } else {
-                    sharks[sIdx].die = true;
-                    map[r][c][0] = i;
-                }
-            }
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int speed = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken()) - 1;
+            int size = Integer.parseInt(st.nextToken());
+            sharks[i] = new Shark(i, x, y, speed, dir, size);
+
+            map[0][x][y] = i;
         }// Input End
 
-        int answer = 0;
-        for (int i = 0; i < C; i++) {
-            answer += sharkCatch(i);
-            moveShark();
+        for (int i = 0; i < C && count != 0; i++) {
+            catchShark(i);
+            sharkMove();
         }
-
         System.out.println(answer);
     }
 
-    private static void moveShark() {
+    private static void sharkMove() {
         for (int i = 1; i <= M; i++) {
             Shark s = sharks[i];
-
-            // 잡힌 상어라면 처리 X
-            if (s.die) {
+            // 죽은 상어라면 넘어간다.
+            if (die[s.idx]) {
                 continue;
             }
+            // 죽은 상어가 아니라면 이동해줌
+            int curX = s.x;
+            int curY = s.y;
+            s.move();
+            changeSharkPos(s, curX, curY);
+        }
+        int[][] tmp = map[1];
+        map[1] = map[0];
+        map[0] = tmp;
+    }
 
-            int speed = s.speed;
-            if (s.d == 0 || s.d == 1) {
-                speed = speed % (2 * (R - 1));
-            } else {
-                speed = speed % (2 * (C - 1));
-            }
-
-            // 기존 위치 저장
-            int x = s.x;
-            int y = s.y;
-            while (true) {
-                int dist = s.getDist();
-
-                // 벽에 붙어있는 경우 그냥 돌아보게만 한다.
-                if (dist == 0) {
-                    s.otherDir();
-                }
-                // 거리가 그 안에 해결 된다면 보내준다.
-                else if (dist >= speed) {
-                    s.x = s.x + dirX[s.d] * speed;
-                    s.y = s.y + dirY[s.d] * speed;
-                    break;
-                }
-                // 거리가 남는다면 벽에 붙히고 나머지 계산
-                else {
-                    speed -= dist;
-                    s.x = s.x + dirX[s.d] * dist;
-                    s.y = s.y + dirY[s.d] * dist;
-                    s.otherDir();
-                }
-            }
-            posShark(s, x, y);
+    private static void changeSharkPos(Shark s, int x, int y) {
+        map[0][x][y] = 0;
+        int idx = map[1][s.x][s.y];
+        if (idx == 0) {
+            map[1][s.x][s.y] = s.idx;
+            return;
         }
 
-        for (int i = 1; i <= M; i++) {
-            Shark s = sharks[i];
-            if (s.die) continue;
-            if (map[s.x][s.y][0] != s.idx) {
-                map[s.x][s.y][0] = s.idx;
-                map[s.x][s.y][1] = 0;
-            }
+        count--;
+        if (sharks[idx].size > s.size) {
+            die[s.idx] = true;
+        } else {
+            die[idx] = true;
+            map[1][s.x][s.y] = s.idx;
         }
     }
 
-    private static void posShark(Shark s, int x, int y) {
-        // 기존 위치에서 상어 지워준다.
-        map[x][y][0] = 0;
-        if (map[s.x][s.y][1] == 0) {
-            map[s.x][s.y][1] = s.idx;
-        } else if (map[s.x][s.y][1] != 0) {
-            int sIdx = map[s.x][s.y][1];
-            Shark originShark = sharks[sIdx];
-            if (originShark.size > s.size) {
-                s.die = true;
-            } else {
-                map[s.x][s.y][1] = s.idx;
-                originShark.die = true;
-            }
-        }
-    }
-
-    private static int sharkCatch(int pos) {
-        // 가장 가까운 상어를 받아온다.
+    private static void catchShark(int pos) {
         for (int i = 0; i < R; i++) {
-            if (map[i][pos][0] == 0) {
-                continue;
+            if (map[0][i][pos] != 0) {
+                int idx = map[0][i][pos];
+                die[idx] = true;
+                answer += sharks[idx].size;
+                map[0][i][pos] = 0;
+                count--;
+                return;
             }
-            Shark s = sharks[map[i][pos][0]];
-            map[i][pos][0] = 0;
-            s.die = true;
-            return s.size;
         }
-
-        return 0;
     }
 }
