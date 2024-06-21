1295094aef617b14527923449f89133e0d479823
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-21 15:47:13 +0900

    [Gold IV] Title: LCS 3, Time: 132 ms, Memory: 17568 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.java
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -2,25 +2,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 문제: BOJ_1958_LCS3
+ * 메모리: 17676KB
+ * 시간: 140ms
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String a = br.readLine();
         String b = br.readLine();
         String c = br.readLine();
-
         System.out.println(getLCS(a, b, c));
     }
 
     private static int getLCS(String a, String b, String c) {
-        int[][][] dp = new int[a.length() + 1][b.length() + 1][c.length() + 1];
-
         int aLen = a.length(), bLen = b.length(), cLen = c.length();
+        int[][][] dp = new int[aLen + 1][bLen + 1][cLen + 1];
         for (int i = 1; i <= aLen; i++) {
             char aChar = a.charAt(i - 1);
             for (int j = 1; j <= bLen; j++) {
                 char bChar = b.charAt(j - 1);
-
                 for (int k = 1; k <= cLen; k++) {
                     char cChar = c.charAt(k - 1);
                     if (aChar == bChar && bChar == cChar) {
