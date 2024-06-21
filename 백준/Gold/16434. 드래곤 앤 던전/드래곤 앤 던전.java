0fb812f7a58c60d902a9e617f0a4ca5e3ed4dc6d
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-21 14:52:21 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 432 ms, Memory: 54576 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -4,64 +4,55 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		long attack = Long.parseLong(st.nextToken());
-		long[] as = new long[n];
-		long[] hs = new long[n];
+    static int N;
+    static long[] hp, damage;
 
-		long tmpAttack = attack;
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			long t = Long.parseLong(st.nextToken());
-			long a = Long.parseLong(st.nextToken());
-			long h = Long.parseLong(st.nextToken());
-			// 몬스터
-			if (t == 1) {
-				long count = h / tmpAttack;
-				if (h % tmpAttack == 0) {
-					count--;
-				}
-				as[i] = count * a;
-			} else {
-				tmpAttack += a;
-				hs[i] = h;
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        hp = new long[N]; // 각 방에서 얻을 수 있는 치유
+        damage = new long[N]; // 각 방에서 받는 피해
 
-		long lower = 0,upper = Long.MAX_VALUE - 1;
-		while (lower < upper) {
-			long mid = (lower + upper) / 2;
-//			System.out.println(lower + ", " + upper + " -> " + mid);
+        long attack = Integer.parseInt(st.nextToken());
+        long t, a, h, upper = 1;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            t = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            // 몬스터 방인 경우
+            if (t == 1) {
+                long count = h / attack;
+                if (h % attack == 0) count--;
+                damage[i] = count * a;
+                upper += damage[i];
+            } else {
+                attack += a;
+                hp[i] = h;
+            }
+        }
 
-			long tmp = mid;
-			for (int i = 0; i < n; i++) {
-//				System.out.println(tmp);
-				if (as[i] == 0) {
-					tmp += hs[i];
-					if (tmp > mid) {
-						tmp = mid;
-					}
-				} else {
-					tmp -= as[i];
-				}
+        long lower = 1;
+        while (lower < upper) {
+            long mid = (lower + upper) >> 1;
+            if (isCan(mid)) {
+                upper = mid;
+            } else {
+                lower = mid + 1;
+            }
+        }
+        System.out.println(lower);
+    }
 
-				if (tmp <= 0) {
-					break;
-				}
-			}
-
-//			System.out.println(tmp + " 결고ㅏ");
-			if (tmp <= 0) {
-				lower = mid + 1;
-			} else {
-				upper = mid;
-			}
-		}
-
-		System.out.println(lower);
-
-	}
+    private static boolean isCan(long maxHp) {
+        long h = maxHp;
+        for (int i = 0; i < N; i++) {
+            h -= damage[i];
+            h += hp[i];
+            if (h > maxHp) h = maxHp;
+            if (h <= 0) return false;
+        }
+        return true;
+    }
 }
