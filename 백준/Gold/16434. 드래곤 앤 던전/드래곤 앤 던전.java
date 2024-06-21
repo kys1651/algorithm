ca85cbdc6423043a57e861a763994529693233a1
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-21 14:57:38 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 344 ms, Memory: 53008 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -4,55 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static long[] hp, damage;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        hp = new long[N]; // 각 방에서 얻을 수 있는 치유
-        damage = new long[N]; // 각 방에서 받는 피해
+        int N = Integer.parseInt(st.nextToken());
 
-        long attack = Integer.parseInt(st.nextToken());
-        long t, a, h, upper = 1;
+        long curAttack = Integer.parseInt(st.nextToken());
+        long curHp = 0, max = 0;
+
+        long t, a, h;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             t = Integer.parseInt(st.nextToken());
             a = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
+
             // 몬스터 방인 경우
             if (t == 1) {
-                long count = h / attack;
-                if (h % attack == 0) count--;
-                damage[i] = count * a;
-                upper += damage[i];
+                // 용수가 공격 받는 횟수
+                long count = h / curAttack;
+                if (h % curAttack == 0) count--; // 0으로 나우어 떨어진 경우 ( 용사가 공격해서 끝난 경우 )
+                curHp += count * a; // 현재 체력에 받는 데미지를 더 해준다.
+                max = Math.max(curHp, max); // 가장 많이 받는 데미지를 기록
             } else {
-                attack += a;
-                hp[i] = h;
+                curAttack += a; // 공격력 증가
+                curHp = Math.max(curHp - h, 0); // 0보다 작아지는 경우( 최대 체력 이상으로 체력을 회복한 경우 )
             }
         }
+        System.out.println(max + 1);
 
-        long lower = 1;
-        while (lower < upper) {
-            long mid = (lower + upper) >> 1;
-            if (isCan(mid)) {
-                upper = mid;
-            } else {
-                lower = mid + 1;
-            }
-        }
-        System.out.println(lower);
-    }
-
-    private static boolean isCan(long maxHp) {
-        long h = maxHp;
-        for (int i = 0; i < N; i++) {
-            h -= damage[i];
-            h += hp[i];
-            if (h > maxHp) h = maxHp;
-            if (h <= 0) return false;
-        }
-        return true;
     }
 }
