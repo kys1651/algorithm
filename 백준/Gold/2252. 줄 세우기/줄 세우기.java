52fde6dbc7046c1ceb49c711c2f8e1e70c675c22
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-25 15:17:35 +0900

    [Gold III] Title: 줄 세우기, Time: 324 ms, Memory: 42260 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,62 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-	static class Node {
-		int to;
-		Node next;
+class Main {
+    static class Node{
+        int idx;
+        Node next;
 
-		public Node(int to, Node next) {
-			this.to = to;
-			this.next = next;
-		}
-	}
+        public Node(int idx, Node next) {
+            this.idx = idx;
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] outDegree = new int[N + 1];
+        Node[] graph = new Node[N + 1];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		Node[] nodes = new Node[N + 1];
-		int[] count = new int[N + 1];
-		int[] result = new int[N];
+        // Input
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            graph[to] = new Node(from, graph[to]);
+            outDegree[from]++;
+        }// Input End
 
-		// Input
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int to = Integer.parseInt(st.nextToken());
-			int from = Integer.parseInt(st.nextToken());
-			nodes[from] = new Node(to,nodes[from]);
-			count[to]++;
-		}
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            if(outDegree[i] == 0) queue.add(i);
+        }
 
-		// 0인 것 받기
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 1; i <= N; i++) {
-			if (count[i] == 0) {
-				queue.add(i);
-			}
-		}
-
-		for (int i = N - 1; i >= 0; i--) {
-			result[i] = queue.poll();
-			for(Node tmp = nodes[result[i]]; tmp != null ; tmp = tmp.next) {
-				if(--count[tmp.to]== 0) {
-					queue.add(tmp.to);
-				}
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			sb.append(result[i]).append(' ');
-		}
-		System.out.println(sb);
-	}
+        StringBuilder sb = new StringBuilder();
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            sb.append(cur).append(' ');
+            for (Node tmp = graph[cur]; tmp != null; tmp = tmp.next) {
+                outDegree[tmp.idx]--;
+                if(outDegree[tmp.idx] == 0) queue.add(tmp.idx);
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            if(outDegree[i] != 0) sb.append(i).append(' ');
+        }
+        System.out.println(sb);
+    }
 }
