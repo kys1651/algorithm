7fabde371283a8a8fa73d6467cdf6497e9d89f39
김용수 <43926186+kys1651@users.noreply.github.com>
2024-06-26 17:41:20 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 728 ms, Memory: 52556 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -1,38 +1,28 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Main {
-    static class Time {
-        int start;
-        int end;
-
-        public Time(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        PriorityQueue<Time> prev = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
-        PriorityQueue<Time> ing = new PriorityQueue<>((o1, o2) -> o1.end - o2.end);
-        // Input
         int N = Integer.parseInt(br.readLine());
-        while (N-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            prev.add(new Time(start, end));
-        }// Input End
 
-        ing.add(prev.poll());
-        while (!prev.isEmpty()) {
-            Time t = prev.poll();
-            if (ing.peek().end <= t.start) ing.poll();
-            ing.add(t);
+        // Input
+        int[][] time = new int[N][2];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] =  Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }// Input End
+        Arrays.sort(time, (o1, o2) -> o1[0] - o2[0]);
+
+        PriorityQueue<int[]> ing = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        ing.add(time[0]);
+        for(int i = 1;i < N; i++) {
+            if (ing.peek()[1] <= time[i][0]) ing.poll();
+            ing.add(time[i]);
         }
         System.out.println(ing.size());
     }
