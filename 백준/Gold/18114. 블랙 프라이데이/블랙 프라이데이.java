94d119849d4c7b771d26f646ecc33ca34e90d8c0
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-01 15:28:22 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 108 ms, Memory: 13616 KB -BaekjoonHub

---
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int[] items = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            items[i] = value;
+            if(value == C) {
+                System.out.println(1);
+                return;
+            }
+        }
+        Arrays.sort(items);
+
+        System.out.println(isValidWeight(items, C) ? 1 : 0);
+    }
+
+    private static boolean isValidWeight(int[] items, int goal) {
+        int l = 0, r = items.length - 1;
+
+        while(l < r){
+            int sum = items[l] + items[r];
+            if(sum > goal){
+                r--;
+            }else if(sum == goal){
+                return true;
+            } else {
+                int rest = goal - sum;
+                if(binarySearch(l + 1, r - 1, rest, items)) return true;
+                l++;
+            }
+        }
+        return false;
+    }
+
+    private static boolean binarySearch(int l, int r, int key, int[] items) {
+        while(l <= r){
+            int mid = (r + l) >> 1;
+            if(items[mid] == key){
+                return true;
+            } else if (items[mid] < key) {
+                l = mid + 1;
+            }else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+}
