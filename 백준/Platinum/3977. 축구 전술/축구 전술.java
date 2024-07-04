a2d400f72e40d8f8f3eac9118ff20ecf7baf54bb
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-05 00:45:41 +0900

    [Platinum IV] Title: 축구 전술, Time: 1688 ms, Memory: 234388 KB -BaekjoonHub

---
+++ 백준/Platinum/3977. 축구 전술/축구 전술.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final String CONFUSED = "Confused";
+    static ArrayList<Integer>[] graph;
+    static Stack<Integer> answer;
+    static Stack<Integer> stack;
+    static int[] parent, sccIdx;
+    static boolean[] visit;
+    static int size, idx;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            if (t != 0) br.readLine();
+
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            init(N);
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                graph[from].add(to);
+            }
+
+            for (int i = 0; i < N; i++) {
+                if (parent[i] == 0) dfs(i);
+            }
+
+            int[] inDegree = new int[size];
+            for (int i = 0; i < N; i++) {
+                for (int next : graph[i]) {
+                    if (sccIdx[i] != sccIdx[next]) {
+                        inDegree[sccIdx[next]]++;
+                    }
+                }
+            }
+
+            int count = 0, start = 0;
+            for (int i = 0; i < size; i++) {
+                if (inDegree[i] == 0) {
+                    count++;
+                    start = i;
+                }
+            }
+
+            if (count > 1) sb.append(CONFUSED).append('\n');
+            else {
+                for (int i = 0; i < N; i++) {
+                    if (sccIdx[i] == start) {
+                        sb.append(i).append('\n');
+                    }
+                }
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    private static void init(int N) {
+        size = idx = 0;
+        parent = new int[N];
+        sccIdx = new int[N];
+        visit = new boolean[N];
+        stack = new Stack<>();
+        graph = new ArrayList[N];
+        for (int i = 0; i < N; i++) graph[i] = new ArrayList<>();
+    }
+
+    private static int dfs(int cur) {
+        parent[cur] = ++idx;
+        stack.push(cur);
+
+        int p = parent[cur];
+        for (int next : graph[cur]) {
+            if (parent[next] == 0) p = Math.min(p, dfs(next));
+            else if (!visit[next]) p = Math.min(p, parent[next]);
+        }
+
+        if (p == parent[cur]) {
+            while (true) {
+                int t = stack.pop();
+                visit[t] = true;
+                sccIdx[t] = size;
+                if (t == cur) break;
+            }
+            size++;
+        }
+
+        return p;
+    }
+}
