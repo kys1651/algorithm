48b3256cf01260887e9c47d5fff15fc6aba954d3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-04 23:36:15 +0900

    [Platinum II] Title: ATM, Time: 3324 ms, Memory: 450888 KB -BaekjoonHub

--- 백준/Platinum/4013. ATM/ATM.java
+++ 백준/Platinum/4013. ATM/ATM.java
@@ -1,87 +1,75 @@
-import java.io.IOException;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-    static int N, idx, size;
-    static int[] parent, sccIdx, dp, value, restaurant;
-    static boolean[] visit;
-    static ArrayList<Integer>[] graph, sccList;
+    static ArrayList<Integer>[] graph, sccGraph;
     static Stack<Integer> stack = new Stack<>();
+    static int[] parent, sccValue, sccIdx, dp;
+    static boolean[] visit;
+    static int sccSize, idx;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        parent = new int[N + 1];
+        sccIdx = new int[N + 1];
+        visit = new boolean[N + 1];
         graph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
-        visit = new boolean[N + 1];
-        parent = new int[N + 1];
 
-        sccIdx = new int[N + 1];
-
+        // Edge Input
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
-
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-
             graph[from].add(to);
-        }
+        }// Edge Input End
 
+        getSccGraph(N, br);
+
+        st = new StringTokenizer(br.readLine());
+        int S = sccIdx[Integer.parseInt(st.nextToken())];
+        int P = Integer.parseInt(st.nextToken());
+
+        bfs(S);
+
+        int answer = 0;
+        st = new StringTokenizer(br.readLine());
+        while(P--> 0) answer = Math.max(answer, dp[sccIdx[Integer.parseInt(st.nextToken())]]);
+        System.out.println(answer);
+    }
+
+    private static void getSccGraph(int N, BufferedReader br) throws IOException {
         for (int i = 1; i <= N; i++) {
             if (parent[i] == 0) dfs(i);
         }
-
-
-        dp = new int[size];
-        value = new int[size];
-        sccList = new ArrayList[size];
-        for (int i = 0; i < size; i++) sccList[i] = new ArrayList<>();
+        dp = new int[sccSize];
+        sccValue = new int[sccSize];
+        sccGraph = new ArrayList[sccSize];
+        for (int i = 0; i < sccSize; i++) sccGraph[i] = new ArrayList<>();
 
         for (int i = 1; i <= N; i++) {
-            // 싸이클 인덱스에 더해줌
-            value[sccIdx[i]] += Integer.parseInt(br.readLine());
-
-            // 연결되는 싸이클에 넣어줘서 새로운 노드 생성
+            sccValue[sccIdx[i]] += Integer.parseInt(br.readLine());
             for (int next : graph[i]) {
                 if (sccIdx[i] != sccIdx[next]) {
-                    sccList[sccIdx[i]].add(sccIdx[next]);
+                    sccGraph[sccIdx[i]].add(sccIdx[next]);
                 }
             }
         }
-
-        st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-
-        restaurant = new int[p];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < p; i++) restaurant[i] = Integer.parseInt(st.nextToken());
-
-        bfs(sccIdx[S]);
-
-        int max = 0;
-        for (int i = 0; i < p; i++) {
-            int idx = sccIdx[restaurant[i]];
-            max = Math.max(dp[idx], max);
-        }
-        System.out.println(max);
     }
 
-    private static void bfs(int s) {
+    private static void bfs(int S) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(s);
-        dp[s] = value[s];
+        dp[S] = sccValue[S];
+        queue.add(S);
 
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int cur = queue.poll();
-            for(int next : sccList[cur]) {
-                int nextWeight = dp[cur] + value[next];
+            for (int next : sccGraph[cur]) {
+                int nextWeight = dp[cur] + sccValue[next];
                 if (dp[next] < nextWeight) {
                     dp[next] = nextWeight;
                     queue.add(next);
@@ -94,20 +82,18 @@ public class Main {
         parent[cur] = ++idx;
         stack.push(cur);
         int p = parent[cur];
-
         for (int next : graph[cur]) {
-            if(parent[next] == 0) p = Math.min(p, dfs(next));
-            else if(!visit[next]) p = Math.min(p, parent[next]);
+            if (parent[next] == 0) p = Math.min(p, dfs(next));
+            else if (!visit[next]) p = Math.min(p, parent[next]);
         }
-
         if (p == parent[cur]) {
-            while(true){
+            while (true) {
                 int t = stack.pop();
                 visit[t] = true;
-                sccIdx[t] = size;
-                if(t == cur) break;
+                sccIdx[t] = sccSize;
+                if (cur == t) break;
             }
-            size++;
+            sccSize++;
         }
         return p;
     }
