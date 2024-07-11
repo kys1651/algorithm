da0b21ba8c410d5c7bd09c9413202b371153aefd
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-11 23:09:40 +0900

    [Gold I] Title: 최종 순위, Time: 480 ms, Memory: 44236 KB -BaekjoonHub

---
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final String FAIL = "IMPOSSIBLE";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            boolean[][] graph = new boolean[N + 1][N + 1];
+            int[] inDegree = new int[N + 1];
+
+            // Input
+            int[] rank = new int[N + 1];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                int v = Integer.parseInt(st.nextToken());
+                for (int j = 1; j <= N; j++) {
+                    if (rank[j] != 0 || j == v) continue;
+                    inDegree[v]++;
+                    graph[j][v] = true;
+                }
+                rank[v] = i;
+            }// Input End
+
+            int M = Integer.parseInt(br.readLine());
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                if (graph[a][b]) {
+                    graph[a][b] = false;
+                    graph[b][a] = true;
+                    inDegree[a]++;
+                    inDegree[b]--;
+                } else {
+                    graph[b][a] = false;
+                    graph[a][b] = true;
+                    inDegree[b]++;
+                    inDegree[a]--;
+                }
+            }
+
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 1; i <= N; i++) {
+                if (inDegree[i] == 0) {
+                    queue.add(i);
+                }
+            }
+
+            int count = N;
+            while(count != 0){
+                if (queue.isEmpty()) {
+                    break;
+                }
+
+                int cur = queue.poll();
+                rank[count--] = cur;
+                for (int i = 1; i <= N; i++) {
+                    if (i == cur || !graph[cur][i]) continue;
+                    if (--inDegree[i] == 0) {
+                        queue.add(i);
+                    }
+                }
+            }
+            if (count == 0) {
+                for (int i = 1; i <= N; i++) {
+                    sb.append(rank[i]).append(' ');
+                }
+            } else {
+                sb.append(FAIL);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+
+    }
+}
