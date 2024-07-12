f210ae8fe562cf3b422f8b757a573ae3997f60ea
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-12 22:32:23 +0900

    [Silver I] Title: 유성, Time: 684 ms, Memory: 154216 KB -BaekjoonHub

--- 백준/Silver/10703. 유성/유성.java
+++ 백준/Silver/10703. 유성/유성.java
@@ -18,12 +18,18 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        // Input
-        map = new int[N][M];
+        // Init
         ArrayList<int[]> meteorList = new ArrayList<>();
-        String input;
         int[] wallHeight = new int[M];
-        Arrays.fill(wallHeight, 3001);
+        int[] meteorHeight = new int[M];
+        for (int i = 0; i < M; i++) {
+            wallHeight[i] = 3001;
+            meteorHeight[i] = -1;
+        }// Init End
+
+        // Input
+        String input;
+        map = new int[N][M];
         for (int i = 0; i < N; i++) {
             input = br.readLine();
             for (int j = 0; j < M; j++) {
@@ -33,20 +39,15 @@ public class Main {
                     wallHeight[j] = Math.min(wallHeight[j], i);
                 } else if (ch == 'X') {
                     map[i][j] = 0;
+                    meteorHeight[j] = Math.max(meteorHeight[j], i);
                     meteorList.add(new int[]{i, j});
                 }
             }
         }// Input End
 
         int minDist = 3001;
-        for (int[] m : meteorList) {
-            int b = wallHeight[m[1]] - m[0] - 1;
-            minDist = Math.min(minDist, b);
-        }
-
-        for(int[] m : meteorList) {
-            map[m[0] + minDist][m[1]] = METEOR;
-        }
+        for (int i = 0; i < M; i++) if (meteorHeight[i] != -1) minDist = Math.min(minDist, wallHeight[i] - meteorHeight[i] - 1);
+        for (int[] m : meteorList) map[m[0] + minDist][m[1]] = METEOR;
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
