26cff419c543549ccb58133e0cc66b8262eea339
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-12 22:33:44 +0900

    [Silver I] Title: 유성, Time: 660 ms, Memory: 155616 KB -BaekjoonHub

--- 백준/Silver/10703. 유성/유성.java
+++ 백준/Silver/10703. 유성/유성.java
@@ -4,19 +4,11 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static final int WALL = 1, METEOR = 2;
-
-    static int N, M;
-    static int[][] map;
-
-    static int[] dirX = {-1, 1, 0, 0};
-    static int[] dirY = {0, 0, -1, 1};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         // Init
         ArrayList<int[]> meteorList = new ArrayList<>();
@@ -29,13 +21,13 @@ public class Main {
 
         // Input
         String input;
-        map = new int[N][M];
+        int[][] map = new int[N][M];
         for (int i = 0; i < N; i++) {
             input = br.readLine();
             for (int j = 0; j < M; j++) {
                 char ch = input.charAt(j);
                 if (ch == '#') {
-                    map[i][j] = WALL;
+                    map[i][j] = 1;
                     wallHeight[j] = Math.min(wallHeight[j], i);
                 } else if (ch == 'X') {
                     map[i][j] = 0;
@@ -47,18 +39,14 @@ public class Main {
 
         int minDist = 3001;
         for (int i = 0; i < M; i++) if (meteorHeight[i] != -1) minDist = Math.min(minDist, wallHeight[i] - meteorHeight[i] - 1);
-        for (int[] m : meteorList) map[m[0] + minDist][m[1]] = METEOR;
+        for (int[] m : meteorList) map[m[0] + minDist][m[1]] = 2;
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == WALL) {
-                    sb.append("#");
-                } else if (map[i][j] == METEOR) {
-                    sb.append("X");
-                } else {
-                    sb.append(".");
-                }
+                if (map[i][j] == 1) sb.append('#');
+                else if (map[i][j] == 2) sb.append('X');
+                else sb.append('.');
             }
             sb.append('\n');
         }
