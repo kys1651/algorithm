331ff43de0d05e298874c5102295dbbd35f3f3a7
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-20 11:24:00 +0900

    [D3] Title: 문자열 교집합, Time: 1,761 ms, Memory: 143,104 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.java
@@ -1,22 +1,32 @@
 import java.util.*;
-class Solution
-{
+import java.io.*;
+
+class Solution {
 	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int tc = 1; tc <= T; tc++)
-		{
-            int n = sc.nextInt();
-            int m =sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
             HashSet<String> set = new HashSet<>();
-            for(int i = 0; i < n; i++){
-                set.add(sc.next());
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < a; i++){
+                set.add(st.nextToken());
             }
-            int result = 0;
-            for(int j = 0; j < m; j++){
-                if(set.contains(sc.next())) result++;
+            
+            st = new StringTokenizer(br.readLine());
+            int ret = 0;
+            for(int i = 0; i < b; i++){
+                if(set.contains(st.nextToken())) ret++;
             }
-            System.out.println("#" + tc + " " + result);
-        }
+		
+            sb.append("#").append(tc).append(' ').append(ret).append('\n');
+		}
+        System.out.println(sb);
 	}
 }
