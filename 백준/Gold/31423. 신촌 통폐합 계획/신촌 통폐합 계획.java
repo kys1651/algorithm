09adf16d727e8f613aa1a94e713cadb0c070c757
김용수 <43926186+kys1651@users.noreply.github.com>
2024-07-23 09:25:44 +0900

    [Gold V] Title: 신촌 통폐합 계획, Time: 668 ms, Memory: 162028 KB -BaekjoonHub

--- 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
+++ 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
@@ -3,48 +3,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node {
-        int idx;
-        Node next;
-
-        public Node(int idx) {
-            this.idx = idx;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[] next = new int[N + 1];
+        int[] tail = new int[N + 1];
 
         // Input
         String[] univ = new String[N + 1];
         for (int i = 1; i <= N; i++) {
             univ[i] = br.readLine();
+            tail[i] = i;
         }// Input End
 
-        // Init
-        Node[] head = new Node[N + 1];
-        Node[] tail = new Node[N + 1];
-        for (int i = 1; i <= N; i++) {
-            head[i] = tail[i] = new Node(i);
-        }// Init End
-
+        int curIdx = 0;
         StringTokenizer st;
-        int endIdx = 0;
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int to = Integer.parseInt(st.nextToken());
             int from = Integer.parseInt(st.nextToken());
-
-            tail[to].next = head[from];
+            next[tail[to]] = from;
             tail[to] = tail[from];
-
-            if (i == N - 1) endIdx = to;
+            curIdx = to;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (Node node = head[endIdx]; node != null; node = node.next) {
-            sb.append(univ[node.idx]);
+        while(curIdx != 0){
+            sb.append(univ[curIdx]);
+            curIdx = next[curIdx];
         }
         System.out.println(sb);
     }
