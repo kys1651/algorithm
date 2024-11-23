0bd64792249b6d4a968539f338add8389a3cdfc3
김용수 <43926186+kys1651@users.noreply.github.com>
2024-11-23 21:37:22 +0900

    [Gold I] Title: 택배, Time: 244 ms, Memory: 18516 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.java
+++ 백준/Gold/8980. 택배/택배.java
@@ -1,62 +1,72 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+class Delivery implements Comparable<Delivery> {
+    int start;
+    int end;
+    int boxNum;
+
+    Delivery(int start, int end, int boxNum) {
+        this.start = start;
+        this.end = end;
+        this.boxNum = boxNum;
+    }
+
+    @Override
+    public int compareTo(Delivery o) {
+        if (this.end == o.end) {
+            return this.start - o.start;
+        }
+        return this.end - o.end;
+    }
+}
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 마을의 수
-        int n = Integer.parseInt(st.nextToken());
-        // 용량의 limit
-        int c = Integer.parseInt(st.nextToken());
-        int[] addTown = new int[n + 1];
-        int[] subTown = new int[n + 1];
-        // 박스 정보의 개수
-        int m = Integer.parseInt(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 마을의 수
+        int c = Integer.parseInt(st.nextToken()); // 트럭 용량
+
+        int m = Integer.parseInt(br.readLine()); // 박스 정보 개수
+        Delivery[] deliveries = new Delivery[m];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int value = Integer.parseInt(st.nextToken());
-            addTown[from] += value;
-            subTown[to] += value;
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int boxNum = Integer.parseInt(st.nextToken());
+            deliveries[i] = new Delivery(start, end, boxNum);
         }
 
-        // 싣는 박스의 개수가 상한보다 크다면 어차피 갖지 못함
-        for (int i = 1; i <= n; i++) {
-            if(addTown[i] >= c){
-                addTown[i] = c;
-            }
-        }
+        // 도착 마을 기준 정렬
+        Arrays.sort(deliveries);
+
+        int[] weight = new int[n + 1]; // 각 경유지의 남은 용량 추적
+        Arrays.fill(weight, c);
 
         int result = 0;
-        int curBox = 0;
-        for (int i = 1; i <= n; i++) {
-            // 박스 배달 가능하다면
-            if (subTown[i] > 0) {
-                // 현재 가진 박스보다 많이 제출 할 수 있다면 전부 반환
-                if(subTown[i] > curBox){
-                    result += curBox;
-                    curBox = 0;
-                }
-                // 현재 가진 박스보다 많이 가지고 있다면 낼 수 있는건 다 낸다.
-                else{
-                    result += subTown[i];
-                    curBox -= subTown[i];
-                }
+        for (Delivery d : deliveries) {
+            int maxBoxNum = Integer.MAX_VALUE;
+
+            // 경유지 용량 확인
+            for (int i = d.start; i < d.end; i++) {
+                maxBoxNum = Math.min(maxBoxNum, weight[i]);
             }
-            // 박스 수령 가능 하고 보내는 택배가 있다면
-            if(curBox < c && addTown[i] > 0){
-                if(curBox + addTown[i] > c){
-                    curBox = c;
-                }else{
-                    curBox += addTown[i];
-                }
+
+            // 실제 운반할 박스 수
+            int sendBox = Math.min(maxBoxNum, d.boxNum);
+
+            // 경유지 용량 업데이트
+            for (int i = d.start; i < d.end; i++) {
+                weight[i] -= sendBox;
             }
+
+            result += sendBox;
         }
+
         System.out.println(result);
     }
 }
