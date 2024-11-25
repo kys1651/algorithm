f43491013ebc44c1021d195354d646ff88a534cc
김용수 <43926186+kys1651@users.noreply.github.com>
2024-11-25 14:44:55 +0900

    [level 0] Title: 배열 만들기 6, Time: 61.41 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -4,15 +4,11 @@ class Solution {
     public int[] solution(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         
-        int size = 0, i = 0;
-        for(; i != arr.length;){
-            if(stack.isEmpty()){
-                stack.push(arr[i++]);
+        for(int i = 0; i != arr.length; i++){
+            if(stack.isEmpty() || stack.peek() != arr[i]){
+                stack.push(arr[i]);
             }else if(stack.peek() == arr[i]){
-                i++;
                 stack.pop();
-            }else if(stack.peek() != arr[i]){
-                stack.push(arr[i++]);
             }
         }
         
