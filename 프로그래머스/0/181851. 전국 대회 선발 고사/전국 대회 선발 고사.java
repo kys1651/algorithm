69d0c7bb9610c6309bb2c2c7632a768eba522c83
김용수 <43926186+kys1651@users.noreply.github.com>
2024-11-29 22:23:42 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 1.01 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,38 +1,12 @@
 import java.util.PriorityQueue;
 
 class Solution {
-    static class Student implements Comparable<Student>{
-        int idx;
-        int rank;
-        boolean attendance;
-        
-        public Student(int idx, int rank, boolean attendance){
-            this.idx = idx;
-            this.rank = rank;
-            this.attendance = attendance;
-        }
-        
-        @Override
-        public int compareTo(Student s1){
-            if(this.attendance && s1.attendance){
-                return Integer.compare(this.rank, s1.rank);
-            }else if(this.attendance){
-                return -1;
-            }else if(s1.attendance){
-                return 1;
-            }
-            return Integer.compare(this.rank, s1.rank);
-        }
-    }
-    
     public int solution(int[] rank, boolean[] attendance) {
-        PriorityQueue<Student> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> rank[a] - rank[b]);
         for(int i = 0; i < rank.length; i++){
-            pq.add(new Student(i, rank[i], attendance[i]));
+            if(attendance[i]) 
+                pq.add(i);
         }
-        int answer = pq.poll().idx * 10000;
-        answer += pq.poll().idx * 100;
-        answer += pq.poll().idx;
-        return answer;
+        return pq.poll() * 10000 + pq.poll() * 100 + pq.poll();
     }
 }
