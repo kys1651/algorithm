d8c6ce3f640dad56d9b9d15eb726b86ee5c58edd
김용수 <43926186+kys1651@users.noreply.github.com>
2024-12-17 23:05:14 +0900

    [level 2] Title: [3차] n진수 게임, Time: 436.78 ms, Memory: 373 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,45 +1,17 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        // n: 진법
-        // t: 미리 구할 숫자의 개수
-        // m: 게임에 참가하는 인원
-        // p: 튜브의 순서
-        // 2, 4, 2, 1이면
-        /*
-        1. 각 수를 N진수 만들기
-        2. N 진수를 전부 연결하기 (길이가 m * t 이상이 될 때 까지)
-        3. 튜브의 순서를 기준으로 m만큼 더해주면서 t개 구하기
-        출력하면서 그냥 숫자로 만들자.
-        */
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(0);
-        for(int i = 1; list.size() < t * m; i++){
-            getNValue(list, n, i);
+        int start = 0;
+        StringBuilder totalString = new StringBuilder();
+        while(totalString.toString().length() < m * t){
+            totalString.append(Integer.toString(start++, n));
         }
+        
         StringBuilder answer = new StringBuilder();
-        for(int idx = p - 1, size = 0; size < t; size++, idx += m){
-            int val = list.get(idx);
-            if(val >= 10){
-                val = 'A' + val - 10;
-            }else{
-                val += '0';
-            }
-            
-            answer.append((char)(val));
+        String total = totalString.toString();
+        for(int i = 0; i < t; i++){
+            answer.append(total.charAt(p - 1 + i * m));
         }
-        return answer.toString();
-    }
-    
-    private static void getNValue(ArrayList<Integer> list, int n, int value){
-        Stack<Integer> tmp = new Stack<>();
-        while(value != 0){
-            tmp.push(value % n);
-            value /= n;
-        }
-        while(!tmp.isEmpty()){
-            list.add(tmp.pop());    
-        }
+        
+        return answer.toString().toUpperCase();
     }
 }
